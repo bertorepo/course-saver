58f352ca18560de9bc946b769979849bc2078f87
jm.deguzman <jm.deguzman@PHR9X00947ML.g07.fujitsu.local>
2020-06-02 07:45:42 +0800

    Venue List

---
+++ src/main/java/com/fujitsu/ph/tsup/SpringBootMvcApplication.java
@@ -0,0 +1,14 @@
+package com.fujitsu.ph.tsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class SpringBootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fujitsu/ph/tsup/venue/management/model/VenueListForm.java
@@ -0,0 +1,28 @@
+package com.fujitsu.ph.tsup.venue.management.model;
+
+import java.util.Set;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+public class VenueListForm {
+	
+	private Set<VenueNames> VNs;
+
+	
+	
+	public Set<VenueNames> getVNs() {
+		return VNs;
+	}
+	public void setVNs(Set<VenueNames> VNs) {
+		this.VNs = VNs;
+	}
+	@Override
+	public String toString() {
+		return "VenueListForm [VNs=" + VNs +"]";
+	}
+
+}
