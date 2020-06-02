843dea38f7301f1dc68a2bc39082f10573e5315c
jc.jimenez <jc.jimenez@fujitsu.com>
2020-06-02 16:54:43 +0800

    updated author of venue update

--- src/main/java/com/fujitsu/ph/tsup/venue/management/model/VenueUpdateForm.java
+++ src/main/java/com/fujitsu/ph/tsup/venue/management/model/VenueUpdateForm.java
@@ -1,5 +1,11 @@
 package com.fujitsu.ph.tsup.venue.management.model;
 
+/*
+ * 	Author: Jimenez, John Carlo, R.
+ *  Last Modified: 6/2/2020
+ * 
+ */
+
 import java.util.Set;
 
 import javax.validation.constraints.Min;
