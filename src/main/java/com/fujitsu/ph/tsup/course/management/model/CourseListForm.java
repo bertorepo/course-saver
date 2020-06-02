e7e4ce9c4c814ce4037b5293ac8a4712624e6755
m.angara <m.angara@PHR9X00943ML.g07.fujitsu.local>
2020-06-02 14:45:26 +0800

    created the course list, controller, form, html

---
+++ src/main/java/com/fujitsu/ph/tsup/course/management/model/CourseListForm.java
@@ -0,0 +1,21 @@
+package com.fujitsu.ph.tsup.course.management.model;
+
+import java.util.Set;
+
+public class CourseListForm {
+	
+	private Set<CourseNames> CNs;
+	public Set<CourseNames> getCNs() 
+	{
+		return CNs;
+	}
+	public void setCNs(Set<CourseNames> CNs) 
+	{
+		this.CNs = CNs;
+	}
+	@Override
+	public String toString() {
+		return "CourseListForm [CNs=" + CNs +"]";
+	}
+
+}
