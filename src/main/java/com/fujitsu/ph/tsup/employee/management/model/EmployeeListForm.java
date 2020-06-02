ddb163b7d2ed1a65c3c283bad3a55ae6492fd010
glenda.cabiling <g.cabiling@fujitsu.com>
2020-06-02 14:14:48 +0800

    List of Employees html file, Contoller, and Form

---
+++ src/main/java/com/fujitsu/ph/tsup/employee/management/model/EmployeeListForm.java
@@ -0,0 +1,83 @@
+package com.fujitsu.ph.tsup.employee.management.model;
+
+public class EmployeeListForm {
+
+
+		private int id;
+		private int empNumber;
+		private String firstName;
+		private String lastName;
+		private String emailAddress;
+		private String userName;
+		
+
+		public EmployeeListForm() {
+			
+		}
+		
+		public EmployeeListForm(int id, int empNumber, String firstName, String lastName, String emailAddress, String userName) {
+			this.id = id;
+			this.empNumber = empNumber;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.emailAddress = emailAddress;
+			this.userName = userName;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public int getEmpNumber() {
+			return empNumber;
+		}
+
+		public void setEmpNumber(int empNumber) {
+			this.empNumber = empNumber;
+		}
+
+		public String getFirstName() {
+			return firstName;
+		}
+
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+
+		public String getLastName() {
+			return lastName;
+		}
+
+		public void setLastName(String lastName) {
+			this.lastName = lastName;
+		}
+
+		public String getEmailAddress() {
+			return emailAddress;
+		}
+
+		public void setEmailAddress(String emailAddress) {
+			this.emailAddress = emailAddress;
+		}
+
+		public String getUserName() {
+			return userName;
+		}
+
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+
+		@Override
+		public String toString() {
+			return "EMP [id=" + id + ", empNumber=" + empNumber + ", firstName=" + firstName + ", lastName=" + lastName
+					+ ", emailAddress=" + emailAddress + ", userName=" + userName + "]";
+		}
+		
+		
+		
+	} 
