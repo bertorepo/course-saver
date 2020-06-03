ac242843ced353a188e4ebc3b39746a18d49d726
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-06-03 17:45:47 +0800

    Course.java - Done

--- src/main/java/com/fujitsu/ph/tsup/domain/lumontad/Course.java
+++ src/main/java/com/fujitsu/ph/tsup/domain/lumontad/Course.java
@@ -1,5 +1,46 @@
 package com.fujitsu.ph.tsup.domain.lumontad;
 
 public class Course {
+    private Long courseID;
+    private String courseName;
+
+    protected Course() {
+
+    }
+
+    private Course(Builder builder) {
+        this.courseID = builder.courseID;
+        this.courseName = builder.courseName;
+    }
+
+    public Long getId() {
+        return courseID;
+    }
+
+    public String getName() {
+        return courseName;
+    }
+
+    public static class Builder {
+        private Long courseID;
+        private String courseName;
+
+        public Builder(String courseName) {
+            validateCourseName(courseName);
+
+            this.courseName = courseName;
+        }
+
+        public Course build() {
+            return new Course(this);
+        }
+
+        private void validateCourseName(String courseName) {
+            if (courseName == null || courseName.isEmpty() || courseName.length() < 5 || courseName.length() > 100) {
+                throw new IllegalArgumentException("Course Name should not be empty");
+            }
+
+        }
+    }
 
 }
