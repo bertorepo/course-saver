2cba53a2c994f2130dcc5f4ab5c9b2c7360b275a
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-06-03 14:14:37 +0800

    Done to Validations

--- src/main/java/com/fujitsu/ph/tsup/employee/management/model/EMPLOYEE_CREATE.java
+++ src/main/java/com/fujitsu/ph/tsup/employee/management/model/EMPLOYEE_CREATE.java
@@ -1,64 +1,117 @@
 package com.fujitsu.ph.tsup.employee.management.model;
 
-public class EMPLOYEE_CREATE {	
-	private long ID;
-	private String NUMBER;
-	private String LAST_NAME;
-	private String FIRST_NAME;
-	private String EMAIL_ADDRESS;
-	private String USERNAME;
-	private String submit;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
-	
-	public Long getID() {
-		return ID;
-	}
-	public void setID(Long ID) {
-		this.ID = ID;
-	}
-	public String getNUMBER() {
-		return NUMBER;
-	}
-	public void setNUMBER(String NUMBER) {
-		this.NUMBER = NUMBER;
-	}
-	public String getLAST_NAME() {
-		return LAST_NAME;
-	}
-	public void setLAST_NAME(String LAST_NAME) {
-		this.LAST_NAME = LAST_NAME;
-	}
-	public String getFIRST_NAME() {
-		return FIRST_NAME;
-	}
-	public void setFIRST_NAME(String FIRST_NAME) {
-		this.FIRST_NAME = FIRST_NAME;
-	}
-	public String getEMAIL_ADDRESS() {
-		return EMAIL_ADDRESS;
-	}
-	public void setEMAIL_ADDRESS(String EMAIL_ADDRESS) {
-		this.EMAIL_ADDRESS = EMAIL_ADDRESS;
-	}
-	public String getUSERNAME() {
-		return USERNAME;
-	}
-	public void setUSERNAME(String USERNAME) {
-		this.USERNAME = USERNAME;
-	}
-	public String getSubmit() {
-		return submit;
-	}
-	public void setSubmit(String submit) {
-		this.submit = submit;
-	}
-	@Override
-	public String toString() {
-		return "EMPLOYEE_CREATE [ID=" + ID + ", NUMBER="
-				+ NUMBER + ", LAST_NAME=" + LAST_NAME + ", FIRST_NAME=" + FIRST_NAME
-				+ ", EMAIL_ADDRESS=" + EMAIL_ADDRESS + ", USERNAME=" + USERNAME + ", submit="
-				+ submit + "]";
-	}
-	
-	
+public class EMPLOYEE_CREATE {
+
+    /* Employee ID */
+    private Long employeeID;
+
+    /* Employee Number */
+    @NotBlank(message = "Employee Number cannot be null")
+    @Size(min = 10, max = 10, message = "Employee number should be 10 characters")
+    private String employeeNumber;
+
+    /* Employee's Lastname */
+    @NotBlank(message = "Last Name annot be Null")
+    private String lastName;
+
+    /* Employee's Firstname */
+    @NotBlank(message = "First Name cannot be Null")
+    private String firstName;
+
+    /* Employee's Email Address */
+    @NotBlank(message = "Email Address cannot be Null")
+    @Email(message = "Invalid Email Address")
+    private String emailAddress;
+
+    /* Employee's Username */
+    @NotBlank(message = "Username cannot be Null")
+    @Size(min = 6, max = 50)
+    private String username;
+
+    /* Submit Button */
+    private String submit;
+
+    /* Getter and Setters */
+
+    /* Getter of Employee ID */
+    public Long getEmployeeID() {
+        return employeeID;
+    }
+
+    /* Setter of Employee ID */
+    public void setEmployeeID(Long employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    /* Getter of Employee Number */
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    /* Setter of Employee Number */
+    public void setEmployeeNumber(String employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    /* Getter of Employee's Lastname */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /* Setter of Employee's Lastname */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /* Getter of Employee's Firstname */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /* Setter of Employee's Firstname */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /* Getter of Employee's Email Address */
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    /* Setter of Employee's Email Address */
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    /* Getter of Employee's Username */
+    public String getUsername() {
+        return username;
+    }
+
+    /* Setter of Employee's Username */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /* Getter of Submit Button */
+    public String getSubmit() {
+        return submit;
+    }
+
+    /* Setter of Submit Button */
+    public void setSubmit(String submit) {
+        this.submit = submit;
+    }
+
+    @Override
+    public String toString() {
+        return "EMPLOYEE_CREATE [employeeID=" + employeeID + ", employeeNumber=" + employeeNumber + ", lastName=" + lastName
+                + ", firstName=" + firstName + ",  emailAddress=" + emailAddress + ", username=" + username
+                + ", submit=" + submit + "]";
+    }
+
 }
