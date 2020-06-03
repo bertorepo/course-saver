b85edf56cbc33c9e9e56c9d9d33f272b87c45aee
jm.deguzman <jm.deguzman@PHR9X00947ML.g07.fujitsu.local>
2020-06-03 10:59:22 +0800

    updated Venue List

--- src/main/java/com/fujitsu/ph/tsup/venue/management/web/VenueListController.java
+++ src/main/java/com/fujitsu/ph/tsup/venue/management/web/VenueListController.java
@@ -26,57 +26,57 @@ import com.fujitsu.ph.tsup.venue.management.model.VenueNames;
 @Controller
 @RequestMapping("/venues")
 public class VenueListController {
-	private static Logger logger = LoggerFactory.getLogger(VenueListController.class);
-	@GetMapping()
-	public String show(Model model) {
-		logger.debug("Model:{}", model);
-		
-		if (model.containsAttribute("venueList")) {
-			return "venue-management/venueList";
-		}
-		
-		VenueListForm venue = new VenueListForm();
-		venue.setSearch("");
-		venue.setVNs(createVNs());
-		model.addAttribute("venueList", venue);
-		
-		return "venue-management/venueList";
-		
-	}
-	@PostMapping("")
-	public String submit(
-			@Valid @ModelAttribute("venueList") VenueListForm venueList,
-			BindingResult result, Model model, RedirectAttributes redirectAttributes) {
-		
-		logger.debug("Venue List:{}", venueList);
-		logger.debug("Result:{}", result);
+    private static Logger logger = LoggerFactory.getLogger(VenueListController.class);
 
-		venueList.setVNs(createVNs());
+    @GetMapping()
+    public String show(Model model) {
+        logger.debug("Model:{}", model);
 
-		model.addAttribute("venueList", venueList);
-		if (result.hasErrors()) {
-			return "venue-management/venueList";
-		}
-		
-		redirectAttributes.addFlashAttribute("venueList", venueList);
-		return "redirect:/venues";
-		
-	}
+        if (model.containsAttribute("venueList")) {
+            return "venue-management/venueList";
+        }
 
+        VenueListForm venue = new VenueListForm();
+        venue.setSearch("");
+        venue.setVns(createVns());
+        model.addAttribute("venueList", venue);
 
-	private Set<VenueNames> createVNs() {
-		Set<VenueNames> vns = new HashSet<>();
-		
-		VenueNames vn1 = new VenueNames();
-		vn1.setId(1);
-		vn1.setName("Mutien Hall");
-		vns.add(vn1);
-		
-		VenueNames vn2 = new VenueNames();
-		vn2.setId(2);
-		vn2.setName("Benilde Hall");
-		vns.add(vn2);
-		
-		return vns;
-	}
+        return "venue-management/venueList";
+
+    }
+
+    @PostMapping("")
+    public String submit(@Valid @ModelAttribute("venueList") VenueListForm venueList, BindingResult result, Model model,
+            RedirectAttributes redirectAttributes) {
+
+        logger.debug("Venue List:{}", venueList);
+        logger.debug("Result:{}", result);
+
+        venueList.setVns(createVns());
+
+        model.addAttribute("venueList", venueList);
+        if (result.hasErrors()) {
+            return "venue-management/venueList";
+        }
+
+        redirectAttributes.addFlashAttribute("venueList", venueList);
+        return "redirect:/venues";
+
+    }
+
+    private Set<VenueNames> createVns() {
+        Set<VenueNames> vns = new HashSet<>();
+
+        VenueNames vn1 = new VenueNames();
+        vn1.setId(1);
+        vn1.setName("Mutien Hall");
+        vns.add(vn1);
+
+        VenueNames vn2 = new VenueNames();
+        vn2.setId(2);
+        vn2.setName("Benilde Hall");
+        vns.add(vn2);
+
+        return vns;
+    }
 }
