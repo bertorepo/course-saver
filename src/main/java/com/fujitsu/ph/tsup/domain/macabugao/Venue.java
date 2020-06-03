df8f7ba3c1a551acf30f7f9aa01e8e2210680372
j.macabugao <j.macabugao@PHR9Z02634ML.g07.fujitsu.local>
2020-06-03 20:42:46 +0800

    Updated Build Pattern

--- src/main/java/com/fujitsu/ph/tsup/domain/macabugao/Venue.java
+++ src/main/java/com/fujitsu/ph/tsup/domain/macabugao/Venue.java
@@ -1,5 +1,45 @@
 package com.fujitsu.ph.tsup.domain.macabugao;
 
 public class Venue {
+    private Long id;
+    private String venueName;
 
+    protected Venue() {
+
+    }
+
+    private Venue(Builder builder) {
+        this.id = builder.id;
+        this.venueName = builder.venueName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getVenueName() {
+        return venueName;
+    }
+
+    public static class Builder {
+        private Long id;
+        private String venueName;
+
+        public Builder(String venueName) {
+            validateVenueName(venueName);
+
+            this.venueName = venueName;
+        }
+
+        public Venue build() {
+            return new Venue(this);
+        }
+
+        private void validateVenueName(String venueName) {
+            if (venueName == null || venueName.isEmpty() || venueName.length() < 1 || venueName.length() > 100) {
+                throw new IllegalArgumentException("Venue Name should not be empty");
+            }
+
+        }
+    }
 }
