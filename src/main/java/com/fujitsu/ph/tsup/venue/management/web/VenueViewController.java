5060c2b97ba190893ef1df0d05eeba3f35158bff
j.yu <j.yu@PHR9X00946ML.g07.fujitsu.local>
2020-06-03 14:56:38 +0800

    modified venues/view

--- src/main/java/com/fujitsu/ph/tsup/venue/management/web/VenueViewController.java
+++ src/main/java/com/fujitsu/ph/tsup/venue/management/web/VenueViewController.java
@@ -1,11 +1,16 @@
+/* Created by: Jhaisy Jade Yu */
 package com.fujitsu.ph.tsup.venue.management.web;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -15,35 +20,45 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.fujitsu.ph.tsup.venue.management.model.VenueListForm;
-import com.fujitsu.ph.tsup.venue.management.model.VenueNames;
+
 import com.fujitsu.ph.tsup.venue.management.model.VenueViewForm;
 
 @Controller
-@RequestMapping("/venues/view")
+@RequestMapping("/venues")
 public class VenueViewController {
 	
-	@GetMapping()
+	private static Logger logger = LoggerFactory.getLogger(VenueViewController.class);
+	
+	@GetMapping("/view")
 	public String show(Model model) {
-		VenueListForm venue = new VenueListForm();
-		Set<VenueNames> vns = new HashSet<>();
-		
-		VenueNames vn1 = new VenueNames();
-		vn1.setId(1);
-		vn1.setName("Mutien Hall");
-		vns.add(vn1);
-		
-		VenueNames vn2 = new VenueNames();
-		vn2.setId(2);
-		vn2.setName("Benilde Hall");
-		vns.add(vn2);
-		
-		/*venue.setVNs(vns);
-		model.addAttribute("venueView", venue);
-		*/
+		logger.debug("Venue:{}", model);
+		if (model.containsAttribute("venueViewForm")) {
+			return "venue-management/venueView";
+		}
+		VenueViewForm venue = new VenueViewForm();
+		venue.setID(1);
+		venue.setVenueName("EcoTower");
+		venue.setSearch("");
+		model.addAttribute("view", venue);
 		return "venue-management/venueView";
+	}
+	@PostMapping("/view")
+	public String submit(
+			@Valid @ModelAttribute("view") VenueViewForm view, 
+			BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+		logger.debug("Venue:{}", view);
+		logger.debug("Result:{}", result);
+		view.setID(1);
+		view.setVenueName("EcoTower");
+		
+		model.addAttribute("view", view);
+		if (result.hasErrors()) {
+			return "venue-management/venueView";
+		}
+		redirectAttributes.addFlashAttribute("view", view);
+		return "redirect:/venues";
 		
 	}
 }
-
