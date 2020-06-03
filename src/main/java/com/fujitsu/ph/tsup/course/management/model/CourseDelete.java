f70f17fdc3a12206225020b3fa602d3f6624199a
k.freo <k.freo@PHR9Z02631ML.g07.fujitsu.local>
2020-06-03 16:43:15 +0800

    Commit

--- src/main/java/com/fujitsu/ph/tsup/course/management/model/CourseDelete.java
+++ src/main/java/com/fujitsu/ph/tsup/course/management/model/CourseDelete.java
@@ -1,36 +1,46 @@
 package com.fujitsu.ph.tsup.course.management.model;
 
+import javax.validation.constraints.NotBlank;
+
+/* Created by: @Author Freo, Kristine Faith */
+
 public class CourseDelete {
 	private int courseId;
 	private String courseName;
+	@NotBlank
+	(message="Field must not be empty")
+	private String search;
 	
 	public CourseDelete() {	
 	}
 	
-	public CourseDelete(int Id, String courseName, String Delete) {
+	public CourseDelete(int Id, String courseName, String search) {
 		this.courseId = Id;
 		this.courseName = courseName;
+		this.search = search;
+		
 	}
-	
 		public int getCourseId() {
 		return courseId;
 	}
-
 	public void setCourseId(int courseId) {
 		this.courseId = courseId;
 	}
-
 	public String getCourseName() {
 		return courseName;
 	}
-
 	public void setCourseName(String courseName) {
 		this.courseName = courseName;
-	}	
-
+	}
+	public String getSearch() {
+		return search;
+	}
+	public void setSearch(String search) {
+		this.search = search;
+	}
 		@Override
 		public String toString() {
-			return "courseDelete[CourseName=" + courseName + ", CourseId=" + courseId + "]";	
+			return "courseDelete[CourseName=" + courseName + ", CourseId=" + courseId +", Search=" + search + "]";	
 	}
 }
 
--- src/main/java/com/fujitsu/ph/tsup/course/management/web/CourseDeleteController.java
+++ src/main/java/com/fujitsu/ph/tsup/course/management/web/CourseDeleteController.java
@@ -1,23 +1,58 @@
 package com.fujitsu.ph.tsup.course.management.web;
 
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/* @Author: Freo, Kristine Faith */
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fujitsu.ph.tsup.course.management.model.CourseDelete;
+import com.fujitsu.ph.tsup.course.management.web.CourseDeleteController;
+
 
 @Controller
 @RequestMapping("/Course")
 public class CourseDeleteController {
-
+	private static Logger logger = LoggerFactory.getLogger(CourseDeleteController.class);
+	
 	@GetMapping("/delete")
 	public String show(Model model) {
+		logger.debug("course:{}", model);
+		if (model.containsAttribute("CourseDelete")) {
+			return "course-management/CourseDelete";
+		}
 		CourseDelete course = new CourseDelete();
 		 course.setCourseId(20202);
 		 course.setCourseName("SDEM1");
+		 course.setSearch("");
 	
 		model.addAttribute("courseDelete", course);
 		return "course-management/courseDelete";
+	}
+	@PostMapping("/delete")
+	public String submit(
+			@Valid @ModelAttribute("CourseDelete") CourseDelete CourseDelete, 
+			BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+		logger.debug("CourseDelete:{}", CourseDelete);
+		logger.debug("Result:{}", result);
+		CourseDelete.setCourseId(20202);
+		CourseDelete.setCourseName("SDEM1");
+		
+		model.addAttribute("CourseDelete", CourseDelete);
+		if (result.hasErrors()) {
+			return "course-management/CourseDelete";
+		}
+		redirectAttributes.addFlashAttribute("courseDelete", CourseDelete);
+		return "redirect:/courseDelete";
 	}
 }
