caa390d24bfe00b70579b798c7fba8c23b28e2e7
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-06-04 08:55:04 +0800

    Done

--- src/main/java/com/fujitsu/ph/tsup/domain/lumontad/Venue.java
+++ src/main/java/com/fujitsu/ph/tsup/domain/lumontad/Venue.java
@@ -1,5 +1,43 @@
 package com.fujitsu.ph.tsup.domain.lumontad;
 
 public class Venue {
+    private Long venueID;
+    private String venueName;
+    
+    protected Venue() {    
+    }
+    
+    private Venue(Builder builder) {
+        this.venueID = builder.venueID;
+        this.venueName = builder.venueName;
+    }
+    
+    public Long getId() {
+        return venueID;
+    }
 
+    public String getVenueName() {
+        return venueName;
+    }
+
+    public static class Builder {
+        private Long venueID;
+        private String venueName;
+
+        public Builder(String venueName) {
+            validateVenueName(venueName);
+
+            this.venueName = venueName;
+        }
+        
+        public Venue build() {
+            return new Venue(this);
+        }
+        
+        private void validateVenueName(String venueName) {
+            if (venueName == null || venueName.isEmpty() || venueName.length() < 10 || venueName.length() > 50) {
+                throw new IllegalArgumentException("venue name should not be empty");
+            }           
+        }
+    }
 }
