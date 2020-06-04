27fd6f3cbc74f674163fcbcd9bcaf6bf4dc34bfe
m.angara <m.angara@PHR9X00943ML.g07.fujitsu.local>
2020-06-04 14:15:32 +0800

    updating builder, from my domain.

--- src/main/java/com/fujitsu/ph/tsup/domain/angara/CourseSchedule.java
+++ src/main/java/com/fujitsu/ph/tsup/domain/angara/CourseSchedule.java
@@ -1,5 +1,122 @@
 package com.fujitsu.ph.tsup.domain.angara;
 
 public class CourseSchedule {
+    private Long id;
+    private Long instructorId;
+    private Long venueId;
+    private Long courseId;
+    private int minRequired;
+    private int maxRequired;
+    private String status;
+
+    protected CourseSchedule() {
+
+    }
+
+    private CourseSchedule(Builder builder) {
+        this.id = builder.id;
+        this.instructorId = builder.instructorId;
+        this.courseId = builder.courseId;
+        this.venueId = builder.venueId;
+        this.minRequired = builder.minRequired;
+        this.maxRequired = builder.maxRequired;
+        this.status = builder.status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getInstructionId() {
+        return instructorId;
+    }
+
+    public Long getVenueId() {
+        return venueId;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public int getMinRequired() {
+        return minRequired;
+    }
+
+    public int getMaxRequired() {
+        return maxRequired;
+    }
+
+    private String getStatus() {
+        return status;
+    }
+
+    public static class Builder {
+        private String status;
+        private int maxRequired;
+        private int minRequired;
+        private Long venueId;
+        private Long courseId;
+        private Long instructorId;
+        private Long id;
+
+        public Builder(Long courseId, Long instructorId, Long venueId, int minRequired, int maxRequired,
+                String status) {
+            ValidateCourseId(courseId);
+            ValidateInstructorId(instructorId);
+            ValidateVenueId(venueId);
+            ValidateMinRequired(minRequired);
+            ValidateMaxRequired(maxRequired);
+            ValidateStatus(status);
+
+            this.courseId = courseId;
+            this.instructorId = instructorId;
+            this.venueId = venueId;
+            this.minRequired = minRequired;
+            this.maxRequired = maxRequired;
+            this.status = status;
+        }
+        
+        public CourseSchedule build() {
+            return new CourseSchedule(this);
+        }
+
+        private void ValidateCourseId(Long courseId) {
+            if (courseId == null || courseId == 0) {
+                throw new IllegalArgumentException("Provide appropriate Course Id.");
+            }
+        }
+
+        private void ValidateInstructorId(Long instructorId) {
+            if (instructorId == 0 || instructorId == null) {
+                throw new IllegalArgumentException("Provide appropriate Instructor Id.");
+            }
+        }
+
+        private void ValidateVenueId(Long venueId) {
+            if (venueId == null) {
+                throw new IllegalArgumentException("Provide appropriate Venue Id.");
+            }
+        }
+
+        private void ValidateMinRequired(int minRequired) {
+            if (minRequired == 0) {
+                throw new IllegalArgumentException("Provide Minimum Required of Participants.");
+            }
+        }
+
+        private void ValidateMaxRequired(int maxRequired) {
+            if (maxRequired <= 100 || maxRequired == 0) {
+                throw new IllegalArgumentException("Provide Maximum Required of Participants.");
+            }
+        }
+
+        private void ValidateStatus(String status) {
+            if (status.isEmpty() || status == null || status.length() < 1) {
+                throw new IllegalArgumentException("Provide Status");
+            }
+        }
+
+    }
 
 }
--- src/main/java/com/fujitsu/ph/tsup/domain/angara/Employee.java
+++ src/main/java/com/fujitsu/ph/tsup/domain/angara/Employee.java
@@ -1,5 +1,101 @@
 package com.fujitsu.ph.tsup.domain.angara;
 
 public class Employee {
+    private Long id;
+    private Long employeeNumber;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String userName;
 
+    protected Employee() {
+    }
+
+    private Employee(Builder builder) {
+        this.id = builder.id;
+        this.emailAddress = builder.emailAddress;
+        this.employeeNumber = builder.employeeNumber;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.userName = builder.userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public static class Builder {
+        public String userName;
+        public String lastName;
+        public String firstName;
+        public String emailAddress;
+        public Long id;
+        private Long employeeNumber;
+
+        public Builder(Long employeeNumber, String firstName, String lastName, String emailAddress, String userName) {
+            validateEmployeeNumber(employeeNumber);
+            validateLastName(lastName);
+            validateLastName(firstName);
+            validateEmailAddress(emailAddress);
+            validateUserName(userName);
+
+            this.employeeNumber = employeeNumber;
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.emailAddress = emailAddress;
+            this.userName = userName;
+        }
+
+        private void validateUserName(String userName) {
+            if (userName.isEmpty() || userName == null || userName.length() > 50 || userName.length() < 3) {
+                throw new IllegalArgumentException("Provide appropriate username.");
+            }
+        }
+
+        private void validateEmailAddress(String emailAddress) {
+            if (emailAddress.isEmpty() || emailAddress == null || emailAddress.length() > 50
+                    || emailAddress.length() < 3) {
+                throw new IllegalArgumentException("Provide appropriate email address.");
+            }
+        }
+
+        private void validateLastName(String lastName) {
+            if (lastName.isEmpty() || lastName == null || lastName.length() > 50 || lastName.length() < 1) {
+                throw new IllegalArgumentException("Provide appropriate last name.");
+            }
+
+        }
+
+        private void validateEmployeeNumber(Long employeeNumber) {
+            if (employeeNumber == null || employeeNumber == 0 || employeeNumber > 50) {
+                throw new IllegalArgumentException("Provide appropriate first name.");
+            }
+        }
+
+        private void validateFirstName(String firstName) {
+            if (firstName.isEmpty() || firstName == null || firstName.length() > 50 || firstName.length() < 1) {
+                throw new IllegalArgumentException("Provide appropriate first name.");
+            }
+        }
+    }
 }
