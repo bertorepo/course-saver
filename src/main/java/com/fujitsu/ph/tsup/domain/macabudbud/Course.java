f31bcf8e5b5187861f54c95d7d01a930948c0c5e
j.macabudbud <j.macabudbud@PHR9Z01428ML.g07.fujitsu.local>
2020-06-04 10:43:28 +0800

    Implementation of builder class with data validation.

--- src/main/java/com/fujitsu/ph/tsup/domain/macabudbud/Course.java
+++ src/main/java/com/fujitsu/ph/tsup/domain/macabudbud/Course.java
@@ -1,5 +1,47 @@
 package com.fujitsu.ph.tsup.domain.macabudbud;
 
 public class Course {
-
+	private Long id;
+	private String courseName;
+	
+	protected Course() {	
+	}
+	
+	private Course(Builder builder) {
+		this.id = builder.id;
+		this.courseName = builder.courseName;
+	}
+	
+	public static class Builder {
+		private Long id;
+		private String courseName;
+		
+		public Builder(Long id, String courseName) {
+			validateCourseName(courseName);
+			
+			this.id = id;
+			this.courseName = courseName;
+		}
+		
+		public Course build() {
+			return new Course(this);
+		}
+		
+		public void validateCourseName(String courseName) {
+			if(courseName == null || courseName.isEmpty()) {
+				throw new IllegalArgumentException("Field required. Course name should notn be empty");
+			}else if(courseName.length() > 100){
+				throw new IllegalArgumentException("Characters exceeds to 100.");
+			}
+		}
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	
+	
 }
--- src/main/java/com/fujitsu/ph/tsup/domain/macabudbud/Venue.java
+++ src/main/java/com/fujitsu/ph/tsup/domain/macabudbud/Venue.java
@@ -1,5 +1,46 @@
 package com.fujitsu.ph.tsup.domain.macabudbud;
 
 public class Venue {
-
+	private Long id;
+	private String venueName;
+	
+	protected Venue() {	
+	}
+	
+	private Venue(Builder builder) {
+		this.id = builder.id;
+		this.venueName = builder.venueName;
+	}
+	
+	public static class Builder {
+		private Long id;
+		private String venueName;
+		
+		public Builder(Long id, String venueName) {
+			validateVenueName(venueName);
+			
+			this.id = id;
+			this.venueName = venueName;
+		}
+		
+		public Venue build() {
+			return new Venue(this);
+		}
+		
+		public void validateVenueName(String venueName) {
+			if(venueName == null || venueName.isEmpty()) {
+				throw new IllegalArgumentException("Field required. Venue name should notn be empty");
+			}else if(venueName.length() > 100){
+				throw new IllegalArgumentException("Characters exceeds to 100.");
+			}
+		}
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public String getVenueName() {
+		return venueName;
+	}	
+	
 }
