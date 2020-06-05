d9262b3d23a9f2c522055e5e315cc118ee3f371c
jc.jimenez <jc.jimenez@fujitsu.com>
2020-06-05 16:06:27 +0800

    Updated venue,course,employee,coursechedule (DAOs,Service,ServiceImpl)

---
+++ src/main/java/com/fujitsu/ph/tsup/domain/jimenez/CourseScheduleService.java
@@ -0,0 +1,9 @@
+package com.fujitsu.ph.tsup.domain.jimenez;
+
+import java.util.Set;
+
+public interface CourseScheduleService {
+    void save(CourseSchedule CourseSchedule);
+    Set<CourseSchedule> findAll();
+    CourseSchedule findById(Long id);
+}
