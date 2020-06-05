49bc6db0aa01cc26feaeaefdf8d6436d5ed07aac
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-06-05 09:31:35 +0800

    Course Service Done

--- src/test/java/com/fujitsu/ph/tsup/domain/lumontad/CourseServiceImplTest.java
+++ src/test/java/com/fujitsu/ph/tsup/domain/lumontad/CourseServiceImplTest.java
@@ -1,62 +1,85 @@
-//
-//package com.fujitsu.ph.tsup.domain.lumontad;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.Mockito.when;
-//
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.junit.rules.ExpectedException;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.TestConfiguration;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//public class CourseServiceImplTest {
-//
-//    @Rule
-//    public ExpectedException thrown = ExpectedException.none();
-//
-//    @TestConfiguration
-//    static class CourseServiceImplestContextConfiguration {
-//
-//        @Bean
-//        CourseService courseService() {
-//            return new CourseServiceImpl();
-//        }
-//
-//    }
-//
-//    @Autowired
-//    private CourseService courseService;
-//
-//    @MockBean
-//    private CourseDao courseDao;
-//
-//    @Test
-//    public void testFindCourse() {
-//        when(courseDao.findByid((long) 2)).thenReturn(createCourse());
-//        Course c = courseService.findByid((long) 2);
-//        assertEquals(c.getId(), new Long(2));
-//    }
-//
-//    @Test
-//    public void testFindCourse_Unequal() {
-//        thrown.expect(IllegalArgumentException.class);
-//        thrown.expectMessage("Course ID not Equal");
-//        when(courseDao.findByid((long) 2)).thenReturn(createCourse_Unequal());
-//
-//        courseService.findByid((long) 2);
-//    }
-//
-//    private Course createCourse() {
-//        return null;
-//    }
-//
-//    private Course createCourse_Unequal() {
-//        return null;
-//    }
-//
-//}
+
+package com.fujitsu.ph.tsup.domain.lumontad;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class CourseServiceImplTest {
+
+    @TestConfiguration
+    static class TestContextConfiguration {
+
+        @Bean
+        CourseService courseService() {
+            /*
+             * returns the implementation that will be injected in the service
+             */
+            return new CourseServiceImpl();
+        }
+
+    }
+
+    @Autowired
+    private CourseService courseService;
+
+    @MockBean
+    private CourseDao courseDao;
+
+    @Test
+    void testSave() {
+        when(courseDao.save(any(Course.class))).thenReturn(createId());
+
+        Course course = new Course.Builder(1L, "BS-ICT").build();
+
+        Long id = courseService.save(course);
+
+        assertEquals(1L, id);
+    }
+
+    @Test
+    void testFindById() {
+        Course expected = createCourse();
+        when(courseDao.findById(any(Long.class))).thenReturn(expected);
+
+        Course course = courseService.findById(1L);
+
+        assertEquals(expected.getId(), course.getId());
+        assertEquals(expected.getName(), course.getName());
+
+    }
+
+    @Test
+    void testFindById_NotFound() {
+        when(courseDao.findById(any(Long.class))).thenThrow(new DataRetrievalFailureException("error"));
+
+        Exception exception = assertThrows(CourseException.class, () -> {
+            courseService.findById(1L);
+        });
+
+        String expectedMessage = "Course not found";
+        String actualMessage = exception.getMessage();
+        assertFalse(actualMessage.contains(expectedMessage));
+    }
+
+    private Course createCourse() {
+        return new Course.Builder(1L, "BS-ICT").build();
+    }
+
+    private Long createId() {
+        return 1L;
+    }
+
+}
