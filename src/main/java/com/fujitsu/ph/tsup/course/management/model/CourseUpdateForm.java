7a8316a6a94cab058aa8c20342129300172b897e
t.oviedo <t.oviedo@PHR9Z02632ML.g07.fujitsu.local>
2020-06-05 18:34:56 +0800

    Updates

--- src/main/java/com/fujitsu/ph/tsup/course/management/model/CourseUpdateForm.java
+++ src/main/java/com/fujitsu/ph/tsup/course/management/model/CourseUpdateForm.java
@@ -1,18 +1,21 @@
 //@Author Thomas Oviedo
 package com.fujitsu.ph.tsup.course.management.model;
 
+import javax.validation.constraints.Size;
+
 public class CourseUpdateForm {
 	private int id;
+	@Size(min = 5)
 	private String name;
 
 	public CourseUpdateForm() {
 
 	}
+
 	public CourseUpdateForm(int Id, String Name) {
 		this.id = Id;
 		this.name = Name;
 	}
-
 
 	public int getId() {
 		return id;
