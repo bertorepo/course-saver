9ade168e72d4c9158f312f26c5e3c2b9573f3168
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-06-05 18:12:12 +0800

    CourseDaoImpl Update, will continue tomorrow

--- src/main/java/com/fujitsu/ph/tsup/domain/lumontad/CourseDaoImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/domain/lumontad/CourseDaoImpl.java
@@ -1,5 +1,35 @@
 package com.fujitsu.ph.tsup.domain.lumontad;
 
-public class CourseDaoImpl {
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CourseDaoImpl implements CourseDao {
+
+    
+    
+//    @Autowired
+//    private NamedParameterJdbcTemplate template;
+    
+    @Override
+    public void save(Course course) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Set<Course> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Course findById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
