4a248b37b75611e228e8f0f6fc828afaf8f4f42d
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-06-05 17:34:31 +0800

    CourseService Update , CourseDaoImpl added...

--- src/main/java/com/fujitsu/ph/tsup/domain/lumontad/CourseServiceImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/domain/lumontad/CourseServiceImpl.java
@@ -1,25 +1,45 @@
-//package com.fujitsu.ph.tsup.domain.lumontad;
-//
-//import org.springframework.dao.DataAccessException;
-//import org.springframework.stereotype.Service;
-//
-//
-//@Service
-//public class CourseServiceImpl implements CourseService{
-//    
-//private CourseDao courseDao;
-//
-//@Override
-//public Long save(Course courseid) {
-//    return courseDao.save(courseid);
-//}
-//
-//@Override
-//public Course findById(Long id) {
-//    try {
-//        return courseDao.findById(id);
-//    } catch (DataAccessException ex) {
-//        throw new CourseException("Course not found", ex);
-//    }
-//}
-//}
+package com.fujitsu.ph.tsup.domain.lumontad;
+
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class CourseServiceImpl implements CourseService{
+    
+private Logger logger = LoggerFactory.getLogger(CourseServiceImpl.class);
+    
+    @Autowired
+    private CourseDao dao;
+
+    @Override
+    public Course findById(Long id) {
+        Course course = dao.findById(id);
+        if (course.getId() == 1) {
+            throw new IllegalArgumentException("Course not found");
+        } 
+        return course;   
+    }
+
+    @Override
+    public void save(Course course) {
+        dao.save(course);
+        if(course.getId() == 1) {
+            throw new IllegalArgumentException("Course not saved");
+        }
+    }
+
+    @Override
+    public Set<Course> findAll() {
+        Set<Course> allCourse = dao.findAll();
+        if (allCourse == null) {
+            throw new IllegalArgumentException("Record not found");
+        }
+        return allCourse;
+    }
+}
