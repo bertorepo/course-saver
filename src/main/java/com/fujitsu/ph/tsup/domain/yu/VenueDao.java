70a842ed93fe3b1244dba2916adbcae03f3a6cf9
j.yu <j.yu@PHR9X00946ML.g07.fujitsu.local>
2020-06-05 17:31:54 +0800

    Employee and Venue

---
+++ src/main/java/com/fujitsu/ph/tsup/domain/yu/VenueDao.java
@@ -0,0 +1,10 @@
+package com.fujitsu.ph.tsup.domain.yu;
+
+import java.util.Set;
+
+public interface VenueDao {
+    void save(Venue id);
+    Set<Venue> findAll();
+    Venue findById(Long id);
+
+}
---
+++ src/main/java/com/fujitsu/ph/tsup/domain/yu/VenueService.java
@@ -0,0 +1,9 @@
+package com.fujitsu.ph.tsup.domain.yu;
+
+import java.util.Set;
+
+public interface VenueService {
+    void save(Venue id);
+    Set<Venue> findAll();
+    Venue findById(Long id);
+}
