5d6d74ca9b5937a4fe33d53bbc0a658b1cd997eb
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-06-08 09:05:41 +0800

    CourseImpl: Done

--- src/main/java/com/fujitsu/ph/tsup/domain/lumontad/CourseDao.java
+++ src/main/java/com/fujitsu/ph/tsup/domain/lumontad/CourseDao.java
@@ -3,9 +3,11 @@ package com.fujitsu.ph.tsup.domain.lumontad;
 import java.util.Set;
 
 public interface CourseDao {
- void save(Course course);
-    
+    void save(Course course);
+
     Set<Course> findAll();
-    
-    Course findById(Long id);  
+
+    Course findById(Long id);
+
+    Long saveLong(Course courseName);
 }
