dac659f79f54850107320c16342e2dc93230ae1e
glenda.cabiling <g.cabiling@fujitsu.com>
2020-06-18 16:01:29 +0800

    removed error

--- src/main/java/com/fujitsu/ph/tsup/domain/cabiling/CourseRowMapper.java
+++ src/main/java/com/fujitsu/ph/tsup/domain/cabiling/CourseRowMapper.java
@@ -7,12 +7,13 @@ import org.springframework.jdbc.core.RowMapper;
 
 public class CourseRowMapper implements RowMapper<Course> {
 
-@Override
-public Course mapRow(ResultSet rs, int rowNum) throws SQLException {
-    Long id = rs.getLong("id");
-    String name = rs.getString("name");
-    
-    Course Course = new Course.Builder(id, name).builder();
-    
-    return Course;
-}}
+	@Override
+	public Course mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Long id = rs.getLong("id");
+		String name = rs.getString("name");
+
+		Course Course = new Course.Builder(id, name).builder();
+
+		return Course;
+	}
+}
