e720e744167af474509e2404a755bf285d2b0717
j.macabudbud <j.macabudbud@PHR9Z01428ML.g07.fujitsu.local>
2020-06-22 11:18:38 +0800

    Addition
    refs #120686 add Added WebConfig file
    
    Added WebConfig file since the view of dashboard is depending on user
    type

---
+++ src/main/java/com/fujitsu/ph/tsup/WebConfig.java
@@ -0,0 +1,29 @@
+package com.fujitsu.ph.tsup;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        configurer.favorPathExtension(true)
+    		.ignoreAcceptHeader(false)
+            .defaultContentType(MediaType.TEXT_HTML)
+            .mediaType("html", MediaType.TEXT_HTML)
+            .mediaType("xml", MediaType.APPLICATION_XML)
+            .mediaType("json", MediaType.APPLICATION_JSON);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/dashboard").setViewName("Dashboard");
+    }
+}
