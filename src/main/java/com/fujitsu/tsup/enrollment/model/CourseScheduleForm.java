da414496c51a54b91a7dd83c5c7ff2426fb943f8
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-06-24 10:36:20 +0800

    refs #120776 add Added CourseScheduleForm
    Added CourseScheduleForm is a JavaBean Doc and is used by other classes
    to declare variables

--- src/main/java/com/fujitsu/tsup/enrollment/model/CourseScheduleForm.java
+++ src/main/java/com/fujitsu/tsup/enrollment/model/CourseScheduleForm.java
@@ -1,6 +1,5 @@
 package com.fujitsu.tsup.enrollment.model;
-
-//==================================================================================================
+//=================================================================================================
 //$Id:PR01$
 //Project Name :Training Sign Up
 //System Name  :Enroll Course
@@ -8,241 +7,161 @@ package com.fujitsu.tsup.enrollment.model;
 //
 //<<Modification History>>
 //Version | Date       | Updated By            | Content
-//--------+------------+-----------------------+---------------------------------------------------
-//0.01    | 06/19/2020 | WS) J.Yu              | New Creation
-//==================================================================================================
+//--------+------------+-----------------------+--------------------------------------------------
+//0.02    | 06/24/2020 | WS) J.Yu              | New Creation
+//=================================================================================================
 /**
 * <pre>
-* JavaBean for CourseScheduleForm 
+* JavaBean for CourseScheduleForm.java
 * <pre>
 * 
 * @version 0.01
-* @author j.yu                         
+* @author j.yu                       
 */
+import java.util.Set;
+
 public class CourseScheduleForm {
-  /* Course Schedule Id */
-  private Long id;
-  
-  /* Course Id */
-  private Long courseId;
-  
-  /* Course Name */
-  private String courseName;
-  
-  /* Instructor Id */
-  private Long instructorId;
-  
-  /* Instructor Name(LASTNAME, FIRSTNAME) */
-  private String instructorName;
-  
-  /* Venue Id */
-  private Long venueId;
-  
-  /* Venue Name */
-  private String venueName;
-  
-  /* Set of course schedule details */
-  private CourseScheduleDetailForm courseScheduleDetail;
-  
-  /* Minimum number of participants */
-  private int minRequired;
-  
-  /* Maximum number of participants */
-  private int maxAllowed;
-  
-  /* Total Number of Participants currently enrolled */
-  private int totalParticipants;
-  
-  /**
-   * JavaBean for CourseScheduleForm
-   */
-  public CourseScheduleForm(Long id, Long courseId, String courseName, Long instructorId, String instructorName,
-          Long venueId, String venueName, CourseScheduleDetailForm courseScheduleDetail, int minRequired,
-          int maxAllowed, int totalParticipants) {
-      super();
-      this.id = id;
-      this.courseId = courseId;
-      this.courseName = courseName;
-      this.instructorId = instructorId;
-      this.instructorName = instructorName;
-      this.venueId = venueId;
-      this.venueName = venueName;
-      this.courseScheduleDetail = courseScheduleDetail;
-      this.minRequired = minRequired;
-      this.maxAllowed = maxAllowed;
-      this.totalParticipants = totalParticipants;
-  }
-  
-  /**
-   * get Course Schedule id 
-   * @return id
-   */
-  public Long getId() {
-      return id;
-  }
-  
-  /** 
-   * set id
-   * @param id
-   */
-  public void setId(Long id) {
-      this.id = id;
-  }
-  
-  /**
-   * get Course id
-   * @return courseId
-   */
-  public Long getCourseId() {
-      return courseId;
-  }
-  
-  /**
-   * set Course id
-   * @param courseId
-   */
-  public void setCourseId(Long courseId) {
-      this.courseId = courseId;
-  }
-  
-  /**
-   * get Course Name
-   * @return getCourseName
-   */
-  public String getCourseName() {
-      return courseName;
-  }
-  
-  /**
-   * set Course Name
-   * @param courseName
-   */
-  public void setCourseName(String courseName) {
-      this.courseName = courseName;
-  }
-  
-  /**
-   * get Instructor Id
-   * @return
-   */
-  public Long getInstructorId() {
-      return instructorId;
-  }
-  
-  /**
-   * set Instructor Id
-   * @param instructorId
-   */
-  public void setInstructorId(Long instructorId) {
-      this.instructorId = instructorId;
-  }
-  
-  /**
-   * get Instructor Name
-   * @return
-   */
-  public String getInstructorName() {
-      return instructorName;
-  }
+    /* Course Schedule Id */
+    private Long id;
 
-  /**
-   * set Instructor Name
-   * @param instructorName
-   */
-  public void setInstructorName(String instructorName) {
-      this.instructorName = instructorName;
-  }
+    /* Course Id */
+    private Long courseId;
 
-  /**
-   * get Venue Id
-   * @return
-   */
-  public Long getVenueId() {
-      return venueId;
-  }
-  
-  /** 
-   * set Venue Id
-   * @param venueId
-   */
-  public void setVenueId(Long venueId) {
-      this.venueId = venueId;
-  }
+    /* Course Name */
+    private String courseName;
 
-  /**  
-   * get Venue Name
-   * @return
-   */
-  public String getVenueName() {
-      return venueName;
-  }
-  
-  /** 
-   * set Venue Name
-   * @param venueName
-   */
-  public void setVenueName(String venueName) {
-      this.venueName = venueName;
-  }
+    /* Instructor Id */
+    private Long instructorId;
 
-  /** 
-   * get Course Schedule Detail Form
-   * @return
-   */
-  public CourseScheduleDetailForm getCourseScheduleDetails() {
-      return courseScheduleDetail;
-  }
-  
-  /** 
-   * set Course Schedule Detail Form
-   * @param courseScheduleDetail
-   */
-  public void setCourseScheduleDetails(CourseScheduleDetailForm courseScheduleDetail) {
-      this.courseScheduleDetail = courseScheduleDetail;
-  }
-  
-  /** 
-   * get Minimum Required
-   * @return
-   */
-  public int getMinRequired() {
-      return minRequired;
-  }
+    /* Instructor Name(LASTNAME, FIRSTNAME) */
+    private String instructorName;
 
-  /** 
-   * set Minimum Required
-   * @param minRequired
-   */
-  public void setMinRequired(int minRequired) {
-      this.minRequired = minRequired;
-  }
+    /* Venue Id */
+    private Long venueId;
 
-  /** 
-   * get Maximum Required
-   * @return
-   */
-  public int getMaxAllowed() {
-      return maxAllowed;
-  }
+    /* Venue Name */
+    private String venueName;
 
-  /**
-   * set Maximum Required
-   * @param maxAllowed
-   */
-  public void setMaxAllowed(int maxAllowed) {
-      this.maxAllowed = maxAllowed;
-  }
+    /* Set of course schedule details */
+    private Set<CourseScheduleDetailForm> courseScheduleDetail;
 
-  /**
-   * get Total Participants
-   * @return
-   */
-  public int getTotalParticipants() {
-      return totalParticipants;
-  }
+    /* Minimum number of participants */
+    private int minRequired;
 
-  /** set Total Participants */
-  public void setTotalParticipants(int totalParticipants) {
-      this.totalParticipants = totalParticipants;
-  }
+    /* Maximum number of participants */
+    private int maxAllowed;
+
+    /* Total Number of Participants currently enrolled */
+    private int totalParticipants;
+
+    /** get Course Schedule id */
+    public Long getId() {
+        return id;
+    }
+
+    /** set id */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /** get Course id */
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    /** set Course id */
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    /** get Course Name */
+    public String getCourseName() {
+        return courseName;
+    }
+
+    /** set Course Name */
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    /** get Instructor Id */
+    public Long getInstructorId() {
+        return instructorId;
+    }
+
+    /** set Instructor Id */
+    public void setInstructorId(Long instructorId) {
+        this.instructorId = instructorId;
+    }
+
+    /** get Instructor Name(LASTNAME, FIRSTNAME) */
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    /** set Instructor Name(LASTNAME, FIRSTNAME) */
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    /** get Venue Id */
+    public Long getVenueId() {
+        return venueId;
+    }
+
+    /** set Venue Id */
+    public void setVenueId(Long venueId) {
+        this.venueId = venueId;
+    }
+
+    /** get Venue Name */
+    public String getVenueName() {
+        return venueName;
+    }
+
+    /** set Venue Name */
+    public void setVenueName(String venueName) {
+        this.venueName = venueName;
+    }
+
+    /** get Set of course schedule details */
+    public Set<CourseScheduleDetailForm> getCourseScheduleDetails() {
+        return courseScheduleDetail;
+    }
+
+    /** set Set of course schedule details */
+    public void setCourseScheduleDetails(Set<CourseScheduleDetailForm> courseScheduleDetail) {
+        this.courseScheduleDetail = courseScheduleDetail;
+    }
+
+    /** get Minimum number of participants */
+    public int getMinRequired() {
+        return minRequired;
+    }
+
+    /** set Minimum number of participants */
+    public void setMinRequired(int minRequired) {
+        this.minRequired = minRequired;
+    }
+
+    /** get Maximum number of participants */
+    public int getMaxAllowed() {
+        return maxAllowed;
+    }
+
+    /** set Maximum number of participants */
+    public void setMaxAllowed(int maxAllowed) {
+        this.maxAllowed = maxAllowed;
+    }
+
+    /** get Total Number of Participants currently enrolled */
+    public int getTotalParticipants() {
+        return totalParticipants;
+    }
+
+    /** set Total Number of Participants currently enrolled */
+    public void setTotalParticipants(int totalParticipants) {
+        this.totalParticipants = totalParticipants;
+    }
+
 }
