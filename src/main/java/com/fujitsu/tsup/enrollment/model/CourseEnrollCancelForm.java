aa9f5dde87cc6fa2c2cd59dbfd203df41e91fa4c
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-06-24 09:59:54 +0800

    refs #120771 add Added CancelCourseScheduleForm.java
    Added CancelCourseScheduleForm.java is a JavaBean Doc and is used to
    cancel the Members' Registration on a specific course

---
+++ src/main/java/com/fujitsu/tsup/enrollment/model/CourseEnrollCancelForm.java
@@ -0,0 +1,167 @@
+package com.fujitsu.tsup.enrollment.model;
+//==================================================================================================
+//$Id:PR01$
+//Project Name :Training Sign Up
+//System Name  :Enroll Course
+//Class Name   :CancelCourseScheduleForm.java
+//
+//<<Modification History>>
+//Version | Date       | Updated By            | Content
+//--------+------------+-----------------------+---------------------------------------------------
+//0.02    | 06/24/2020 | WS) T.Oviedo          | New Creation
+//==================================================================================================
+/**
+* <pre>
+* JavaBean for CancelCourseScheduleForm.java
+* <pre>
+* 
+* @version 0.01
+* @author t.oviedo                          
+*/
+import java.util.Set;
+
+public class CourseEnrollCancelForm {
+    /* Course Schedule Id */
+    private Long id;
+
+    /* Course Id */
+    private Long courseId;
+
+    /* Course Name */
+    private String courseName;
+
+    /* Instructor Id */
+    private Long instructorId;
+
+    /* Instructor Name(LASTNAME, FIRSTNAME) */
+    private String instructorName;
+
+    /* Venue Id */
+    private Long venueId;
+
+    /* Venue Name */
+    private String venueName;
+
+    /* Set of course schedule details */
+    private Set<CourseScheduleDetailForm> courseScheduleDetail;
+
+    /* Minimum number of participants */
+    private int minRequired;
+
+    /* Maximum number of participants */
+    private int maxAllowed;
+
+    /* Total Number of Participants currently enrolled */
+    private int totalParticipants;
+
+    /** get Course Schedule id */
+    public Long getId() {
+        return id;
+    }
+
+    /** set id */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /** get Course id */
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    /** set Course id */
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    /** get Course Name */
+    public String getCourseName() {
+        return courseName;
+    }
+
+    /** set Course Name */
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    /** get Instructor Id */
+    public Long getInstructorId() {
+        return instructorId;
+    }
+
+    /** set Instructor Id */
+    public void setInstructorId(Long instructorId) {
+        this.instructorId = instructorId;
+    }
+
+    /** get Instructor Name(LASTNAME, FIRSTNAME) */
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    /** set Instructor Name(LASTNAME, FIRSTNAME) */
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    /** get Venue Id */
+    public Long getVenueId() {
+        return venueId;
+    }
+
+    /** set Venue Id */
+    public void setVenueId(Long venueId) {
+        this.venueId = venueId;
+    }
+
+    /** get Venue Name */
+    public String getVenueName() {
+        return venueName;
+    }
+
+    /** set Venue Name */
+    public void setVenueName(String venueName) {
+        this.venueName = venueName;
+    }
+
+    /** get Set of course schedule details */
+    public Set<CourseScheduleDetailForm> getCourseScheduleDetails() {
+        return courseScheduleDetail;
+    }
+
+    /** set Set of course schedule details */
+    public void setCourseScheduleDetails(Set<CourseScheduleDetailForm> courseScheduleDetail) {
+        this.courseScheduleDetail = courseScheduleDetail;
+    }
+
+    /** get Minimum number of participants */
+    public int getMinRequired() {
+        return minRequired;
+    }
+
+    /** set Minimum number of participants */
+    public void setMinRequired(int minRequired) {
+        this.minRequired = minRequired;
+    }
+
+    /** get Maximum number of participants */
+    public int getMaxAllowed() {
+        return maxAllowed;
+    }
+
+    /** set Maximum number of participants */
+    public void setMaxAllowed(int maxAllowed) {
+        this.maxAllowed = maxAllowed;
+    }
+
+    /** get Total Number of Participants currently enrolled */
+    public int getTotalParticipants() {
+        return totalParticipants;
+    }
+
+    /** set Total Number of Participants currently enrolled */
+    public void setTotalParticipants(int totalParticipants) {
+        this.totalParticipants = totalParticipants;
+    }
+
+}
