8e605c5c9b7595578fb90580524fad5ce56b6ba6
j.macabugao <j.macabugao@PHR9Z02634ML.g07.fujitsu.local>
2020-06-24 10:32:22 +0800

    Addition
    refs #120826 add Added ScheduleDao Interface
    
    Added a ScheduleDao Interface because it serves as the data access
    interface for schedule related database access.

---
+++ src/main/java/com/fujitsu/ph/tsup/scheduling/dao/ScheduleDao.java
@@ -0,0 +1,65 @@
+package com.fujitsu.ph.tsup.scheduling.dao;
+
+import java.time.ZonedDateTime;
+import java.util.Set;
+
+import com.fujitsu.ph.tsup.employee.management.model.EmployeeForm;
+import com.fujitsu.ph.tsup.scheduling.model.CourseForm;
+import com.fujitsu.ph.tsup.scheduling.domain.CourseSchedule;
+import com.fujitsu.ph.tsup.scheduling.model.VenueForm;
+//=======================================================
+//$Id: PR02$
+//Project Name: Training Sign Up
+//Class Name: ScheduleDao.java
+//
+//<<Modification History>>
+//Version | Date       | Updated by      | Content
+//--------+------------+-----------------+---------------
+//0.01    | 06/23/2020 | WS) J.Macabugao | New Creation
+//
+//
+//=======================================================
+
+/**
+* <pre>
+* The data access interface for schedule related database access
+* <pre>
+* @version 0.01
+* @author j.macabugao
+*
+*/
+public interface ScheduleDao {
+
+    /**
+     * Finds the scheduled courses starting from today onwards
+     * @param scheduledStartDate
+     * @param scheduledEndDateTime 
+     */
+	Set<CourseSchedule> findAllScheduledCourses(ZonedDateTime scheduledStartDateTime, 
+			ZonedDateTime scheduledEndDateTime );
+	
+	 /**
+     * Finds all courses
+     * 
+     */
+	Set<CourseForm> findAllCourses();
+	
+	 /**
+     * Finds all instructors
+     * 
+     */
+	Set<EmployeeForm>findAllInstructors();
+	
+	 /**
+     * Finds all venues
+     * 
+     */
+	Set<VenueForm> findAllVenues();
+	
+	 /**
+     * Saves the CourseSchedule and CourseScheduleDetail object
+     * @param courseSchedule
+     */
+	CourseSchedule saveCourseSchedule(CourseSchedule courseSchedule);
+	
+}
