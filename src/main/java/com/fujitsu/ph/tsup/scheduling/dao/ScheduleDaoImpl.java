48f8ef65faa832b7f9e5beb8255cb92bcd8f59c0
jc.jimenez <jc.jimenez@fujitsu.com>
2020-06-25 16:45:14 +0800

    Function Modification
    refs #121268 update Following ScheduleDaoImpl Addition, Updated
    saveCourseSchedule
    
    Updated saveCourseSchedule since the return type should be void, not
    CourseSchedule

--- src/main/java/com/fujitsu/ph/tsup/scheduling/dao/ScheduleDaoImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/scheduling/dao/ScheduleDaoImpl.java
@@ -1,5 +1,46 @@
 package com.fujitsu.ph.tsup.scheduling.dao;
 
-public class ScheduleDaoImpl {
+import java.time.ZonedDateTime;
+import java.util.Set;
+
+import com.fujitsu.ph.tsup.scheduling.domain.CourseSchedule;
+import com.fujitsu.ph.tsup.scheduling.model.CourseForm;
+import com.fujitsu.ph.tsup.scheduling.model.InstructorForm;
+import com.fujitsu.ph.tsup.scheduling.model.VenueForm;
+
+public class ScheduleDaoImpl implements ScheduleDao{
+    
+    
+
+    @Override
+    public Set<CourseSchedule> findAllScheduledCourses(ZonedDateTime scheduledStartDateTime,
+            ZonedDateTime scheduledEndDateTime) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<CourseForm> findAllCourses() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<InstructorForm> findAllInstructors() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<VenueForm> findAllVenues() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void saveCourseSchedule(CourseSchedule courseSchedule) {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
