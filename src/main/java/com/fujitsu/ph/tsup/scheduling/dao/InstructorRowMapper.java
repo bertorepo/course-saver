82c5f77d1038c958e5377c3120b406668bfd840e
j.macabugao <j.macabugao@PHR9Z02634ML.g07.fujitsu.local>
2020-06-26 20:40:26 +0800

    Function Modification
    refs #121520 update Following IntructorRowMapper Update, Added Javadoc
    Comments

--- src/main/java/com/fujitsu/ph/tsup/scheduling/dao/InstructorRowMapper.java
+++ src/main/java/com/fujitsu/ph/tsup/scheduling/dao/InstructorRowMapper.java
@@ -1,5 +1,25 @@
 package com.fujitsu.ph.tsup.scheduling.dao;
+//=======================================================
+//$Id: PR02$
+//Project Name: Training Sign Up
+//Class Name: CourseRowMapper.java
+//
+//<<Modification History>>
+//Version | Date       | Updated by      | Content
+//--------+------------+-----------------+---------------
+//0.01    | 06/26/2020 | WS) J.Macabugao  | New Creation
+//
+//
+//=======================================================
 
+/**
+* <pre>
+* Custom RowMapper Class for findAllInstructors()
+* <pre>
+* @version 0.01
+* @author j.macabugao
+*
+*/
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -7,16 +27,26 @@ import org.springframework.jdbc.core.RowMapper;
 
 import com.fujitsu.ph.tsup.scheduling.model.InstructorForm;
 public class InstructorRowMapper implements RowMapper<InstructorForm>{
-
+	
+	
+	/**
+     * <pre>
+     * Maps the Rows returned by ResultSet
+     * <pre>
+     * @param ResultSet rs
+     * @param int rowNum
+     * @throws SQLException
+     */
 	@Override
 	public InstructorForm mapRow(ResultSet rs, int rowNum) throws SQLException {
 		InstructorForm instructorForm = new InstructorForm();
-		
+
 		Long instructorId = rs.getLong("instructorId");
-		String instructorName = rs.getNString("instructorName");
-		instructorForm.setId(instructorId);
-		instructorForm.setName(instructorName);
+		String instructorLastName = rs.getString("instructorLastName");
+		String instructorFirstName = rs.getNString("instructorFirstName");
 		
+		instructorForm.setId(instructorId);
+		instructorForm.setName(instructorLastName + "," + instructorFirstName);
 		
 		return instructorForm;
 	}
