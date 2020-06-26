b72320d069902ae9c851e9e3bdc9f9cfdcd8a3ae
jc.jimenez <jc.jimenez@fujitsu.com>
2020-06-26 18:31:15 +0800

    Function Modification
    refs #121268 update Following ScheduleDaoImpl Update, Updated
    saveCourseSchedule()
    
    Updated saveCourseSchedule() since the added function Insert values to
    the system.

--- src/main/java/com/fujitsu/ph/tsup/scheduling/dao/ScheduleDaoImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/scheduling/dao/ScheduleDaoImpl.java
@@ -6,12 +6,14 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
 import com.fujitsu.ph.tsup.scheduling.domain.CourseSchedule;
+import com.fujitsu.ph.tsup.scheduling.domain.CourseScheduleDetail;
 import com.fujitsu.ph.tsup.scheduling.model.CourseForm;
 import com.fujitsu.ph.tsup.scheduling.model.InstructorForm;
 import com.fujitsu.ph.tsup.scheduling.model.VenueForm;
@@ -60,9 +62,38 @@ public class ScheduleDaoImpl implements ScheduleDao{
 
     @Override
     public void saveCourseSchedule(CourseSchedule courseSchedule) {
-        String courseScheduleSql = "INSERT INTO COURSE_SCHEDULE(ID, COURSE_ID, INSTRUCTOR_ID, VENUE_ID) "+
-                "VALUES (:id, :name)";
+        String courseScheduleSql = "INSERT INTO COURSE_SCHEDULE"
+                + "(ID, COURSE_ID, INSTRUCTOR_ID, VENUE_ID, MIN_REQUIRED, MAX_ALLOWED, STATUS) "+
+                "VALUES (:id, :course_id, :instructor_id, :venue_id, :min_required, :max_allowed, :status)";
         
+        SqlParameterSource namedParameters = new MapSqlParameterSource()
+                    .addValue("id", courseSchedule.getId())
+                    .addValue("course_id", courseSchedule.getCourseId())
+                    .addValue("instructor_id", courseSchedule.getInstructorId())
+                    .addValue("venue_id", courseSchedule.getVenueId())
+                    .addValue("min_required", courseSchedule.getMinRequired())
+                    .addValue("max_allowed", courseSchedule.getMaxAllowed())
+                    .addValue("status", courseSchedule.getStatus());
+        template.update(courseScheduleSql, namedParameters);
+        
+        String courseScheduleDetailSql = "INSERT INTO COURSE_SCHEDULE_DETAIL" + 
+                "(ID, COURSE_SCHEDULE_ID, SCHEDULED_START_DATETIME, SCHEDULED_END_DATETIME, DURATION)"+
+                "VALUES (:id, :course_schedule_id, :scheduled_start_datetime, :scheduled_end_datetime, "
+                + ":duration";
+        
+        Set<CourseScheduleDetail> courseScheduleDetail = courseSchedule.getCourseScheduleDetail();
+        
+        for (CourseScheduleDetail courseSchedDetail : courseScheduleDetail) {
+            SqlParameterSource courseSchedDetailParameters = new MapSqlParameterSource()
+                    .addValue("id", courseSchedDetail.getId())
+                    .addValue("course_schedule_id", courseSchedDetail.getCourseScheduleId())
+                    .addValue("scheduled_start_datetime", courseSchedDetail.getScheduledStartDateTime())
+                    .addValue("scheduled_end_datetime", courseSchedDetail.getScheduledEndDateTime())
+                    .addValue("duration", courseSchedDetail.getDuration());
+            template.update(courseScheduleDetailSql, courseSchedDetailParameters);
+        }
     }
+    
+    
 
 }
