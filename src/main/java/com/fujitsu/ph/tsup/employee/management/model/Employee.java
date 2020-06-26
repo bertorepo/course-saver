082ba61b08a79127f39f9382e3381e5f8cc85815
j.macabudbud <j.macabudbud@PHR9Z01428ML.g07.fujitsu.local>
2020-06-26 16:01:55 +0800

    refs #12193 update Web Configuration and Employee Authorization
    Updated the dependencies and Employee Authorization and Model to follow
    the SS document

--- src/main/java/com/fujitsu/ph/tsup/employee/management/model/Employee.java
+++ src/main/java/com/fujitsu/ph/tsup/employee/management/model/Employee.java
@@ -1,10 +1,5 @@
 package com.fujitsu.ph.tsup.employee.management.model;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 /**
  * <pre>
  * It is a JavaBean for Employee
@@ -26,96 +21,172 @@ import javax.validation.constraints.Size;
 //--------+------------+-----------------------+---------------------------------------------------
 //0.01    | 06/24/2020 | WS) J.Macabudbud            | New Creation
 public class Employee {
-	@NotNull 
-	private long id;
+	private Long id;
 	private String number;
-	@NotBlank(message = "First Name cannot be null")
 	private String firstName;
-	@NotBlank(message = "Last Name cannot be null")
 	private String lastName;
-	@NotBlank(message = "Employee Number cannot be null")
-	@NotBlank(message = "Email Address cannot be null")
-	@Email(message = "Email should be valid")
 	private String emailAddress;
-	@NotBlank(message = "Username cannot be null")
 	private String username;
+
 	/**
 	 * @return the id
 	 */
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(long id) {
-		this.id = id;
-	}
+
 	/**
 	 * @return the number
 	 */
 	public String getNumber() {
 		return number;
 	}
-	/**
-	 * @param number the number to set
-	 */
-	public void setNumber(String number) {
-		this.number = number;
-	}
+
 	/**
 	 * @return the firstName
 	 */
 	public String getFirstName() {
 		return firstName;
 	}
-	/**
-	 * @param firstName the firstName to set
-	 */
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+
 	/**
 	 * @return the lastName
 	 */
 	public String getLastName() {
 		return lastName;
 	}
-	/**
-	 * @param lastName the lastName to set
-	 */
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+
 	/**
 	 * @return the emailAddress
 	 */
 	public String getEmailAddress() {
 		return emailAddress;
 	}
-	/**
-	 * @param emailAddress the emailAddress to set
-	 */
-	public void setEmailAddress(String emailAddress) {
-		this.emailAddress = emailAddress;
-	}
+
 	/**
 	 * @return the username
 	 */
 	public String getUsername() {
 		return username;
 	}
+	
 	/**
-	 * @param username the username to set
+	 * The builder class of the Employee. 
+	 * @param builder
 	 */
-	public void setUsername(String username) {
-		this.username = username;
+	private Employee(Builder builder) {
+		this.id = builder.id;
+		this.number = builder.number;
+		this.firstName = builder.firstName;
+		this.lastName = builder.lastName;
+		this.emailAddress = builder.emailAddress;
+		this.username = builder.username;
+	}
+	
+	public static class Builder {
+		private Long id;
+		private String number;
+		private String firstName;
+		private String lastName;
+		private String emailAddress;
+		private String username;
+		
+		/**
+		 * The builder is a public static member class of Employee
+		 * @param id
+		 * @param number
+		 * @param firstName
+		 * @param lastName
+		 * @param emailAddress
+		 * @param username
+		 */
+		public Builder(Long id, String number, String firstName, String lastName, 
+						String emailAddress, String username) {
+			validateId(id);
+			validateNumber(number);
+			validateFirstName(firstName);
+			validateLastName(lastName);
+			validateEmailAddress(emailAddress);
+			validateUsername(username);
+			
+			this.id = id;
+			this.number = number;
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.emailAddress = emailAddress;
+			this.username = username;
+		}
+		
+		/**
+		 * Validates id
+		 * @param id
+		 * @throws IllegalArgumentException
+		 */
+		private void validateId(Long id) {
+			if(id == null || id.toString().isEmpty() || id == 0) {
+				throw new IllegalArgumentException("Id shoud not be empty");
+			}
+		}
+		
+		/**
+		 * Validates number
+		 * @param number
+		 * @throws IllegalArgumentException
+		 */
+		private void validateNumber(String number) {
+			if(number == null || number.isEmpty()) {
+				throw new IllegalArgumentException("Number shoud not be empty");
+			}
+		}
+		
+		/**
+		 * Validates First Name
+		 * @param firstName
+		 * @throws IllegalArgumentException
+		 */
+		private void validateFirstName(String firstName) {
+			if(firstName == null || firstName.isEmpty()) {
+				throw new IllegalArgumentException("First Name shoud not be empty");
+			}
+		}
+		
+		/**
+		 * Validates Last Name
+		 * @param lastName
+		 * @throws IllegalArgumentException
+		 */
+		private void validateLastName(String lastName) {
+			if(lastName == null || lastName.isEmpty()) {
+				throw new IllegalArgumentException("Last Name shoud not be empty");
+			}
+		}
+		
+		/**
+		 * Validates Email Address
+		 * @param emailAddress
+		 * @throws IllegalArgumentException
+		 */
+		private void validateEmailAddress(String emailAddress) {
+			if(emailAddress == null || emailAddress.isEmpty()) {
+				throw new IllegalArgumentException("Email Address shoud not be empty");
+			}
+		}
+		
+		/**
+		 * Validates Username
+		 * @param username
+		 * @throws IllegalArgumentException
+		 */
+		private void validateUsername(String username) {
+			if(username == null || username.isEmpty()) {
+				throw new IllegalArgumentException("Username shoud not be empty");
+			}
+		}
 	}
 	
 	@Override
 	public String toString() {
-		return "EmployeeForm [firstName=" + firstName + ", lastName="
-				+ lastName + ", employeeNumber=" + number + ", emailAddress=" + emailAddress + 
-				", username=" + username + "]";
+		return "EmployeeForm [firstName=" + firstName + ", lastName=" + lastName + ", employeeNumber=" + number
+				+ ", emailAddress=" + emailAddress + ", username=" + username + "]";
 	}
 }
