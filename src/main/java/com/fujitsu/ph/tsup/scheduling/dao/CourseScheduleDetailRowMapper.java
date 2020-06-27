64d27e602fc240743cd848465a44304c47d1b09f
jc.jimenez <jc.jimenez@fujitsu.com>
2020-06-27 09:26:55 +0800

    Function Modification
    refs #121531 update Following CourseScheduleDetailRowMapper Addition,
    Updated mapRow()

--- src/main/java/com/fujitsu/ph/tsup/scheduling/dao/CourseScheduleDetailRowMapper.java
+++ src/main/java/com/fujitsu/ph/tsup/scheduling/dao/CourseScheduleDetailRowMapper.java
@@ -9,31 +9,17 @@ import com.fujitsu.ph.tsup.scheduling.domain.CourseScheduleDetail;
 
 import org.springframework.jdbc.core.RowMapper;
 
-public class CourseScheduleDetailRowMapper implements RowMapper <CourseScheduleDetail>{
-    
-    
+public class CourseScheduleDetailRowMapper implements RowMapper <CourseScheduleDetailForm>{
     
     @Override
-    public  CourseScheduleDetail mapRow(ResultSet csd, int rowNum) throws SQLException {
-        
-        
-        CourseScheduleDetail courseScheduleDetail = new CourseScheduleDetail();
-        
-        
-        
-    
+    public  CourseScheduleDetailForm mapRow(ResultSet csd, int rowNum) throws SQLException {
+
         Long id = csd.getLong("id");
         Long courseScheduleId = csd.getLong("courseScheduleId");
         ZonedDateTime scheduledStartDateTime = csd.getZonedDateTime("scheduledStartDateTime");
         ZonedDateTime scheduledEndDateTime = csd.getZonedDateTime("scheduledStartDateTime");
         float duration = csd.getFloat("duration");
-        
-        courseScheduleDetail.setId(id);
-        courseScheduleDetail.setCourseScheduleId(courseScheduleId);
-        courseScheduleDetail.setScheduledStartDateTime(scheduledStartDateTime);
-        courseScheduleDetail.setScheduledEndDateTime(scheduledStartDateTime);
-        courseScheduleDetail.setDuration(duration);
-        
+
         return courseScheduleDetail;
 }
 }
