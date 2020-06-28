e38c0d5ab7e4b0353f4193d5464c193f45db0931
j.balanon <j.balanon@PHR9X00940ML.g07.fujitsu.local>
2020-06-28 15:49:53 +0800

    Function Modification
    refs #120992 update Following ScheduleDaoImpl update, updated
    findAllScheduledCourses()
    
    Updated findAllScheduledCourses() since the added function inserts the
    values to the database

--- src/main/java/com/fujitsu/ph/tsup/scheduling/dao/ScheduleDaoImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/scheduling/dao/ScheduleDaoImpl.java
@@ -68,13 +68,28 @@ public class ScheduleDaoImpl implements ScheduleDao {
     public Set<CourseSchedule> findAllScheduledCourses(ZonedDateTime scheduledStartDateTime,
             ZonedDateTime scheduledEndDateTime) {
 
-        String query = "SELECT * FROM COURSE_SCHEDULE, COURSE_SCHEDULE_DETAIL, COURSE, EMPLOYEE, VENUE"
-                + "WHERE COURSE_SCHEDULE_ID BETWEEN fromDateTime AND ToDateTime"
-                + "ORDER BY COURSE_SCHEDULED_ID AND SCHEDULED_START_DATE_TIME";
-        
-        List<CourseSchedule> courseScheduleList = template.query(query, new CourseScheduleRowMapper());
+        String query = "SELECT \r\n" + "CSCHED.ID AS ID,\r\n" + "CSCHED.COURSE_ID AS COURSE_ID,\r\n"
+                + "C.NAME AS COURSE_NAME,\r\n" + "CSCHED.INSTRUCTOR_ID AS INSTRUCTOR_ID,\r\n"
+                + "E.LAST_NAME AS INSTRUCTOR_LAST_NAME,\r\n" + "E.FIRST_NAME AS INSTRUCTOR_FIRST_NAME,\r\n"
+                + "CSCHED.VENUE_ID AS VENUE_ID,\r\n" + "V.NAME AS VENUE_NAME,\r\n"
+                + "CSCHED.MIN_REQUIRED AS MIN_REQUIRED,\r\n" + "CSCHED.MAX_REQUIRED AS MAX_REQUIRED,\r\n"
+                + "COUNT(CPART.PARTICIPANT_ID) AS TOTAL_PARTICIPANTS,\r\n" + "CSCHED.STATUS AS STATUS\r\n"
+                + "FROM COURSE_SCHEDULE AS CSCHED\r\n" + "INNER JOIN COURSE_SCHEDULE_DETAIL AS CSCHEDDET\r\n"
+                + "ON CSCHED.ID = CSCHEDDET.COURSE_SCHEDULE_ID\r\n" + "INNER JOIN COURSE AS C\r\n"
+                + "ON CSCHED.COURSE_ID = C.ID\r\n" + "INNER JOIN EMPLOYEE AS E\r\n"
+                + "ON CSCHED.INSTRUCTOR_ID = E.ID\r\n" + "INNER JOIN VENUE AS V\r\n" + "ON CSCHED.VENUE_ID = V.ID\r\n"
+                + "INNER JOIN COURSE_PARTICIPANT AS CPART\r\n" + "ON CSCHED.ID = CPART.COURSE_SCHEDULE_ID\r\n"
+                + "WHERE CSCHEDDET.SCHEDULED_START_DATETIME BETWEEN :fromDateTime AND :toDateTime\r\n"
+                + "ORDER BY CSCHED.ID AND CSCHEDDET.SCHEDULED_START_DATETIME ";
+
+        SqlParameterSource courseScheduleParameters = new MapSqlParameterSource()
+                .addValue("scheduledStartDateTime", scheduledStartDateTime)
+                .addValue("scheduledEndDateTime", scheduledEndDateTime);
+
+        List<CourseSchedule> courseScheduleList = template.query(query, courseScheduleParameters,
+                new CourseScheduleRowMapper());
         Set<CourseSchedule> courseSchedule = new HashSet<>(courseScheduleList);
-        
+
         return courseSchedule;
     }
 
