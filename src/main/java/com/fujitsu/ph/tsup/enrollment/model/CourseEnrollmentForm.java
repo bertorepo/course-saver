0c42298eb5b7ebc7be2d7f8277657c5068f69026
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-06-29 17:15:08 +0800

    Modification Details
    Updated JavaBeans because of Formatting Rules

---
+++ src/main/java/com/fujitsu/ph/tsup/enrollment/model/CourseEnrollmentForm.java
@@ -0,0 +1,119 @@
+package com.fujitsu.ph.tsup.enrollment.model;
+
+//==================================================================================================
+//$Id:PR01$
+//Project Name :Training Sign Up
+//System Name  :Enroll Course
+//Class Name   :CourseEnrollmentForm.java
+//
+//<<Modification History>>
+//Version | Date       | Updated By            | Content
+//--------+------------+-----------------------+---------------------------------------------------
+//0.01    | 06/23/2020 | WS) M. Rivera         | New Creation
+//==================================================================================================
+/**
+* <pre>
+* JavaBean for CourseEnrollmentForm
+* <pre>
+* 
+* @version 0.01
+* @author m.rivera                          
+*/
+
+import java.time.ZonedDateTime;
+import java.util.Set;
+
+public class CourseEnrollmentForm {
+    /* Course Participant ID */
+    private Long id;
+
+    /* Course Schedule ID */
+    private Long courseScheduleId;
+
+    /* Course Name */
+    private String courseName;
+
+    /* Instructor Name (LASTNAME, FIRSTNAME) */
+    private String instructorName;
+
+    /* Venue Name */
+    private String venueName;
+
+    /* Course Participant ID */
+    private Set<CourseScheduleDetailForm> courseScheduleDetails;
+
+    /* Course Participant ID */
+    private ZonedDateTime registrationDate;
+
+    /** Course Participant ID Getter */
+    public Long getId() {
+        return id;
+    }
+
+    /** Schedule ID Getter */
+    public Long getCourseScheduleId() {
+        return courseScheduleId;
+    }
+
+    /** Course Name Getter */
+    public String getCourseName() {
+        return courseName;
+    }
+
+    /** Instructor Name Getter */
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    /** Venue Name Getter */
+    public String getVenueName() {
+        return venueName;
+    }
+
+    /** Course Schedule Details Getter */
+    public Set<CourseScheduleDetailForm> getCourseScheduleDetails() {
+        return courseScheduleDetails;
+    }
+
+    /** Registration Date Getter */
+    public ZonedDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    /** Course Participant ID Setter */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /** Course Schedule ID Setter */
+    public void setCourseScheduleId(Long courseScheduleId) {
+        this.courseScheduleId = courseScheduleId;
+    }
+
+    /** Course Name Setter */
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+
+    }
+
+    /** Instructor ID Setter */
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+
+    }
+
+    /** Venue Name Setter */
+    public void setVenueName(String venueName) {
+        this.venueName = venueName;
+    }
+
+    /** CourseScheduleDetails Setter */
+    public void setCourseScheduleDetails(Set<CourseScheduleDetailForm> courseScheduleDetails) {
+        this.courseScheduleDetails = courseScheduleDetails;
+    }
+
+    /** Registration Date Setter */
+    public void setRegistrationDate(ZonedDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
---
+++ src/main/java/com/fujitsu/ph/tsup/enrollment/model/CourseScheduleDetailForm.java
@@ -0,0 +1,81 @@
+package com.fujitsu.ph.tsup.enrollment.model;
+
+//==================================================================================================
+//$Id:PR01$
+//Project Name :Training Sign Up
+//System Name  :Enroll Course
+//Class Name   :CourseScheduleDetailForm.java
+//
+//<<Modification History>>
+//Version | Date       | Updated By            | Content
+//--------+------------+-----------------------+---------------------------------------------------
+//0.01    | 06/25/2020 | WS) K.Freo            | New Creation
+//==================================================================================================
+/**
+* <pre>
+* JavaBean for CourseScheduleDetailForm
+* <pre>
+* 
+* @version 0.01
+* @author k.freo                   
+*/
+import java.time.ZonedDateTime;
+
+public class CourseScheduleDetailForm {
+    /* Course ID */
+    private Long id;
+
+    /* Course Schedule Start Date */
+    private ZonedDateTime scheduledStartDateTime;
+
+    /* Course Schedule End Date */
+    private ZonedDateTime scheduledEndDateTime;
+
+    /* Course Duration */
+    private float duration;
+
+    /** Course ID Getter */
+    public Long getId() {
+        return id;
+    }
+
+    /** Course ID Setter */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /** Course Schedule Start Date Getter */
+    public ZonedDateTime getScheduledStartDateTime() {
+        return scheduledStartDateTime;
+    }
+
+    /** Course Schedule Start Date Setter */
+    public void setScheduledStartDateTime(ZonedDateTime scheduledStartDateTime) {
+        this.scheduledStartDateTime = scheduledStartDateTime;
+    }
+
+    /** Course Schedule End Date Getter */
+    public ZonedDateTime getScheduledEndDateTime() {
+        return scheduledEndDateTime;
+    }
+
+    /** Course Schedule End Date Setter */
+    public void setScheduledEndDateTime(ZonedDateTime scheduledEndDateTime) {
+        this.scheduledEndDateTime = scheduledEndDateTime;
+    }
+
+    /** Course Duration Getter */
+    public float getDuration() {
+        return duration;
+    }
+
+    /** Course Duration Setter */
+    public void setDuration(float duration) {
+        this.duration = duration;
+    }
+    
+    public String toString() {
+        return "CourseScheduleDetailForm [id=" + id + ", scheduledStartDateTime=" + scheduledStartDateTime
+                + ", scheduledEndDateTime=" + scheduledEndDateTime + ", duration=" + duration + "]";
+    }
+}
