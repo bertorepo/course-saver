6325b72c3cc8ad00a0bb97ed2b5bafeb3cc7578d
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-06-29 14:24:02 +0800

    refs #121538 update Updated EnrollmentRowMapperCourseParticipant.java
    Added EnrollmentRowMapperCourseParticipant.java and
    EnrollmentRowMapperCourseSchedule.java because of Formatting Rules

--- src/main/java/com/fujitsu/ph/tsup/enrollment/dao/EnrollmentRowMapperCourseParticipant.java
+++ src/main/java/com/fujitsu/ph/tsup/enrollment/dao/EnrollmentRowMapperCourseParticipant.java
@@ -9,7 +9,7 @@ package com.fujitsu.ph.tsup.enrollment.dao;
 //<<Modification History>>
 //Version | Date       | Updated By            | Content
 //--------+------------+-----------------------+--------------------------------------------------
-//0.02    | 06/24/2020 | WS) M.Lumontad        | New Creation
+//0.01    | 06/26/2020 | WS) M.Lumontad        | New Creation
 //=================================================================================================
 /**
 * <pre>
