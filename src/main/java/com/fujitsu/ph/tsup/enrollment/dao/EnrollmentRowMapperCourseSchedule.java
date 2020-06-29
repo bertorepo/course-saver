e9f5eba475797ee8a67ed32a1f12bffa789e6834
j.yu <j.yu@PHR9X00946ML.g07.fujitsu.local>
2020-06-29 19:02:29 +0800

    refs #120775 updated Updated EnrollmentRowMapperCourseSchedule.java
    Added missing resultSets

--- src/main/java/com/fujitsu/ph/tsup/enrollment/dao/EnrollmentRowMapperCourseSchedule.java
+++ src/main/java/com/fujitsu/ph/tsup/enrollment/dao/EnrollmentRowMapperCourseSchedule.java
@@ -9,6 +9,7 @@ package com.fujitsu.ph.tsup.enrollment.dao;
 //Version | Date       | Updated By            | Content
 //--------+------------+-----------------------+--------------------------------------------------
 //0.01    | 06/26/2020 | WS) M.Lumontad        | New Creation
+//0.01    | 06/29/2020 | WS) J.Yu              | Update
 //=================================================================================================
 /**
 * <pre>
@@ -20,19 +21,46 @@ package com.fujitsu.ph.tsup.enrollment.dao;
 */
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.jdbc.core.RowMapper;
-
 import com.fujitsu.ph.tsup.enrollment.domain.CourseSchedule;
+import com.fujitsu.ph.tsup.enrollment.domain.CourseScheduleDetail;
 
 public class EnrollmentRowMapperCourseSchedule implements RowMapper<CourseSchedule> {
 
     @Override
-    public CourseSchedule mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Long id = rs.getLong("id");
-
-        CourseSchedule courseSchedule = new CourseSchedule.Builder(id).build();
-        return courseSchedule;
+    public CourseSchedule mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Long id = resultSet.getLong("ID");
+        Long courseId = resultSet.getLong("COURSE_ID");
+        String courseName = resultSet.getString("COURSE_NAME");
+        Long instructorId = resultSet.getLong("INSTRUCTOR_ID");
+        String instructorLastName = resultSet.getString("INSTRUCTOR_LAST_NAME");
+        String instructorFirstName = resultSet.getString("INSTRUCTOR_FIRST_NAME");
+        Long venueId = resultSet.getLong("VENUE_ID");
+        String venueName = resultSet.getString("VENUE_NAME");
+        int minRequired = resultSet.getInt("MIN_REQUIRED");
+        int maxAllowed = resultSet.getInt("MAX_ALLOWED");
+        int totalParticipants = resultSet.getInt("TOTAL_PARTICIPANTS");
+        char status = resultSet.getString("STATUS").charAt(0);
+        ZonedDateTime scheduledStartDateTime = ZonedDateTime.ofInstant(resultSet.getTimestamp("SCHEDULED_START_DATETIME").toInstant(),
+                ZoneId.of("UTC"));
+        ZonedDateTime scheduledEndDateTime = ZonedDateTime.ofInstant(resultSet.getTimestamp("SCHEDULED_END_DATETIME").toInstant(),
+                ZoneId.of("UTC"));
+        
+        CourseScheduleDetail courseScheduleDetail = 
+                new CourseScheduleDetail.Builder(id, scheduledStartDateTime, scheduledEndDateTime).build();
+        
+        Set<CourseScheduleDetail> courseScheduleDetailSet = new HashSet<>();
+        courseScheduleDetailSet.add(courseScheduleDetail);
+        
+        CourseSchedule courseSchedule = new CourseSchedule.Builder(id, courseId, courseName, instructorId, 
+                instructorLastName, instructorFirstName, venueId, venueName, minRequired, 
+                maxAllowed, totalParticipants, status).addDetail(courseScheduleDetailSet).build();
+       
+        return courseSchedule; 
     }
 
 }
