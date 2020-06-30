d22ed15119d7dd18c957d8e95567ea5e09fceac8
jm.deguzman <jm.deguzman@PHR9X00947ML.g07.fujitsu.local>
2020-06-30 19:16:36 +0800

    refs #121276 update Updated DashboardInstructorDaoImpl.java
    Updated DashboardInstructorDaoImpl.java, changed sql queries to all
    capital so that the queries will be accessible to other members'
    database

--- src/main/java/com/fujitsu/ph/tsup/dashboard/dao/DashboardInstructorDaoImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/dashboard/dao/DashboardInstructorDaoImpl.java
@@ -49,23 +49,23 @@ public class DashboardInstructorDaoImpl implements DashboardInstructorDao {
      */
     @Override
     public Set<DashboardInstructorForm> findCourses(Long employeeId) {
-        String sql = "SELECT c.name AS \"c.name\", " +
-                "csd.scheduled_start_datetime AS \"csd.scheduled_start_datetime\", " +
-                "csd.scheduled_end_datetime AS \"csd.scheduled_end_datetime\", " +
-                "v.name  AS \"v.name\", "+
-                "e.id AS \"e.id\", " +
-                "cs.status  AS \"cs.status\" " +
-                "FROM course_schedule cs " +
-                "INNER JOIN course c " +
-                "ON cs.course_id = c.id " +
-                "INNER JOIN course_schedule_detail csd " +
-                "ON cs.id = csd.course_schedule_id " +
-                "INNER JOIN venue v " +
-                "ON cs.venue_id = v.id " +
-                "INNER JOIN employee e " +
-                "ON cs.instructor_id = e.id " +
-                "WHERE cs.status= :status  " +
-                "AND e.id = :employeeId " +
+        String sql = "SELECT C.NAME AS \"C.NAME\", " +
+                "CSD.SCHEDULED_START_DATETIME AS \"CSD.SCHEDULED_START_DATETIME\", " +
+                "CSD.SCHEDULED_END_DATETIME AS \"CSD.SCHEDULED_END_DATETIME\", " +
+                "V.NAME AS \"V.NAME\", "+
+                "E.ID AS \"E.ID AS\", " +
+                "CS.STATUS  AS \"CS.STATUS\" " +
+                "FROM COURSE_SCHEDULE CS " +
+                "INNER JOIN COURSE C " +
+                "ON CS.COURSE_ID = C.ID " +
+                "INNER JOIN COURSE_SCHEDULE_DETAIL CSD " +
+                "ON CS.ID = CSD.COURSE_SCHEDULE_ID " +
+                "INNER JOIN VENUE V " +
+                "ON CS.VENUE_ID = V.ID " +
+                "INNER JOIN EMPLOYEE E " +
+                "ON CS.INSTRUCTOR_ID = E.ID " +
+                "WHERE CS.STATUS= :status  " +
+                "AND E.ID = :employeeId " +
                 "LIMIT 5";
         SqlParameterSource namedParameters = new MapSqlParameterSource()
                 .addValue("employeeId", employeeId)
@@ -84,14 +84,14 @@ public class DashboardInstructorDaoImpl implements DashboardInstructorDao {
      */
     @Override
     public int getCoursesToday(Long employeeId) {
-        String sql = "SELECT COUNT(course.name) " + 
-                "FROM course_schedule " + 
-                "INNER JOIN employee ON course_schedule.instructor_id = employee.id " + 
-                "INNER JOIN course ON course_schedule.course_id = course.id " + 
-                "INNER JOIN course_schedule_detail ON course_schedule.id = course_schedule_detail.course_schedule_id " + 
-                "WHERE DATE(course_schedule_detail.scheduled_start_datetime) = :startDateTime " + 
-                "AND employee.id = :employeeId " + 
-                "AND course_schedule.status = :status";
+        String sql = "SELECT COUNT(COURSE.NAME) " + 
+                "FROM COURSE_SCHEDULE " + 
+                "INNER JOIN EMPLOYEE ON COURSE_SCHEDULE.INSTRUCTOR_ID = EMPLOYEE.ID " + 
+                "INNER JOIN COURSE ON COURSE_SCHEDULE.COURSE_ID = COURSE.ID " + 
+                "INNER JOIN COURSE_SCHEDULE_DETAIL ON COURSE_SCHEDULE.ID = COURSE_SCHEDULE_DETAIL.COURSE_SCHEDULE_ID " + 
+                "WHERE DATE(COURSE_SCHEDULE_DETAIL.SCHEDULED_START_DATETIME) = :startDateTime " + 
+                "AND EMPLOYEE.ID = :employeeId " + 
+                "AND COURSE_SCHEDULE.STATUS = :status";
         SqlParameterSource namedParameters = new MapSqlParameterSource()
                 .addValue("startDateTime", LocalDate.now())
                 .addValue("employeeId", employeeId)
