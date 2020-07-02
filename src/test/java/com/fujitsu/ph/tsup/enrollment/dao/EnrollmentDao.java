4ba6d1152827a9c650be5cbf7df3fd66b9a0d769
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-07-02 11:38:30 +0800

    Function Modification
    refs #121430 update updated EnrollmentDao.java and
    EnrollmentService.java
    
    Updated EnrollmentDao.java and EnrollmentService.java under Test Package
    for JUnit Testing because of JavaDoc

--- src/test/java/com/fujitsu/ph/tsup/enrollment/dao/EnrollmentDao.java
+++ src/test/java/com/fujitsu/ph/tsup/enrollment/dao/EnrollmentDao.java
@@ -1,5 +1,25 @@
 package com.fujitsu.ph.tsup.enrollment.dao;
 
+//=================================================================================================
+//$Id:PR01$
+//Project Name :Training Sign Up
+//System Name  :Enroll Course
+//Class Name   :EnrollmentDao.java
+//
+//<<Modification History>>
+//Version | Date       | Updated By            | Content
+//--------+------------+-----------------------+--------------------------------------------------
+//0.01    | 07/02/2020 | WS) M.Lumontad        | New Creation
+//=================================================================================================
+/**
+* <pre>
+* EnrollmentDao.java under Test Package is used for JUnit Testing
+* <pre>
+* 
+* @version 0.01
+* @author m.lumontad                      
+*/
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
--- src/test/java/com/fujitsu/ph/tsup/enrollment/service/EnrollmentService.java
+++ src/test/java/com/fujitsu/ph/tsup/enrollment/service/EnrollmentService.java
@@ -1,5 +1,26 @@
 package com.fujitsu.ph.tsup.enrollment.service;
 
+
+//=================================================================================================
+//$Id:PR01$
+//Project Name :Training Sign Up
+//System Name  :Enroll Course
+//Class Name   :EnrollmentDao.java
+//
+//<<Modification History>>
+//Version | Date       | Updated By            | Content
+//--------+------------+-----------------------+--------------------------------------------------
+//0.01    | 07/02/2020 | WS) M.Lumontad        | New Creation
+//=================================================================================================
+/**
+* <pre>
+* EnrollmentService.java under Test Package is used for JUnit Testing
+* <pre>
+* 
+* @version 0.01
+* @author m.lumontad                      
+*/
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
