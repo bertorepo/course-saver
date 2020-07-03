13c9ad5b0a6b51f216b2affe47b7449f83d97d1e
jc.jimenez <jc.jimenez@fujitsu.com>
2020-07-03 14:19:09 +0800

    Function Modification
    refs #121993 update Following ScheduleServiceTest Update, Added Test
    Cases for Exceptions and Validations.

--- src/test/java/com/fujitsu/ph/tsup/scheduling/service/ScheduleServiceTest.java
+++ src/test/java/com/fujitsu/ph/tsup/scheduling/service/ScheduleServiceTest.java
@@ -1,10 +1,14 @@
 package com.fujitsu.ph.tsup.scheduling.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.doThrow;
 
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.HashSet;
 import java.util.Set;
@@ -50,9 +54,24 @@ public class ScheduleServiceTest {
         CourseSchedule courseSchedule = createViewCourseSchedule();
         Set<CourseSchedule> courseScheduleSet = new HashSet<>();
         courseScheduleSet.add(courseSchedule);
-        when(scheduleDao.findAllScheduledCourses(any(ZonedDateTime.class), any(ZonedDateTime.class))).thenReturn(courseScheduleSet);
-        Set<CourseSchedule> anotherCourseSchedule = scheduleDao.findAllScheduledCourses(ZonedDateTime.now(), ZonedDateTime.now().plusHours(5));
+        when(scheduleDao.findAllScheduledCourses(any(ZonedDateTime.class), any(ZonedDateTime.class)))
+                .thenReturn(courseScheduleSet);
+        Set<CourseSchedule> anotherCourseSchedule = 
+                scheduleDao.findAllScheduledCourses(ZonedDateTime.now(), ZonedDateTime.now().plusHours(5));
         assertEquals(courseScheduleSet.size(), anotherCourseSchedule.size());
+    }
+    
+    @Test
+    void testFindAllScheduledCourses_Error() {
+        when(scheduleDao.findAllScheduledCourses(any(ZonedDateTime.class), any(ZonedDateTime.class)))
+                .thenThrow(new DataRetrievalFailureException("error"));
+        
+        Exception courseScheduleException = assertThrows(IllegalArgumentException.class, () 
+                -> scheduleService.findAllScheduledCourses(ZonedDateTime.now(), ZonedDateTime.now().plusHours(5)));
+        
+        String expectedMessage = "Can't access fromDate and toDate.";
+        String actualMessage = courseScheduleException.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
     }
     
     @Test
@@ -66,7 +85,34 @@ public class ScheduleServiceTest {
         
         when(scheduleDao.findAllCourses()).thenReturn(courses);
         Set<CourseForm> service = scheduleService.findAllCourses();
+        assertNotNull(service);
         assertEquals(service.size(), courses.size());
+    }
+    
+    @Test
+    void testFindAllCourses_Error() {
+        when(scheduleDao.findAllCourses()).thenThrow(new DataRetrievalFailureException("error"));
+        
+        Exception courseScheduleException = assertThrows(IllegalArgumentException.class, () 
+                -> scheduleService.findAllCourses());
+        
+        String expectedMessage = "Can't access Courses";
+        String actualMessage = courseScheduleException.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+        
+        
+    }
+    
+    @Test
+    void testFindAllCourses_Null() {
+        when(scheduleDao.findAllCourses()).thenReturn(null);
+        
+        Exception courseScheduleException = assertThrows(IllegalArgumentException.class, () 
+                -> scheduleService.findAllCourses());
+        
+        String expectedMessage = "Can't find Courses";
+        String actualMessage = courseScheduleException.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage)); 
     }
     
     @Test
@@ -80,7 +126,34 @@ public class ScheduleServiceTest {
         
         when(scheduleDao.findAllInstructors()).thenReturn(instructors);
         Set<InstructorForm> service = scheduleService.findAllInstructors();
+        assertNotNull(service);
         assertEquals(service.size(), instructors.size());
+    }
+    
+    @Test
+    void testFindAllInstructors_Error() {
+        when(scheduleDao.findAllInstructors()).thenThrow(new DataRetrievalFailureException("error"));
+        
+        Exception courseScheduleException = assertThrows(IllegalArgumentException.class, () 
+                -> scheduleService.findAllInstructors());
+        
+        String expectedMessage = "Can't access Instructors";
+        String actualMessage = courseScheduleException.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+        
+        
+    }
+    
+    @Test
+    void testFindAllInstructors_Null() {
+        when(scheduleDao.findAllInstructors()).thenReturn(null);
+        
+        Exception courseScheduleException = assertThrows(IllegalArgumentException.class, () 
+                -> scheduleService.findAllInstructors());
+        
+        String expectedMessage = "Can't find Instructors";
+        String actualMessage = courseScheduleException.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage)); 
     }
     
     @Test
@@ -95,7 +168,34 @@ public class ScheduleServiceTest {
         when(scheduleDao.findAllVenues()).thenReturn(venues);
         
         Set<VenueForm> service = scheduleService.findAllVenues();
+        assertNotNull(service);
         assertEquals(service.size(), venues.size());
+    }
+    
+    @Test
+    void testFindAllVenues_Error() {
+        when(scheduleDao.findAllVenues()).thenThrow(new DataRetrievalFailureException("error"));
+        
+        Exception courseScheduleException = assertThrows(IllegalArgumentException.class, () 
+                -> scheduleService.findAllVenues());
+        
+        String expectedMessage = "Can't access Venues";
+        String actualMessage = courseScheduleException.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+        
+        
+    }
+    
+    @Test
+    void testFindAllVenues_Null() {
+        when(scheduleDao.findAllVenues()).thenReturn(null);
+        
+        Exception courseScheduleException = assertThrows(IllegalArgumentException.class, () 
+                -> scheduleService.findAllVenues());
+        
+        String expectedMessage = "Can't find Venues";
+        String actualMessage = courseScheduleException.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage)); 
     }
     
     @Test
@@ -111,6 +211,20 @@ public class ScheduleServiceTest {
         assertEquals(courseSchedule.getMinRequired(), 1);
         assertEquals(courseSchedule.getCourseScheduleDetail().size(), 1);
         assertEquals(courseSchedule.getMaxAllowed(), 10);
+    }
+    
+    @Test
+    void testCreateCourseSchedule_Error() {
+        doThrow(new DataRetrievalFailureException("error")).when(scheduleDao).saveCourseSchedule(any(CourseSchedule.class));
+        
+        CourseSchedule courseSchedule = createCourseSchedule();
+        
+        Exception courseScheduleException = assertThrows(IllegalArgumentException.class, () 
+                -> scheduleService.createCourseSchedule(courseSchedule));
+        
+        String expectedMessage = "Can't save course schedule";
+        String actualMessage = courseScheduleException.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
     }
     
     private CourseSchedule createCourseSchedule() {
