2eecceeaaedebc9977e40335719fa8ad667015e4
j.macabudbud <j.macabudbud@PHR9Z01428ML.g07.fujitsu.local>
2020-07-06 16:26:12 +0800

    refs #121293 update Upated Test Class
    Upated test class for the method of findByUsername since the user may
    have more than 1 role

--- src/test/java/com/fujitsu/ph/tsup/authorization/infrastructure/AuthorizationDaoTest.java
+++ src/test/java/com/fujitsu/ph/tsup/authorization/infrastructure/AuthorizationDaoTest.java
@@ -1,6 +1,9 @@
 package com.fujitsu.ph.tsup.authorization.infrastructure;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,13 +19,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.fujitsu.ph.tsup.authz.core.model.EmployeeAuth;
 import com.fujitsu.ph.tsup.authz.dao.AuthorizationDao;
 import com.fujitsu.ph.tsup.authz.dao.AuthorizationDaoImpl;
+import com.fujitsu.ph.tsup.common.domain.Auth;
 
 @RunWith(SpringRunner.class)
 @JdbcTest
 @ActiveProfiles({ "postgres-test" })
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public class AuthorizationDaoTest {
-
 	@Autowired
 	private AuthorizationDao dao;
 
@@ -37,10 +40,18 @@ public class AuthorizationDaoTest {
 	@Test
 	public void testFindByUsername() {
 		String username = "l.lorenzo";
-		EmployeeAuth result = dao.findByUsername(username);
-		System.out.println(result);
-		assertTrue(result.getUsername().equals(username));
-		assertTrue(result.getAuthzSet().contains("PMO"));
+		Set<EmployeeAuth> result = dao.findByUsername(username);
+		assertTrue(result.size() == 2);
+		for (EmployeeAuth value : result) {
+			assertEquals(value.getUsername(), username);
+			if (value.getAuthzSet().toString().equalsIgnoreCase("PMO")) {
+				assertEquals(value.getAuthzSet().toString(), Auth.PMO.toString());
+			} else if (value.getAuthzSet().toString().equalsIgnoreCase("INSTRUCTOR")) {
+				assertEquals(value.getAuthzSet().toString(), Auth.INSTRUCTOR.toString());
+			} else if (value.getAuthzSet().toString().equalsIgnoreCase("MEMBER")) {
+				assertEquals(value.getAuthzSet().toString(), Auth.MEMBER.toString());
+			}
+		}
 	}
 
 }
