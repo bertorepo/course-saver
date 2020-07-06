041ea66b5f49b4cbe9d23cebbf5930e902e3d6a6
k.freo <k.freo@PHR9Z02631ML.g07.fujitsu.local>
2020-07-06 19:08:23 +0800

    refs #122003 - update Updated PT_SpecificationandCertificate
    Update CourseParticipant Test modify formats for requirement purposes.

---
+++ src/test/java/com/fujitsu/ph/tsup/enrollment/domain/CourseParticipantTest.java
@@ -0,0 +1,14 @@
+package com.fujitsu.ph.tsup.enrollment.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CourseParticipantTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
