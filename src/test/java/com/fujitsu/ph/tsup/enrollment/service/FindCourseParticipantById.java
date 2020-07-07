79bc4d7789bf0431de51f2d210c2a9a3f29f82c8
m.lumontad <m.lumontad@PHR9X00951ML.g07.fujitsu.local>
2020-07-07 17:42:37 +0800

    refs #121430 update Updated FindCourseParticipantById.java
    Updated FindCourseParticipantById.java on Test Package for JUnit Testing

--- src/test/java/com/fujitsu/ph/tsup/enrollment/service/FindCourseParticipantById.java
+++ src/test/java/com/fujitsu/ph/tsup/enrollment/service/FindCourseParticipantById.java
@@ -10,6 +10,7 @@ package com.fujitsu.ph.tsup.enrollment.service;
 //Version | Date       | Updated By            | Content
 //--------+------------+-----------------------+---------------------------------------------------
 //0.01    | 06/23/2020 | WS) M. Lumontad       | New Creation
+//0.01    | 07/07/2020 | WS) M. Lumontad       | Update
 //==================================================================================================
 /**
 * <pre>
@@ -21,7 +22,6 @@ package com.fujitsu.ph.tsup.enrollment.service;
 */
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -36,29 +36,95 @@ import com.fujitsu.ph.tsup.enrollment.domain.CourseParticipant;
 
 @ExtendWith(SpringExtension.class)
 class FindCourseParticipantById {
+
+    /**
+     * Used to define Bean
+     */
     @TestConfiguration
     static class CourseServiceImplestContextConfiguration {
+
+        /**
+         * Test Specific Bean
+         */
         @Bean
         EnrollmentService enrollmentService() {
             return new EnrollmentServiceImpl();
         }
     }
 
+    /**
+     * Test Specific Bean
+     */
     @Autowired
     private EnrollmentService service;
+
+    /**
+     * Allows to mock classes or interface,
+     * to record or verify behaviors
+     */
     @MockBean
     private EnrollmentDao dao;
 
+    /**
+     * JUnit Assertion without Errors in Mock Bean data and Test Specific Data 
+     */
     @Test
     void findCourseParticipantById() {
         CourseParticipant result = findParticipantId();
-        when(dao.findCourseParticipantById(any(Long.class))).thenReturn(result);
+        when(dao.findCourseParticipantById(10L)).thenReturn(result);
 
-        CourseParticipant courseParticipant = service.findCourseParticipantById(1L);
+        CourseParticipant courseParticipant = service.findCourseParticipantById(10L);
         assertEquals(result.getParticipantId(), courseParticipant.getParticipantId());
     }
 
+
+    /**
+     * JUnit Assertion with Errors in Mock Bean data and Test Specific Data 
+     */
+    @Test
+    void findCourseParticipantById_Error() {
+
+        CourseParticipant result = findParticipantId();
+        when(dao.findCourseParticipantById(20L)).thenReturn(result);
+
+        CourseParticipant courseParticipant = service.findCourseParticipantById(10L);
+        assertEquals(result.getParticipantId(), courseParticipant.getParticipantId());
+
+    }
+
+    /**
+     * JUnit Assertion with Errors in Mock Bean data and Test Specific Data
+     * the return value is null 
+     */
+    @Test
+    void findCourseParticipantById_ReturnNull() {
+
+        CourseParticipant result = findParticipantId();
+        when(dao.findCourseParticipantById(10L)).thenReturn(null);
+
+        CourseParticipant courseParticipant = service.findCourseParticipantById(10L);
+        assertEquals(result.getParticipantId(), courseParticipant.getParticipantId());
+
+    }
+
+    
+    /**
+     * JUnit Assertion with Errors in Mock Bean data and Test Specific Data
+     * the Mock Bean Value for Participant ID is null 
+     */
+ 
+    @Test
+    void findCourseParticipantById_DaoNull() {
+
+        CourseParticipant result = findParticipantId();
+        when(dao.findCourseParticipantById(null)).thenReturn(result);
+
+        CourseParticipant courseParticipant = service.findCourseParticipantById(10L);
+        assertEquals(result.getParticipantId(), courseParticipant.getParticipantId());
+
+    }
+
     private CourseParticipant findParticipantId() {
-        return new CourseParticipant.Builder(10L,1L).build();
+        return new CourseParticipant.Builder(10L, 10L).build();
     }
 }
