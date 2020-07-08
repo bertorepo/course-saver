cf510f132a2b5d728fb309fa428d7cb41b183de7
r.ramos <r.ramos@PHR9X00949ML.g07.fujitsu.local>
2020-07-08 17:25:35 +0800

    refs #120881 - update Updated AttendanceServiceImpl.java
    Update changeStatus method to be able to test properly

--- src/main/java/com/fujitsu/ph/tsup/attendance/service/AttendanceServiceImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/attendance/service/AttendanceServiceImpl.java
@@ -28,6 +28,7 @@ import org.springframework.stereotype.Service;
 //0.02    | 07/03/2020 | WS) K.abad, WS) R.Ramos                                 | Update
 //0.03    | 07/07/2020 | WS) J.iwarat                                            | Update
 //0.04    | 07/08/2020 | WS) J.iwarat                                            | Update
+//0.05    | 07/08/2020 | WS) R.ramos                                             | Update
 //==================================================================================================
 /**
  * <pre>
@@ -35,7 +36,7 @@ import org.springframework.stereotype.Service;
  * In this class, it implements the AttendanceService class for the initial setting of the database
  * </pre>
  * 
- * @version 0.04
+ * @version 0.05
  * @author k.abad
  * @author m.angara
  * @author j.iwarat
@@ -46,203 +47,205 @@ import org.springframework.stereotype.Service;
 @Service
 public class AttendanceServiceImpl implements AttendanceService {
 
-    /*
-     * AttendanceDao
-     */
-    @Autowired
-    private AttendanceDao attendanceDao;
+	/*
+	 * AttendanceDao
+	 */
+	@Autowired
+	private AttendanceDao attendanceDao;
 
-    /**
-     * <pre>
-     * Finds all scheduled courses based on the given date range Call
-     * attendanceDao.findAllScheduledCourses using the given fromDateTime,
-     * toDateTime, instructor id and return the result
-     * 
-     * <pre>
-     * 
-     * @param fromDateTime
-     * @param toDateTime
-     * @param instructorId
-     * @return CourseScheduleSet
-     */
+	/**
+	 * <pre>
+	 * Finds all scheduled courses based on the given date range Call
+	 * attendanceDao.findAllScheduledCourses using the given fromDateTime,
+	 * toDateTime, instructor id and return the result
+	 * 
+	 * <pre>
+	 * 
+	 * @param fromDateTime
+	 * @param toDateTime
+	 * @param instructorId
+	 * @return CourseScheduleSet
+	 */
 
-    @Override
-    public Set<CourseSchedule> findAllScheduledCoursesByInstructor(ZonedDateTime fromDateTime, ZonedDateTime toDateTime,
-            Long instructorId) {
-        try {
-            return attendanceDao.findAllScheduledCourses(fromDateTime, toDateTime, instructorId);
-        } catch (DataAccessException e) {
-            throw new IllegalArgumentException("Can't find from date time, to date time and instructor Id.", e);
-        }
+	@Override
+	public Set<CourseSchedule> findAllScheduledCoursesByInstructor(ZonedDateTime fromDateTime, ZonedDateTime toDateTime,
+			Long instructorId) {
+		try {
+			return attendanceDao.findAllScheduledCourses(fromDateTime, toDateTime, instructorId);
+		} catch (DataAccessException e) {
+			throw new IllegalArgumentException("Can't find from date time, to date time and instructor Id.", e);
+		}
 
-    }
+	}
 
-    /**
-     * <pre>
-     * Finds course schedule based on the given course id Call
-     * attendanceDao.findCourseScheduleById using the given course id and return the
-     * result
-     * 
-     * <pre>
-     * 
-     * @param id
-     * @return CourseParticipant Set
-     */
-    @Override
-    public Set<CourseParticipant> findCourseScheduleById(Long id) {
-        try {
-            return attendanceDao.findCourseScheduleById(id);
-        } catch (DataAccessException e) {
-            throw new IllegalArgumentException("Course Schedule not found.", e);
-        }
-    }
+	/**
+	 * <pre>
+	 * Finds course schedule based on the given course id Call
+	 * attendanceDao.findCourseScheduleById using the given course id and return the
+	 * result
+	 * 
+	 * <pre>
+	 * 
+	 * @param id
+	 * @return CourseParticipant Set
+	 */
+	@Override
+	public Set<CourseParticipant> findCourseScheduleById(Long id) {
+		try {
+			return attendanceDao.findCourseScheduleById(id);
+		} catch (DataAccessException e) {
+			throw new IllegalArgumentException("Course Schedule not found.", e);
+		}
+	}
 
-    /**
-     * <pre>
-     * Finds the course schedule and the participants and those who are attending
-     * using the course schedule Id Get those you signed up Call the
-     * attendanceDao.findCourseScheduleDetailParticipantsById using the given id and
-     * return the result Get those you logged-in Call the
-     * attendanceDao.findCourseAttendanceByCourseScheduleDetailId using the given id
-     * and return the result Merge the the results of the previous steps. Ensure
-     * that there are no duplicate participants. If there are duplicate
-     * participants, retain the record from Step 2.1 Return the result of the merge
-     * course attendance set.
-     * 
-     * <pre>
-     * 
-     * @param id
-     * @return CourseAttendance Set
-     */
+	/**
+	 * <pre>
+	 * Finds the course schedule and the participants and those who are attending
+	 * using the course schedule Id Get those you signed up Call the
+	 * attendanceDao.findCourseScheduleDetailParticipantsById using the given id and
+	 * return the result Get those you logged-in Call the
+	 * attendanceDao.findCourseAttendanceByCourseScheduleDetailId using the given id
+	 * and return the result Merge the the results of the previous steps. Ensure
+	 * that there are no duplicate participants. If there are duplicate
+	 * participants, retain the record from Step 2.1 Return the result of the merge
+	 * course attendance set.
+	 * 
+	 * <pre>
+	 * 
+	 * @param id
+	 * @return CourseAttendance Set
+	 */
 
-    @Override
-    public Set<CourseAttendance> findCourseAttendanceByCourseScheduleDetailId(Long id) {
-        try {
-            Set<CourseAttendance> signedUp = attendanceDao.findCourseScheduleDetailParticipantsById(id);
-            Set<CourseAttendance> loggedIn = attendanceDao.findCourseAttendanceByCourseScheduleDetailId(id);
-            Map<Long, CourseAttendance> signUpAndLoggedIn = new HashMap<>();
+	@Override
+	public Set<CourseAttendance> findCourseAttendanceByCourseScheduleDetailId(Long id) {
+		try {
+			Set<CourseAttendance> signedUp = attendanceDao.findCourseScheduleDetailParticipantsById(id);
+			Set<CourseAttendance> loggedIn = attendanceDao.findCourseAttendanceByCourseScheduleDetailId(id);
+			Map<Long, CourseAttendance> signUpAndLoggedIn = new HashMap<>();
 
-            for (CourseAttendance p : signedUp) {
-                signUpAndLoggedIn.put(p.getParticipantId(), p);
-            }
+			for (CourseAttendance p : signedUp) {
+				signUpAndLoggedIn.put(p.getParticipantId(), p);
+			}
 
-            for (CourseAttendance l : loggedIn) {
-                if (!signUpAndLoggedIn.containsKey(l.getParticipantId())) {
-                    signUpAndLoggedIn.put(l.getParticipantId(), l);
-                }
-            }
+			for (CourseAttendance l : loggedIn) {
+				if (!signUpAndLoggedIn.containsKey(l.getParticipantId())) {
+					signUpAndLoggedIn.put(l.getParticipantId(), l);
+				}
+			}
 
-            return new HashSet<>(signUpAndLoggedIn.values());
-        } catch (DataAccessException e) {
-            throw new IllegalArgumentException("No record found.", e);
-        }
-    }
+			return new HashSet<>(signUpAndLoggedIn.values());
+		} catch (DataAccessException e) {
+			throw new IllegalArgumentException("No record found.", e);
+		}
+	}
 
-    /**
-     * <pre>
-     * Finds the course schedule and the participants and those who are attending
-     * using the course schedule Id For each courseAttendance in the Set if
-     * courseAttendance.id = null, Call attendanceDao.saveAttendance using the
-     * courseAttendance otherwise, Call attendanceDao.updateAttendance using the
-     * courseAttendance
-     * 
-     * <pre>
-     * 
-     * @param courseAttendanceSet
-     */
-    @Override
-    public void changeStatus(Set<CourseAttendance> courseAttendanceSet) {
-        try {
-            CourseAttendance courseAttendance = (CourseAttendance) courseAttendanceSet;
-            attendanceDao.saveAttendance(courseAttendance);
-            if (((CourseAttendance) courseAttendanceSet).getId() == null) {
-                throw new IllegalArgumentException("Id not found.");
-            }
-            attendanceDao.updateAttendance(courseAttendance);
-        } catch (DataAccessException e) {
-            throw new IllegalArgumentException("No record found.", e);
-        }
+	/**
+	 * <pre>
+	 * Finds the course schedule and the participants and those who are attending
+	 * using the course schedule Id For each courseAttendance in the Set if
+	 * courseAttendance.id = null, Call attendanceDao.saveAttendance using the
+	 * courseAttendance otherwise, Call attendanceDao.updateAttendance using the
+	 * courseAttendance
+	 * 
+	 * <pre>
+	 * 
+	 * @param courseAttendanceSet
+	 */
+	@Override
+	public void changeStatus(Set<CourseAttendance> courseAttendanceSet) {
+		try {
+			for (CourseAttendance courseAttendance : courseAttendanceSet) {
 
-    }
+				if (courseAttendance.getId() == null) {
+					attendanceDao.saveAttendance(courseAttendance);
+				} else {
+					attendanceDao.updateAttendance(courseAttendance);
+				}
+			}
+		} catch (DataAccessException e) {
+			throw new IllegalArgumentException("Cannot insert/update data", e);
+		}
 
-    /**
-     * <pre>
-     * Finds Course absent participant Call
-     * attendanceDao.findCourseScheduleDetailParticipantsById and return the result
-     * Call attendanceDao.findCourseAttendanceByCourseScheduleDetailId and return
-     * the result Merged the results and get only the participants that have
-     * signed-up but didn't logged-in
-     * 
-     * <pre>
-     * 
-     * @param id
-     * @return CourseAttendance Set
-     */
-    @Override
-    public Set<CourseAttendance> findCourseAbsentParticipantByCourseScheduleDetailId(Long id) {
-        try {
-            Map<Long, CourseAttendance> absentMap = new HashMap<>();
-            Set<CourseAttendance> signedUp = attendanceDao.findCourseScheduleDetailParticipantsById(id);
-            Set<CourseAttendance> loggedIn = attendanceDao.findCourseAttendanceByCourseScheduleDetailId(id);
+	}
 
-            for (CourseAttendance a : signedUp) {
-                absentMap.put(a.getParticipantId(), a);
-            }
+	/**
+	 * <pre>
+	 * Finds Course absent participant Call
+	 * attendanceDao.findCourseScheduleDetailParticipantsById and return the result
+	 * Call attendanceDao.findCourseAttendanceByCourseScheduleDetailId and return
+	 * the result Merged the results and get only the participants that have
+	 * signed-up but didn't logged-in
+	 * 
+	 * <pre>
+	 * 
+	 * @param id
+	 * @return CourseAttendance Set
+	 */
+	@Override
+	public Set<CourseAttendance> findCourseAbsentParticipantByCourseScheduleDetailId(Long id) {
+		try {
+			Map<Long, CourseAttendance> absentMap = new HashMap<>();
+			Set<CourseAttendance> signedUp = attendanceDao.findCourseScheduleDetailParticipantsById(id);
+			Set<CourseAttendance> loggedIn = attendanceDao.findCourseAttendanceByCourseScheduleDetailId(id);
 
-            for (CourseAttendance l : loggedIn) {
-                absentMap.remove(l.getParticipantId());
-            }
-            Set<CourseAttendance> valueSet = new HashSet<>(absentMap.values());
-            return valueSet;
-        } catch (DataAccessException e) {
-            throw new IllegalArgumentException("No record found.", e);
-        }
-    }
+			for (CourseAttendance a : signedUp) {
+				absentMap.put(a.getParticipantId(), a);
+			}
 
-    /**
-     * <pre>
-     * Finds the course schedule and the participants and those who are attending
-     * using the course schedule detail Id Call the
-     * attendanceDao.findCourseScheduleDetailById using the given id and return the
-     * result
-     * 
-     * <pre>
-     * 
-     * @param id
-     * @return courseAttendance
-     */
-    @Override
-    public Set<CourseAttendance> findCourseScheduleDetailById(Long id) {
-        try {
-            Set<CourseAttendance> courseAttendance = attendanceDao.findCourseScheduleDetailById(id);
-            if (((CourseAttendance) courseAttendance).getId() == 0)
-                ;
-            return courseAttendance;
-        } catch (DataAccessException e) {
-            throw new IllegalArgumentException("No records found.", e);
-        }
+			for (CourseAttendance l : loggedIn) {
+				absentMap.remove(l.getParticipantId());
+			}
+			Set<CourseAttendance> valueSet = new HashSet<>(absentMap.values());
+			return valueSet;
+		} catch (DataAccessException e) {
+			throw new IllegalArgumentException("No record found.", e);
+		}
+	}
 
-    }
+	/**
+	 * <pre>
+	 * Finds the course schedule and the participants and those who are attending
+	 * using the course schedule detail Id Call the
+	 * attendanceDao.findCourseScheduleDetailById using the given id and return the
+	 * result
+	 * 
+	 * <pre>
+	 * 
+	 * @param id
+	 * @return courseAttendance
+	 */
+	@Override
+	public Set<CourseAttendance> findCourseScheduleDetailById(Long id) {
+		try {
+			Set<CourseAttendance> courseAttendance = attendanceDao.findCourseScheduleDetailById(id);
+			if (((CourseAttendance) courseAttendance).getId() == 0)
+				;
+			return courseAttendance;
+		} catch (DataAccessException e) {
+			throw new IllegalArgumentException("No records found.", e);
+		}
 
-    /**
-     * <pre>
-     * Finds the course schedule and the participants and those who are attending
-     * using the course schedule detail Id Call the attendanceDao.saveAttendance
-     * using the courseAttendance
-     * 
-     * <pre>
-     * 
-     * @param courseAttendance
-     */
+	}
 
-    @Override
-    public void attend(CourseAttendance courseAttendance) {
-        try {
-            attendanceDao.saveAttendance(courseAttendance);
-            if (courseAttendance.getId() == null)
-                ;
-        } catch (DataAccessException e) {
-            throw new IllegalArgumentException("No records found.", e);
-        }
-    }
+	/**
+	 * <pre>
+	 * Finds the course schedule and the participants and those who are attending
+	 * using the course schedule detail Id Call the attendanceDao.saveAttendance
+	 * using the courseAttendance
+	 * 
+	 * <pre>
+	 * 
+	 * @param courseAttendance
+	 */
+
+	@Override
+	public void attend(CourseAttendance courseAttendance) {
+		try {
+			attendanceDao.saveAttendance(courseAttendance);
+			if (courseAttendance.getId() == null)
+				;
+		} catch (DataAccessException e) {
+			throw new IllegalArgumentException("No records found.", e);
+		}
+	}
 }
