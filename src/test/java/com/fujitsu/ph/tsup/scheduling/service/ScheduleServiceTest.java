74f7865c16096b681863f4ea8274b43868bd0e65
j.macabugao <j.macabugao@PHR9Z02634ML.g07.fujitsu.local>
2020-07-08 11:29:33 +0800

    Function Modification
    refs #122197 update Following CourseScheduleTest, ScheduleDaoTest,
    ScheduleServiceTest Update, Added javadoc comments.

--- src/test/java/com/fujitsu/ph/tsup/scheduling/service/ScheduleServiceTest.java
+++ src/test/java/com/fujitsu/ph/tsup/scheduling/service/ScheduleServiceTest.java
@@ -1,5 +1,24 @@
 package com.fujitsu.ph.tsup.scheduling.service;
+/**
+* <pre>
+* The Unit Testing of schedule service
+* <pre>
+* @version 0.01
+* @author jc.jimenez
+*/
 
+//=======================================================
+//$Id: PR02$
+//Project Name: Training Sign Up
+//Class Name: ScheduleServiceTest.java
+//
+//<<Modification History>>
+//Version | Date       | Updated by      | Content
+//--------+------------+-----------------+---------------
+//0.01    | 07/02/2020 | WS) JC. Jimenez | New Creation
+//
+//
+//=======================================================
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -34,19 +53,37 @@ public class ScheduleServiceTest {
     @TestConfiguration
     static class TestContextConfiguration {
         
+    	/**
+    	 * ScheduleService
+    	 * @return ScheduleServiceImpl
+    	 */
         @Bean
         public ScheduleService scheduleService() {
             return new ScheduleServiceImpl();
         }
         
     }
-    
+	
+	/**
+     * ScheduleService as dependency
+     */
     @Autowired
     private ScheduleService scheduleService;
     
+	
+	/**
+     * ScheduleDao as mock object
+     */
     @MockBean
     private ScheduleDao scheduleDao;
     
+	
+    /**
+     * <pre>
+     * testFindAllScheduledCourses with Valid Values
+     * Call scheduleDao.findAllSchedledCourses and test if scheduled courses is return
+     * <pre>
+     */
     @Test
     void testFindAllScheduledCourses() {
         CourseSchedule courseSchedule = createViewCourseSchedule();
@@ -59,6 +96,13 @@ public class ScheduleServiceTest {
         assertEquals(courseScheduleSet.size(), anotherCourseSchedule.size());
     }
     
+    
+    /**
+     * <pre>
+     * testFindAllScheduledCourses with Error Message
+     * Call scheduleDao.findAllSchedledCourses and test if an error message is return
+     * <pre>
+     */
     @Test
     void testFindAllScheduledCourses_Error() {
         when(scheduleDao.findAllScheduledCourses(any(ZonedDateTime.class), any(ZonedDateTime.class)))
@@ -72,6 +116,12 @@ public class ScheduleServiceTest {
         assertTrue(actualMessage.contains(expectedMessage));
     }
     
+    /**
+     * <pre>
+     * testFindAllCourses with Valid Values
+     * Call scheduleDao.findAllCourses and test if courses is return
+     * <pre>
+     */
     @Test
     void testFindAllCourses() {
         CourseForm courseForm = new CourseForm();
@@ -87,6 +137,13 @@ public class ScheduleServiceTest {
         assertEquals(service.size(), courses.size());
     }
     
+    
+    /**
+     * <pre>
+     * testFindAllCourses with Error Message 
+     * Call scheduleDao.findAllCourses and test if an error message is return
+     * <pre>
+     */
     @Test
     void testFindAllCourses_Error() {
         when(scheduleDao.findAllCourses()).thenThrow(new DataRetrievalFailureException("error"));
@@ -101,6 +158,13 @@ public class ScheduleServiceTest {
         
     }
     
+    
+    /**
+     * <pre>
+     * testFindAllCourses with Null Value
+     * Call scheduleDao.findAllCourses and test if an error message is retur
+     * <pre>
+     */
     @Test
     void testFindAllCourses_Null() {
         when(scheduleDao.findAllCourses()).thenReturn(null);
@@ -113,6 +177,13 @@ public class ScheduleServiceTest {
         assertTrue(actualMessage.contains(expectedMessage)); 
     }
     
+    
+    /**
+     * <pre>
+     * testFindAllInstructors with Valid Values
+     * Call scheduleDao.findAllInstructors and test if instructors is return
+     * <pre>
+     */
     @Test
     void testFindAllInstructors() {
         InstructorForm instructorForm = new InstructorForm();
@@ -128,6 +199,12 @@ public class ScheduleServiceTest {
         assertEquals(service.size(), instructors.size());
     }
     
+    /**
+     * <pre>
+     * testFindAllInstructors with Error Message
+     * Call scheduleDao.findAllInstructors and test if an error message is return
+     * <pre>
+     */
     @Test
     void testFindAllInstructors_Error() {
         when(scheduleDao.findAllInstructors()).thenThrow(new DataRetrievalFailureException("error"));
@@ -142,6 +219,12 @@ public class ScheduleServiceTest {
         
     }
     
+    /**
+     * <pre>
+     * testFindAllInstructors with Null Value
+     * Call scheduleDao.findAllInstructors and test if an error message is return
+     * <pre>
+     */
     @Test
     void testFindAllInstructors_Null() {
         when(scheduleDao.findAllInstructors()).thenReturn(null);
@@ -154,6 +237,12 @@ public class ScheduleServiceTest {
         assertTrue(actualMessage.contains(expectedMessage)); 
     }
     
+    /**
+     * <pre>
+     * testFindAllVenues with Valid Values
+     * Call scheduleDao.findAllVenues and test if values is return
+     * <pre>
+     */
     @Test
     void testFindAllVenues() {
         VenueForm venueForm = new VenueForm();
@@ -170,6 +259,12 @@ public class ScheduleServiceTest {
         assertEquals(service.size(), venues.size());
     }
     
+    /**
+     * <pre>
+     * testFindAllVenues with Error Message
+     * Call scheduleDao.findAllVenues and test if an error message is return
+     * <pre>
+     */
     @Test
     void testFindAllVenues_Error() {
         when(scheduleDao.findAllVenues()).thenThrow(new DataRetrievalFailureException("error"));
@@ -184,6 +279,12 @@ public class ScheduleServiceTest {
         
     }
     
+    /**
+     * <pre>
+     * testFindAllVenues with Null Value
+     * Call scheduleDao.findAllVenues and test if an error message is return
+     * <pre>
+     */
     @Test
     void testFindAllVenues_Null() {
         when(scheduleDao.findAllVenues()).thenReturn(null);
@@ -196,6 +297,12 @@ public class ScheduleServiceTest {
         assertTrue(actualMessage.contains(expectedMessage)); 
     }
     
+    /**
+     * <pre>
+     * testCreateCourseSchedule with Null and Valid Value
+     * Call scheduleDao.saveCourseSchedule and test if values are save and return error message if null values are save
+     * <pre>
+     */
     @Test
     void testCreateCourseSchedule() {
         doThrow(new DataRetrievalFailureException("error")).when(scheduleDao).saveCourseSchedule(null);
@@ -211,6 +318,12 @@ public class ScheduleServiceTest {
         assertEquals(courseSchedule.getMaxAllowed(), 10);
     }
     
+    /**
+     * <pre>
+     * testCreateCourseSchedule with Error Message
+     * Call scheduleDao.saveCourseSchedule and test if error message is return
+     * <pre>
+     */
     @Test
     void testCreateCourseSchedule_Error() {
         doThrow(new DataRetrievalFailureException("error")).when(scheduleDao).saveCourseSchedule(any(CourseSchedule.class));
@@ -225,6 +338,12 @@ public class ScheduleServiceTest {
         assertTrue(actualMessage.contains(expectedMessage));
     }
     
+    
+    /**
+     * <pre>
+     * Build data for Create Course Schedule
+     * <pre>
+     */
     private CourseSchedule createCourseSchedule() {
         CourseScheduleDetail courseScheduleDetail = 
                 new CourseScheduleDetail.Builder(1L, ZonedDateTime.now(), ZonedDateTime.now().plusHours(5)).build();
@@ -233,6 +352,11 @@ public class ScheduleServiceTest {
         return new CourseSchedule.Builder(1L, 1L, 1L, 1, courseScheduleDetailSet).maxAllowed(10).build();
     }
     
+    /**
+     * <pre>
+     * Builded data for Create View Course Schedule
+     * <pre>
+     */
     private CourseSchedule createViewCourseSchedule() {
         CourseScheduleDetail courseScheduleDetail = 
                 new CourseScheduleDetail.Builder(1L, ZonedDateTime.now(), ZonedDateTime.now().plusHours(5)).build();
