a03f4fbfa81c55613f676aecb3b1f5ac43bc8a9c
k.freo <k.freo@PHR9Z02631ML.g07.fujitsu.local>
2020-07-10 16:03:57 +0800

    refs #122003 add Add ShowCourseDeclineFormTest  under
    added ShowCourseDeclineFormTest class for requirement purposes.

---
+++ src/test/java/com/fujitsu/ph/tsup/enrollment/web/ShowCourseDeclineFormTest.java
@@ -0,0 +1,27 @@
+package com.fujitsu.ph.tsup.enrollment.web;
+
+//=================================================================================================
+//$Id:PR01$
+//Project Name :Training Sign Up
+//System Name  :Enroll Course
+//Class Name   :SubmitCourseDeclineFormTest.java
+//
+//<<Modification History>>
+//Version | Date       | Updated By            | Content
+//--------+------------+-----------------------+--------------------------------------------------
+//0.02    | 07/10/2020 | WS) K.Freo            | New Creation
+//=================================================================================================
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShowCourseDeclineFormTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
