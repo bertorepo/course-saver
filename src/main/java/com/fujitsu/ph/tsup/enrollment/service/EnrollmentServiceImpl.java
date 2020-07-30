d63b5de19e175bf61eb62db47ec9b4728f560e7b
t.oviedo <t.oviedo@PHR9Z02632ML.g07.fujitsu.local>
2020-07-30 15:04:03 +0800

    refs #120667 update Update Enrollment Package
    Update EnrollmentController, EnrollmentServiceImpl and viewCourseEnroll
    html fix datetime-local input and modals

--- src/main/java/com/fujitsu/ph/tsup/enrollment/service/EnrollmentServiceImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/enrollment/service/EnrollmentServiceImpl.java
@@ -48,7 +48,7 @@ public class EnrollmentServiceImpl implements EnrollmentService {
         try {
                Set<CourseSchedule> courseScheduleSet = enrollmentDao
                        .findAllScheduledCourses(fromDateTime, toDateTime);
-                 if (courseScheduleSet.isEmpty() || courseScheduleSet == null) {
+                 if (courseScheduleSet == null || courseScheduleSet.isEmpty()){
                         throw new IllegalArgumentException("No schedules found");
                   }
                 return courseScheduleSet;
