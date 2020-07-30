d76137f4802521eabb1908e7f0554a1c0bace417
kenneth.abad <k.abad@fujitsu.com>
2020-07-30 13:05:59 +0800

    refs #121991 add Added features for Controller test, RowMappers, and html files for TS-UP project
    Added/Updated controller test, rowmappers, and html files for additional
    features

--- src/main/java/com/fujitsu/ph/tsup/attendance/dao/CourseAttendanceRowMapper.java
+++ src/main/java/com/fujitsu/ph/tsup/attendance/dao/CourseAttendanceRowMapper.java
@@ -1,10 +1,13 @@
 package com.fujitsu.ph.tsup.attendance.dao;
 
 import com.fujitsu.ph.tsup.attendance.domain.CourseAttendance;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.ZonedDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+
 import org.springframework.jdbc.core.RowMapper;
 
 //==================================================================================================
@@ -14,11 +17,12 @@ import org.springframework.jdbc.core.RowMapper;
 //Class Name   :CourseAttendanceRowMapper.java
 //
 //<<Modification History>>
-//Version | Date       | Updated By            | Content
-//--------+------------+-----------------------+---------------------------------------------------
-//0.01    | 06/26/2020 |  WS) J. Iwarat        | New Creation
-//0.02    | 07/07/2020 |  WS) J. Iwarat        | Update
-//0.02    | 07/08/2020 |  WS) J. Iwarat        | Update
+//Version | Date       | Updated By                                      | Content
+//--------+------------+-------------------------------------------------+--------------------------
+//0.01    | 06/26/2020 |  WS) J. Iwarat                                  | New Creation
+//0.02    | 07/07/2020 |  WS) J. Iwarat                                  | Update
+//0.03    | 07/08/2020 |  WS) J. Iwarat                                  | Update
+//0.04    | 07/27/2020 |  WS) K. Abad, WS) J. Iwarat, WS) R.Ramos        | Update
 //==================================================================================================
 /**
  * <pre>
@@ -26,8 +30,10 @@ import org.springframework.jdbc.core.RowMapper;
  * In this class, it maps the row of a resultset on a per row basis
  * </pre>
  * 
- * @version 0.02
+ * @version 0.04
+ * @author k.abad
  * @author j.iwarat
+ * @author r.ramos
  */
 public class CourseAttendanceRowMapper implements RowMapper<CourseAttendance> {
 
@@ -37,26 +43,28 @@ public class CourseAttendanceRowMapper implements RowMapper<CourseAttendance> {
         Long id = rs.getLong("id");
         Long courseScheduleDetailId = rs.getLong("COURSE_SCHEDULE_ID");
         String courseName = rs.getString("COURSE_NAME");
-        String instructorName = rs.getString("INSTRUCTOR_LAST_NAME") + ", " + rs.getString("INSTRUCTOR_FIRST_NAME");
+        String instructorName = rs.getString("INSTRUCTOR_LAST_NAME") + 
+                ", " + rs.getString("INSTRUCTOR_FIRST_NAME");
         String venueName = rs.getString("VENUE_NAME");
         Long participantId = rs.getLong("PARTICIPANT_ID");
-        String participantName = rs.getString("PARTICIPANT_LAST_NAME") + ", " + rs.getString("PARTICIPANT_FIRST_NAME");
+        String participantName = rs.getString("PARTICIPANT_LAST_NAME") + 
+                ", " + rs.getString("PARTICIPANT_FIRST_NAME");
         float duration = rs.getFloat("DURATION");
         char status = rs.getString("STATUS").charAt(0);
 
         ZonedDateTime scheduledStartDateTime = ZonedDateTime
-                .ofInstant(rs.getTimestamp("SCHEDULED_START_DATETIME").toInstant(), ZoneId.of("UTC"));
-
+                .ofInstant(rs.getTimestamp("SCHEDULED_START_DATETIME").toLocalDateTime()
+                .toInstant(ZoneOffset.UTC),ZoneId.of("UTC")); 
         ZonedDateTime scheduledEndDateTime = ZonedDateTime
-                .ofInstant(rs.getTimestamp("SCHEDULED_END_DATETIME").toInstant(), ZoneId.of("UTC"));
+                .ofInstant(rs.getTimestamp("SCHEDULED_END_DATETIME").toLocalDateTime()
+                .toInstant(ZoneOffset.UTC),ZoneId.of("UTC"));
+        ZonedDateTime loginDateTime = ZonedDateTime
+                .ofInstant(rs.getTimestamp("LOG_IN_DATETIME").toLocalDateTime()
+                .toInstant(ZoneOffset.UTC),ZoneId.of("UTC"));
 
-        ZonedDateTime loginDateTime = ZonedDateTime.ofInstant(rs.getTimestamp("LOG_IN_DATETIME").toInstant(),
-                ZoneId.of("UTC"));
-
-        CourseAttendance courseAttendance = new CourseAttendance.Builder(id, courseScheduleDetailId, courseName,
-                instructorName, venueName, participantId, participantName, scheduledStartDateTime, scheduledEndDateTime,
-                duration, loginDateTime, status).build();
-
+        CourseAttendance courseAttendance = new CourseAttendance.Builder(id, courseScheduleDetailId, 
+                courseName,instructorName, venueName, participantId, participantName, scheduledStartDateTime, 
+                scheduledEndDateTime,duration, loginDateTime, status).build();
         return courseAttendance;
     }
 }
--- src/main/java/com/fujitsu/ph/tsup/attendance/dao/CourseScheduleDetailRowMapper.java
+++ src/main/java/com/fujitsu/ph/tsup/attendance/dao/CourseScheduleDetailRowMapper.java
@@ -1,11 +1,16 @@
 package com.fujitsu.ph.tsup.attendance.dao;
 
+import com.fujitsu.ph.tsup.attendance.domain.CourseScheduleDetail;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+
 import org.springframework.jdbc.core.RowMapper;
-import com.fujitsu.ph.tsup.attendance.domain.CourseScheduleDetail;
+
 //==================================================================================================
 //$Id:PR03$
 //Project Name :Training Sign Up
@@ -13,19 +18,22 @@ import com.fujitsu.ph.tsup.attendance.domain.CourseScheduleDetail;
 //Class Name   :CourseScheduleDetailRowMapper.java
 //
 //<<Modification History>>
-//Version | Date       | Updated By            | Content
-//--------+------------+-----------------------+---------------------------------------------------
-//0.01    | 06/26/2020 |  WS) J. Iwarat        | New Creation
+//Version | Date       | Updated By                                      | Content
+//--------+------------+-------------------------------------------------+--------------------------
+//0.01    | 06/26/2020 | WS) J. Iwarat                                   | New Creation
+//0.02    | 07/27/2020 | WS) K. Abad, WS) J. Iwarat, WS) R.Ramos         | Update
 //==================================================================================================
 /**
-* <pre>
-* It is the row mapping class that maps each row to a result object
-* In this class, it maps the row of a resultset on a per row basis
-* </pre>
-* 
-* @version 0.01
-* @author j.iwarat
-*/
+ * <pre>
+ * It is the row mapping class that maps each row to a result object
+ * In this class, it maps the row of a resultset on a per row basis
+ * </pre>
+ * 
+ * @version 0.02
+ * @author k.abad
+ * @author j.iwarat
+ * @author r.ramos
+ */
 public class CourseScheduleDetailRowMapper implements RowMapper<CourseScheduleDetail> {
 
     @Override
@@ -35,14 +43,14 @@ public class CourseScheduleDetailRowMapper implements RowMapper<CourseScheduleDe
         Long courseScheduleId = rs.getLong("COURSE_SCHEDULE_ID");
 
         ZonedDateTime scheduledStartDateTime = ZonedDateTime
-                .ofInstant(rs.getTimestamp("SCHEDULED_START_DATETIME").toInstant(), ZoneId.of("UTC"));
-
+                .ofInstant(rs.getTimestamp("SCHEDULED_START_DATETIME").toLocalDateTime()
+                .toInstant(ZoneOffset.UTC), ZoneId.of("UTC")); 
         ZonedDateTime scheduledEndDateTime = ZonedDateTime
-                .ofInstant(rs.getTimestamp("SCHEDULED_END_DATETIME").toInstant(), ZoneId.of("UTC"));
+                .ofInstant(rs.getTimestamp("SCHEDULED_END_DATETIME").toLocalDateTime()
+                .toInstant(ZoneOffset.UTC),ZoneId.of("UTC"));
 
         CourseScheduleDetail courseScheduleDetail = new CourseScheduleDetail.Builder(id, courseScheduleId,
                 scheduledStartDateTime, scheduledEndDateTime).build();
         return courseScheduleDetail;
     }
-
 }
