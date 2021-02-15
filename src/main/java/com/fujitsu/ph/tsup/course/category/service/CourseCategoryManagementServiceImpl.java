09a989e28dcfcab204551a520b981fc615cc2f4a
glenda.cabiling <g.cabiling@fujitsu.com>
2021-02-15 16:06:07 +0800

    Function Modification
    refs #132349 update Updated CourseCategoryManagementServiceImpl.java
    
    Added methods needed for Course Category View: findAllCourseCategory and
    findCourseCategoryByName.

--- src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementServiceImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementServiceImpl.java
@@ -1,13 +1,58 @@
 /**
- * 
+ * Copyright (C) 2020 FUJITSU LIMITED All rights reserved.
  */
 package com.fujitsu.ph.tsup.course.category.service;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fujitsu.ph.tsup.course.category.dao.CourseCategoryManagementDao;
+import com.fujitsu.ph.tsup.course.category.model.CourseCategory;
 
 /**
  * @author a.batongbaca
  * @version creation: 0.01 Date: 2021-02-08
  *
  */
-public class CourseCategoryManagementServiceImpl {
+@Service
+public class CourseCategoryManagementServiceImpl  implements CourseCategoryManagementService{
 
+    @Autowired
+    CourseCategoryManagementDao courseCategoryManagementDao;
+    
+    @Override
+    public Set<CourseCategory> findAllCourseCategory() {
+
+        return courseCategoryManagementDao.findAllCourseCategory();
+    }
+
+    @Override
+    public Set<CourseCategory> findCourseCategoryByName(String name) {
+
+        Set<CourseCategory> courseCategoryFormList = courseCategoryManagementDao.findCourseCategoryByName(name);
+        
+        try {
+            
+            if(courseCategoryFormList == null || courseCategoryFormList.isEmpty()) {
+                
+                return courseCategoryManagementDao.findAllCourseCategory();
+                
+            } else {
+                
+                return courseCategoryFormList;
+                
+            }
+            
+        } catch(Exception ex) {
+            
+            ex.printStackTrace();
+            
+        }
+        
+         return courseCategoryFormList;
+        
+    
+    }
 }
