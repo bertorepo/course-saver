3938da1b96c3f24503c013159358df05fbf0789a
ph_zdeguia <z.deguia@fujitsu.com>
2021-02-16 15:14:58 +0800

    refs #132363 Added service implementation for delete course category by ID and finding course category by ID
    Added deleteCourseCategoryById and findCourseCategoryById in
    CourseCategoryManagementServiceImpl

--- src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementServiceImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementServiceImpl.java
@@ -3,11 +3,35 @@
  */
 package com.fujitsu.ph.tsup.course.category.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fujitsu.ph.tsup.course.category.dao.CourseCategoryManagementDao;
+import com.fujitsu.ph.tsup.course.category.model.CourseCategory;
+
 /**
  * @author a.batongbaca
  * @version creation: 0.01 Date: 2021-02-08
  *
  */
-public class CourseCategoryManagementServiceImpl {
+@Service
+public class CourseCategoryManagementServiceImpl implements CourseCategoryManagementService {
 
+    @Autowired
+    CourseCategoryManagementDao courseCategoryManagementDao;
+
+    @Override
+    public void deleteCourseCategoryById(Long id) {
+
+        courseCategoryManagementDao.deleteCourseCategoryById(id);
+
+    }
+
+    @Override
+    public CourseCategory findCourseCategoryById(Long id) {
+
+        CourseCategory courseCategoryResult = courseCategoryManagementDao.findCourseCategoryById(id);
+
+        return courseCategoryResult;
+    }
 }
