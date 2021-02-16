51c27ab93c98baff355124151161330641d2d8c6
j.zamora <j.zamora@WS-FDC-9489.g07.fujitsu.local>
2021-02-16 15:17:06 +0800

    refs #132341 add CourseCategoryManagementService

--- src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementService.java
+++ src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementService.java
@@ -1,13 +1,19 @@
-/**
- * 
- */
 package com.fujitsu.ph.tsup.course.category.service;
+
+import java.util.Set;
+import com.fujitsu.ph.tsup.course.category.model.CourseCategory;
 
 /**
  * @author a.batongbaca
- * @version creation: 0.01 Date: 2021-02-08
+ * @version Creation: 0.01 Date: 2021-02-28
  *
  */
+
 public interface CourseCategoryManagementService {
 
+    // A method that checks if the input has a duplicate
+    Set<CourseCategory> findCourseCategoryByName(String name);
+
+    // A method that creates course category
+    void createCourseCategory(CourseCategory courseCategory);
 }
