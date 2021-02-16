1701e78ef41038aed8be72c147f4aa01a0578d4e
ph_abatongbacal <a.batongbacal@fujitsu.com>
2021-02-16 15:06:09 +0800

    refs #132355 add javadocs

--- src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementService.java
+++ src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementService.java
@@ -3,15 +3,29 @@
  */
 package com.fujitsu.ph.tsup.course.category.service;
 
-import java.util.Set;
-
 import com.fujitsu.ph.tsup.course.category.model.CourseCategory;
-
+//=======================================================
+//$Id: PR10$
+//Project Name: Training Sign Up
+//System Name : Course Category Management Process
+//Class Name: CourseCategoryManagementService.java
+//
+//<<Modification History>>
+//Version | Date       | Updated by      | Content
+//--------+------------+-----------------+---------------
+//0.01    | 02/08/2020 | WS) A.Batongbacal   | New Creation
+//0.02    | 02/15/2020 | WS) A.Batongbacal   | Update
+//=======================================================
 /**
- * @author a.batongbaca
- * @version creation: 0.01 Date: 2021-02-08
- *
- */
+* <pre>
+* The interface of course category management service
+* 
+* <pre>
+* 
+* @version 0.02
+* @author a.batongbaca
+*
+*/
 public interface CourseCategoryManagementService {
 
     void updateCourseCategory(CourseCategory courseCategory);
