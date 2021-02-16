05acc3bb32e2333351f5b0074de14b55a68d85f3
ph_zdeguia <z.deguia@fujitsu.com>
2021-02-16 15:07:51 +0800

    refs #132362 Added service for delete course category by ID and finding course category by ID
    Added deleteCourseCategoryById and findCourseCategoryById in
    CourseCategoryManagementService

--- src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementService.java
+++ src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementService.java
@@ -3,6 +3,8 @@
  */
 package com.fujitsu.ph.tsup.course.category.service;
 
+import com.fujitsu.ph.tsup.course.category.model.CourseCategory;
+
 /**
  * @author a.batongbaca
  * @version creation: 0.01 Date: 2021-02-08
@@ -10,4 +12,8 @@ package com.fujitsu.ph.tsup.course.category.service;
  */
 public interface CourseCategoryManagementService {
 
+    void deleteCourseCategoryById(Long id);
+
+    CourseCategory findCourseCategoryById(Long id);
+    
 }
