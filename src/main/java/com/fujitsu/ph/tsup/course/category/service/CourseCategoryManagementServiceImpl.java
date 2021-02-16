461c7940f5ab07ae7d9bde89044cce14f1eed113
ph_abatongbacal <a.batongbacal@fujitsu.com>
2021-02-16 15:03:56 +0800

    refs #132356 add javadocs

--- src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementServiceImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementServiceImpl.java
@@ -6,15 +6,33 @@ package com.fujitsu.ph.tsup.course.category.service;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.fujitsu.ph.tsup.course.category.dao.CourseCategoryManagementDao;
 import com.fujitsu.ph.tsup.course.category.model.CourseCategory;
-
+//=======================================================
+//$Id: PR10$
+//Project Name: Training Sign Up
+//System Name : Course Category Management Process
+//Class Name: CourseCategoryManagementServiceImpl.java
+//
+//<<Modification History>>
+//Version | Date       | Updated by      | Content
+//--------+------------+-----------------+---------------
+//0.01    | 02/08/2020 | WS) A.Batongbacal   | New Creation
+//0.02    | 02/15/2020 | WS) A.Batongbacal   | Update
+//=======================================================
 /**
- * @author a.batongbaca
- * @version creation: 0.01 Date: 2021-02-08
- *
- */
+* <pre>
+* The implementation of course category management service
+* 
+* <pre>
+* 
+* @version 0.02
+* @author a.batongbaca
+*
+*/
+@Service
 public class CourseCategoryManagementServiceImpl implements CourseCategoryManagementService{
 
     @Autowired
