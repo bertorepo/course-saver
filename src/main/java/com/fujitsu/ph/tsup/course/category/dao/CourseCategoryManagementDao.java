09541ef6ab55263b963afab872dc4d587a153c9c
j.lira <j.lira@PHR0500519ML.g07.fujitsu.local>
2021-02-18 19:28:11 +0800

    refs #132350 create CourseCategoryManagementDao
    added method for searching, finding and loading course category.

--- src/main/java/com/fujitsu/ph/tsup/course/category/dao/CourseCategoryManagementDao.java
+++ src/main/java/com/fujitsu/ph/tsup/course/category/dao/CourseCategoryManagementDao.java
@@ -1,13 +1,26 @@
-/**
- * 
+/*
+ * Copyright (C) 2020 FUJITSU LIMITED All rights reserved.
  */
 package com.fujitsu.ph.tsup.course.category.dao;
 
+import java.util.Set;
+
+import com.fujitsu.ph.tsup.course.category.model.CourseCategory;
+
 /**
- * @author a.batongbaca
- * @version creation: 0.01 Date: 2021-02-08
+ * CourseManagementDao class
+ * 
+ * 
  *
  */
 public interface CourseCategoryManagementDao {
 
+	   // Method for finding Course Category by Id
+    CourseCategory findCourseCategoryById(Long id);
+    
+    // Method for loading all course category in Course Category View
+    Set<CourseCategory> findAllCourseCategory();
+    
+    // Method for searching Course Category by Name
+    Set<CourseCategory> findCourseCategoryByName(String name);
 }
