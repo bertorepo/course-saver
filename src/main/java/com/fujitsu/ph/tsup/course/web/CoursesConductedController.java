01cbbea373f1a8fc85d871e7c72702240b258366
m.rivera <m.rivera@PHR9Z02636ML.g07.fujitsu.local>
2021-02-24 14:30:22 +0800

    ref #132800 update Updated CoursesConductedController.java

--- src/main/java/com/fujitsu/ph/tsup/course/web/CoursesConductedController.java
+++ src/main/java/com/fujitsu/ph/tsup/course/web/CoursesConductedController.java
@@ -1,3 +1,13 @@
+//==================================================================================================																																											
+// Project Name : Training Sign Up
+// System Name  : CoursesConductedController																																								
+// Class Name   : CoursesConductedController.java 																																											
+//																																											
+// <<Modification History>> 																																											
+// Version | Date       | Updated By            | Content																																											
+//---------+------------+-----------------------+--------------------------------------------------- 																																											
+// 1.0.0   | 2021/02/17 | WS)M.Rivera           | New Creation																																											
+//==================================================================================================
 package com.fujitsu.ph.tsup.course.web;
 
 import java.time.ZonedDateTime;
@@ -16,7 +26,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.fujitsu.ph.tsup.course.model.CourseForm;
 import com.fujitsu.ph.tsup.course.model.CoursesConducted;
 import com.fujitsu.ph.tsup.course.model.CoursesConductedForm;
 import com.fujitsu.ph.tsup.course.model.CoursesConductedListForm;
@@ -25,11 +34,10 @@ import com.fujitsu.ph.tsup.course.service.CoursesConductedService;
 /**
  * <pre>
  * The controller for the courses conducted
+ * </pre>
  * 
- * <pre>
- * 
- * @version 0.01
  * @author m.rivera
+ * @version 1.0.0
  */
 
 @Controller
@@ -49,9 +57,9 @@ public class CoursesConductedController {
     
     /**
      * <pre>
-     * View all course schedule. Method = GET
+     * View all course conducted. Method = GET
      * 
-     * <pre>
+     * </pre>
      * 
      * @param courseConductedForm    form
      * @param BindingResult          bindingResult
@@ -60,9 +68,9 @@ public class CoursesConductedController {
      */    
     
     @GetMapping("/course")
-	public String viewCoursesConducted(@Valid @ModelAttribute("coursesConducted") CoursesConductedListForm coursesConductedListForm, BindingResult bindingResult,Model model){
-	    
-        logger.debug("CourseConductedListForm: {}", coursesConductedListForm);
+	public String viewCoursesConducted(@Valid @ModelAttribute("coursesConducted") 
+	CoursesConductedListForm coursesConductedListForm, BindingResult bindingResult,Model model){
+    	logger.debug("CourseConductedListForm: {}", coursesConductedListForm);
         logger.debug("Result: {}", bindingResult);
         
 
@@ -70,11 +78,13 @@ public class CoursesConductedController {
             return "course-management/generateReports";
         }
 		
-        if (coursesConductedListForm.getScheduledStartDateTime() == null || coursesConductedListForm.getScheduledEndDateTime() == null) {
+        if (coursesConductedListForm.getScheduledStartDateTime() == null || 
+        		coursesConductedListForm.getScheduledEndDateTime() == null) {
         	coursesConductedListForm.setScheduledStartDateTime(ZonedDateTime.now().withHour(0).withMinute(0));
         	coursesConductedListForm.setScheduledEndDateTime(ZonedDateTime.now().plusDays(5));
         }
-        Set<CoursesConducted> courseConducted = coursesConductedService.findAllCoursesConducted(coursesConductedListForm.getScheduledStartDateTime(), 
+        Set<CoursesConducted> courseConducted = coursesConductedService.findAllCoursesConducted(
+        		coursesConductedListForm.getScheduledStartDateTime(), 
         		coursesConductedListForm.getScheduledEndDateTime());
         
         Set<CoursesConductedForm> coursesConductedFormSet = new HashSet<>();
@@ -91,7 +101,6 @@ public class CoursesConductedController {
         
         coursesConductedListForm.setCoursesConductedSet(coursesConductedFormSet);
         
-        
         model.addAttribute("coursesConductedForm", coursesConductedListForm);
         model.addAttribute("coursesConducted", coursesConductedFormSet);
         
@@ -100,7 +109,6 @@ public class CoursesConductedController {
     
     @GetMapping("/load")
     public String viewSummaryCourseConducted() {
-
     	return "course-management/generateReports";
     }
 
