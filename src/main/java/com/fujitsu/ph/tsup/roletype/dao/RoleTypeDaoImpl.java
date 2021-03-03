87a1eba2c01f45203b605e1d06dc29ce5d8ac725
Baton <m.baton@fujitsu.com>
2021-03-03 15:10:59 +0800

    ref#132380-BforSoftDeleteMod

--- src/main/java/com/fujitsu/ph/tsup/roletype/dao/RoleTypeDaoImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/roletype/dao/RoleTypeDaoImpl.java
@@ -26,18 +26,20 @@ import com.fujitsu.ph.tsup.roletype.domain.RoleType;
 //0.04    | 2021/02/22 | WS) j.sayaboc         | Updated
 //0.05    | 2021/02/24 | WS) p.cui             | Updated
 //0.06    | 2021/02/26 | WS) c.sinda           | Updated
+//0.07    | 2021/03/03 | WS) m.baton		   | Updated
 //==================================================================================================
 
 
 /**
  * RoleTypeDaoImpl class
  * 
- * @version 0.06
+ * @version 0.07
  * @author rl.naval
  * @author s.labador
  * @author p.cui
  * @author j.sayaboc
  * @author c.sinda
+ * @author m.baton
  */
 
 @Repository
