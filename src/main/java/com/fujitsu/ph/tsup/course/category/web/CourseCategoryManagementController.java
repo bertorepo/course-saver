aec0dba6f43dceb387ed6d6885b9763e2da8933a
Zedriz De Guia <z.deguia@fujitsu.com>
2021-03-19 15:33:53 +0800

    removed unused import in controller

--- src/main/java/com/fujitsu/ph/tsup/course/category/web/CourseCategoryManagementController.java
+++ src/main/java/com/fujitsu/ph/tsup/course/category/web/CourseCategoryManagementController.java
@@ -8,7 +8,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
