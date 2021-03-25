450e5fb48219090f0e2383dc0b6bae3e0da1e55c
ph_abatongbacal <a.batongbacal@fujitsu.com>
2021-03-25 13:51:45 +0800

    add single quote in invalid characters

--- src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementServiceImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/course/category/service/CourseCategoryManagementServiceImpl.java
@@ -140,7 +140,7 @@ public class CourseCategoryManagementServiceImpl implements CourseCategoryManage
     
     private boolean checkForSpecialCharacter(String input) {
         
-        String specialCharactersString = "*/\"\\:<>?|"; 
+        String specialCharactersString = "*/\"'\\:<>?|"; 
         for (int i=0; i < input.length() ; i++)
         {
             char ch = input.charAt(i);
--- src/main/resources/static/js/lib/viewCourseCategory.js
+++ src/main/resources/static/js/lib/viewCourseCategory.js
@@ -28,7 +28,7 @@ function validateIfEmpty() {
 	var id = document.getElementById("id");
 	var category = document.getElementById("category");
 	var detail = document.getElementById("detail");
-	var format = /[*:?"\\|<>\/]/;
+	var format = /[*:'?"\\|<>\/]/;
 	document.getElementById("updateBtn").disabled = false;
 	document.getElementById("categoryErrorMsg").innerHTML = "";
 	document.getElementById("detailErrorMsg").innerHTML = "";
