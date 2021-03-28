87f163a96437ae5330053ebd26f191b5bbb44100
r.paculanang <r.paculanang@PHR0400662CL.g07.fujitsu.local>
2021-03-29 02:14:17 +0800

    Added Feature ts up func#132580 auto registration

---
+++ src/main/java/com/fujitsu/ph/tsup/authz/config/TsupAuthenticationSuccessHandler.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2020 FUJITSU LIMITED All rights reserved.
+ */
+package com.fujitsu.ph.tsup.authz.config;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.fujitsu.ph.auth.model.FpiUser;
+import com.fujitsu.ph.tsup.authz.service.AuthorizationService;
+import com.fujitsu.ph.tsup.common.domain.Employee;
+
+/**
+ * TsupAuthenticationSuccessHandler class
+ * 
+ * @author k.sala (New Creation by: k.sala)
+ * @version 0.01
+ */
+public class TsupAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Autowired
+    private AuthorizationService authorizationService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+
+        handle(request, response, authentication);
+        clearAuthenticationAttributes(request);
+    }
+
+    protected void handle(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException {
+        String targetUrl = determineTargetUrl(authentication);
+
+        if (response.isCommitted()) {
+            logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+            return;
+        }
+
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+
+    /**
+     * This method will look for the user from employee table if user found return dashboard else register
+     * 
+     * @param authentication
+     * @return /register or /dashboard
+     */
+    protected String determineTargetUrl(final Authentication authentication) {
+        FpiUser user = (FpiUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        Employee employee = authorizationService.findDetailsByUsername(user.getUserName());
+        if (employee == null) {
+            return "/register/#registerModal";
+        } else {
+            return "/dashboard";
+        }
+    }
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return;
+        }
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+    }
+}
