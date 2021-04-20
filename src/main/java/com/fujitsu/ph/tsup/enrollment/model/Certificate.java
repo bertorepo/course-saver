32c80ac8481a3542eebce71e188deb18dd080610
Salvador <m.salvador@fujitsu.com>
2021-04-20 11:46:36 +0800

    ref#<134723> created Certificate.java

--- src/main/java/com/fujitsu/ph/tsup/enrollment/model/Certificate.java
+++ src/main/java/com/fujitsu/ph/tsup/enrollment/model/Certificate.java
@@ -2,4 +2,39 @@ package com.fujitsu.ph.tsup.enrollment.model;
 
 public class Certificate {
 
+	private String certificate;
+	private Long userId;
+	private Long id;
+	private Long courseId;
+	public String getCertificate() {
+		return certificate;
+	}
+	public void setCertificate(String certificate) {
+		this.certificate = certificate;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(Long courseId) {
+		this.courseId = courseId;
+	}
+	@Override
+	public String toString() {
+		return "Certificate [certificate=" + certificate + ", userId=" + userId + ", id=" + id + ", courseId="
+				+ courseId + "]";
+	}
+	
+	
 }
