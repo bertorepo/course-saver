211cdfc7e2db992b65bc1ad3674b92e4f5f36f55
r.rivero <r.rivero@PHR4900852ML.g07.fujitsu.local>
2021-04-21 11:48:27 +0800

    refs #134805 add SummaryGSTDevDao

---
+++ src/main/java/com/fujitsu/ph/tsup/report/summary/gst/dev/dao/SummaryGSTDevDao.java
@@ -0,0 +1,9 @@
+package com.fujitsu.ph.tsup.report.summary.gst.dev.dao;
+
+import com.fujitsu.ph.tsup.report.summary.gst.dev.model.SummaryGSTDevForm;
+
+public interface SummaryGSTDevDao {
+	
+	SummaryGSTDevForm getSummary();
+
+}
