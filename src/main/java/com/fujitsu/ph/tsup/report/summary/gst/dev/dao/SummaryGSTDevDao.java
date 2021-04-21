d97ffda59e5e8bc62c9f62404173a2f809aef4dc
ph_abatongbacal <a.batongbacal@fujitsu.com>
2021-04-21 14:23:06 +0800

    remove incorrect package name

--- src/main/java/com/fujitsu/ph/tsup/report/summary/gst/dev/dao/SummaryGSTDevDao.java
+++
@@ -1,9 +0,0 @@
-package com.fujitsu.ph.tsup.report.summary.gst.dev.dao;
-
-import com.fujitsu.ph.tsup.report.summary.gst.dev.model.SummaryGSTDevForm;
-
-public interface SummaryGSTDevDao {
-	
-	SummaryGSTDevForm getSummary();
-
-}
