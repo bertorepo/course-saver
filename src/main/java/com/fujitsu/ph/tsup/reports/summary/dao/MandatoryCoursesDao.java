d5e3078c772be334af76d41433e447a94790cb5e
j.barbadillo <j.barbadillo@PHR7Z01866ML.g07.fujitsu.local>
2021-04-21 16:57:15 +0800

    Refs #134950 add MandatoryCoursesDao.java - Created the MandatoryCoursesDao

---
+++ src/main/java/com/fujitsu/ph/tsup/reports/summary/dao/MandatoryCoursesDao.java
@@ -0,0 +1,72 @@
+//==================================================================================================																																											
+// Project Name : Training Sign Up
+// System Name  : MandatoryCoursesDao																																								
+// Class Name   : MandatoryCoursesDao.java 																																											
+//																																											
+// <<Modification History>> 																																											
+// Version | Date       | Updated By            | Content																																											
+//---------+------------+-----------------------+--------------------------------------------------- 																																											
+// 1.0.0   | 2021/04/21 | WS)J.Barbadillo       | New Creation																																											
+//==================================================================================================																																																																																						
+package com.fujitsu.ph.tsup.reports.summary.dao; 
+
+import java.time.ZonedDateTime;
+import java.util.Set;
+
+
+import com.fujitsu.ph.tsup.reports.summary.dao.*;
+
+/**
+* <pre>
+* 	The Dao for the MandatoryCoursesController
+* </pre>
+* 
+* @author j.barbadillo
+* @version 1.0.0
+*/
+
+public interface MandatoryCoursesDao {
+	
+	/**
+	 * <pre>
+     * 	Find the total number of  JDU based on the given date range
+     * </pre>
+     * @param selectedStartDateTime
+     * @param selectedEndDateTime 
+     */
+	/*Set<MandatoryCourses> findMandatoryCourses(ZonedDateTime selectedStartDateTime,
+			ZonedDateTime selectedEndDateTime);*.
+	
+	
+	/**
+	 * <pre>
+     * 	Count the total number of JDU
+     * </pre>
+     * @param selectedStartDateTime
+     * @param selectedEndDateTime 
+     */
+	int findTotalNumberOfJdu(ZonedDateTime selectedStartDateTime,
+			ZonedDateTime selectedEndDateTime);
+	
+	/**
+	 * <pre>
+     * 	Find the total number of  JDU who finished training  based on the given date range  
+     * </pre>
+     * @param selectedStartDateTime
+     * @param selectedEndDateTime 
+     */
+	int findTotalNumberOfJduWhoFinishedTraining(ZonedDateTime selectedStartDateTime,
+			ZonedDateTime selectedEndDateTime);
+	
+	
+	/**
+	 * <pre>
+     * 	Find the total number of  JDU who finished training lastweek  based on the given date range  
+     * </pre>
+     * @param selectedStartDateTime
+     * @param selectedEndDateTime 
+     */
+	int findTotalNumberOfJduWhoFinishedTrainingLastWeek(ZonedDateTime selectedStartDateTime,
+			ZonedDateTime selectedEndDateTime);
+	
+}
