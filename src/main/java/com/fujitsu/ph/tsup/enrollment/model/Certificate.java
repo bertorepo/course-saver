32329c5a5ec4ab9fc0220f7d1b32d8af33ab3313
Salvador <m.salvador@fujitsu.com>
2021-04-22 09:41:02 +0800

    ref#<134723> Modified Certificate.java

--- src/main/java/com/fujitsu/ph/tsup/enrollment/model/Certificate.java
+++ src/main/java/com/fujitsu/ph/tsup/enrollment/model/Certificate.java
@@ -1,40 +1,154 @@
 package com.fujitsu.ph.tsup.enrollment.model;
 
 public class Certificate {
-
+	
 	private String certificate;
-	private Long userId;
 	private Long id;
 	private Long courseId;
+	private Long userId;
+	
+	private Certificate(Builder builder) {
+
+        this.id = builder.id;
+        this.certificate = builder.certificate;
+        this.courseId = builder.courseId;
+        this.userId = builder.userId;
+    }
+	
 	public String getCertificate() {
+		
 		return certificate;
+		
 	}
+	
 	public void setCertificate(String certificate) {
+		
 		this.certificate = certificate;
+		
 	}
-	public Long getUserId() {
-		return userId;
-	}
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
+	
 	public Long getId() {
+		
 		return id;
+		
 	}
+	
 	public void setId(Long id) {
+		
 		this.id = id;
+		
 	}
+	
 	public Long getCourseId() {
+		
 		return courseId;
+		
 	}
+	
 	public void setCourseId(Long courseId) {
+		
 		this.courseId = courseId;
+		
 	}
-	@Override
-	public String toString() {
-		return "Certificate [certificate=" + certificate + ", userId=" + userId + ", id=" + id + ", courseId="
-				+ courseId + "]";
+	
+	public Long getUser() {
+		
+		return userId;
+		
 	}
+	
+	public void setUser(Long userId) {
+		
+		this.userId = userId;
+		
+	}
+	
+	/** Builder Class
+     * @author c.arias
+     *
+     */
+    public static class Builder {
+
+
+        private Long courseId;
+		private Long id;
+		private String certificate;
+		private Long userId;
+
+		/** Builder Constructor
+         * @param certificate
+         */
+        public Builder(Long courseId, String certificate, Long userId) {
+
+        	validateUserId(userId);
+            validateCertificate(certificate);
+            this.certificate = certificate;
+            this.userId = userId;
+            this.courseId = courseId;
+
+        }
+
+        /** Builder Constructor
+         * @param id
+         * @param certificate
+         */
+        public Builder(Long id, String certificate) {
+
+            validateId(id);
+            validateCertificate(certificate);
+
+            this.id = id;
+            this.certificate = certificate;
+
+        }
+        
+        public Certificate build() {
+
+            return new Certificate(this);
+
+        }
+
+        /** Validate course name if null or empty
+         * @param name Course Name
+         */
+        private void validateCertificate(String certificate) {
+
+            if (certificate.equals(null) || certificate.isEmpty()) {
+
+                throw new IllegalArgumentException("Course should not be empty");
+
+            }
+
+        }
+
+        /**
+         * Validate Course id if null or 0
+         * @param id
+         */
+        private void validateId(Long id) {
+
+            if (id == null || id == 0) {
+
+                throw new IllegalArgumentException("Id should not be empty");
+
+            }
+        }
+        
+        /**
+         * Validate User id if null or 0
+         * @param id
+         */
+        private void validateUserId(Long userId) {
+
+            if (userId == null) {
+
+                throw new IllegalArgumentException("Id should not be empty");
+
+            }
+        }
+
+
+    }
 	
 	
 }
