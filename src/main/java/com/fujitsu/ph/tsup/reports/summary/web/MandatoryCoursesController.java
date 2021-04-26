d5bcd2da9eee81c83dd8599219d79ed52f2e0d07
j.zamora <j.zamora@WS-FDC-9489.g07.fujitsu.local>
2021-04-26 17:00:22 +0800

    refs #134965 updates MandatoryCoursesController

--- src/main/java/com/fujitsu/ph/tsup/reports/summary/web/MandatoryCoursesController.java
+++ src/main/java/com/fujitsu/ph/tsup/reports/summary/web/MandatoryCoursesController.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright (C) 2021 FUJITSU LIMITED All rights reserved.
+ */
 package com.fujitsu.ph.tsup.reports.summary.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,38 +13,66 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.fujitsu.ph.tsup.reports.summary.model.MandatoryCourses;
 import com.fujitsu.ph.tsup.reports.summary.service.MandatoryCoursesService;
 
+/**
+ * The controller of mandatory courses
+ * 
+ * @author c.fuerzas (New Creation by: j.zamora)
+ * @version Revision: 0.01 Date: 2021-04-21
+ */
 @Controller
 @RequestMapping("mandatoryCourses")
 public class MandatoryCoursesController {
     @Autowired
     private MandatoryCoursesService mandatoryCoursesService;
-    
+
+     /**
+     * loads the summaryMandatoryCourses view
+     * 
+     * @param model
+     * @return string
+     */
     @GetMapping("load")
     public String loadGenerateReport(Model model) {
         return "reports/summaryMandatoryCourses";
     }
 
+     /**
+     * Passes the data to summaryMandatoryCourses when view button is pressed
+     * 
+     * @param form
+     * @param model
+     * @param bindingResult
+     * @return string
+     */
     @GetMapping("view")
     public String viewMandatoryCoursesReports(MandatoryCourses form, Model model, 
             BindingResult bindingResult) {
         MandatoryCourses courseDetails = new MandatoryCourses.Builder(form.getId(),form.getName()).build();
         model.addAttribute("getTotalNumberOfJduMembers", mandatoryCoursesService.getTotalNumberOfJduMembers());
-        model.addAttribute("getTotalNumberOfJduMembers", mandatoryCoursesService.getTotalNumberOfCompletion(courseDetails));
-        model.addAttribute("getTotalNumberOfJduMembers", mandatoryCoursesService.getTotalNumberOfCompletionLastWeek(courseDetails));
-        model.addAttribute("getTotalNumberOfJduMembers", mandatoryCoursesService.getPercentageCompletion());
-        model.addAttribute("getTotalNumberOfJduMembers", mandatoryCoursesService.getPercentageCompletionLastWeek());
+        //model.addAttribute("getTotalNumberOfCompletion", mandatoryCoursesService.getTotalNumberOfCompletion(courseDetails));
+        //model.addAttribute("getTotalNumberOfCompletionLastWeek", mandatoryCoursesService.getTotalNumberOfCompletionLastWeek(courseDetails));
+        model.addAttribute("getPercentageCompletion", mandatoryCoursesService.getPercentageCompletion());
+        model.addAttribute("getPercentageCompletionLastWeek", mandatoryCoursesService.getPercentageCompletionLastWeek());
         return "reports/summaryMandatoryCourses";
     }
-    
+
+    /**
+    * Passes the data to summaryMandatoryCourses when export button is pressed
+    * 
+    * @param form
+    * @param model
+    * @param bindingResult
+    * @return string
+    */
     @GetMapping("export")
     public String exportMandatoryCoursesReports(MandatoryCourses form, Model model, 
             BindingResult bindingResult) {
         MandatoryCourses courseDetails = new MandatoryCourses.Builder(form.getId(),form.getName()).build();
         model.addAttribute("getTotalNumberOfJduMembers", mandatoryCoursesService.getTotalNumberOfJduMembers());
-        model.addAttribute("getTotalNumberOfJduMembers", mandatoryCoursesService.getTotalNumberOfCompletion(courseDetails));
-        model.addAttribute("getTotalNumberOfJduMembers", mandatoryCoursesService.getTotalNumberOfCompletionLastWeek(courseDetails));
-        model.addAttribute("getTotalNumberOfJduMembers", mandatoryCoursesService.getPercentageCompletion());
-        model.addAttribute("getTotalNumberOfJduMembers", mandatoryCoursesService.getPercentageCompletionLastWeek());
+        //model.addAttribute("getTotalNumberOfCompletion", mandatoryCoursesService.getTotalNumberOfCompletion(courseDetails));
+        //model.addAttribute("getTotalNumberOfCompletionLastWeek", mandatoryCoursesService.getTotalNumberOfCompletionLastWeek(courseDetails));
+        model.addAttribute("getPercentageCompletion", mandatoryCoursesService.getPercentageCompletion());
+        model.addAttribute("getPercentageCompletionLastWeek", mandatoryCoursesService.getPercentageCompletionLastWeek());
         return "reports/summaryMandatoryCourses";
     }
 }
