b59a2fdc7675cd7c747f5489d1c8985de09a09d8
ph_abatongbacal <a.batongbacal@fujitsu.com>
2021-04-30 08:39:56 +0800

    refs #134803 update SummaryGSTDevServiceImpl.java

--- src/main/java/com/fujitsu/ph/tsup/report/summary/service/SummaryGSTDevServiceImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/report/summary/service/SummaryGSTDevServiceImpl.java
@@ -10,6 +10,11 @@
 //==================================================================================================
 package com.fujitsu.ph.tsup.report.summary.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -38,7 +43,41 @@ public class SummaryGSTDevServiceImpl implements SummaryGSTDevService {
      */
 	@Override
 	public SummaryGSTDevForm getSummary() {
-		return reportSummaryGSTDevDao.getSummary();
+//		return reportSummaryGSTDevDao.getSummary();
+	    Set<Long> employeeList = reportSummaryGSTDevDao.findAllJDUDev();
+        Set<Long> courseList = reportSummaryGSTDevDao.findAllCourseByCategoryId();
+        HashMap<Integer, Integer> totalCoursePerEmployee = new HashMap<>();
+        
+        //for JDU Dev who Finished 
+        for (long employee_id : employeeList) {
+            int total_course_completed = reportSummaryGSTDevDao.findTotalCoursePerEmployee(courseList, employee_id);
+            totalCoursePerEmployee.put((int) employee_id, total_course_completed);
+        }
+        int totalDevFin = (int) totalCoursePerEmployee.entrySet().stream().filter(v -> v.getValue().equals(courseList.size())).count();
+
+        //for JDU Dev who Finished Last Week
+        totalCoursePerEmployee = new HashMap<>();
+        for (long employee_id : employeeList) {
+            int total_course_completed = reportSummaryGSTDevDao.findTotalCoursePerEmployeeLastWeek(courseList, employee_id);
+            totalCoursePerEmployee.put((int) employee_id, total_course_completed);
+        }
+        int totalDevFinLastWk = (int) totalCoursePerEmployee.entrySet().stream().filter(v -> v.getValue().equals(courseList.size())).count();
+
+        int percentageFinToday = (totalDevFin * 100) / employeeList.size();
+        int percentageFinLastWk = (totalDevFinLastWk * 100) / employeeList.size();
+        
+        SummaryGSTDevForm summary = new SummaryGSTDevForm();
+        summary.setTotalNoJDUDevValue(employeeList.size());
+        summary.setTotalNoJDUDevLastWeekValue(reportSummaryGSTDevDao.findAllJDUDevLastWeek());
+        summary.setTotalNoOrigMemValue(reportSummaryGSTDevDao.findAllJDUOriginalMembers());
+        summary.setTotalNoNewMemValue(reportSummaryGSTDevDao.findAllJDUNewMembers());
+        summary.setTotalNoJDUDevFinValue(totalDevFin);
+        summary.setTotalNoJDUDevLastWkFinValue(totalDevFinLastWk);
+        summary.setPercentageFinTodayValue(percentageFinToday);
+        summary.setPercentageFinLastWkValue(percentageFinLastWk);
+        
+        
+        return summary;
 	}
 	
 }
