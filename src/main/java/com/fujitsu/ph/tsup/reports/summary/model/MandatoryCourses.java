6950b94da9f0c7b6591574aa936874bd469da3a2
Zedriz De Guia <z.deguia@fujitsu.com>
2021-05-03 13:01:44 +0800

    updated model MandatoryCourses

--- src/main/java/com/fujitsu/ph/tsup/reports/summary/model/MandatoryCourses.java
+++ src/main/java/com/fujitsu/ph/tsup/reports/summary/model/MandatoryCourses.java
@@ -4,6 +4,7 @@
 package com.fujitsu.ph.tsup.reports.summary.model;
 
 import java.time.ZonedDateTime;
+import java.util.Set;
 
 import javax.validation.constraints.NotNull;
 
@@ -27,42 +28,11 @@ public class MandatoryCourses {
      * Course name
      */
     private String name;
-
     /**
-     *  Start Date and Time
+     * List of Mandatory Courses
      */
-    @NotNull
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    private ZonedDateTime startDateTime;
+    private Set<MandatoryCourses> mandatoryCoursesSet;
     
-    /**
-     *  End Date and Time
-     */
-    @NotNull
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    private ZonedDateTime endDateTime;
-   
-    /**
-     * Empty Constructor for ConductedCourse class
-     */
-    public MandatoryCourses() {
-
-    }
-
-    /**
-     * MandatoryCourses Constructor
-     * @param builder Builder
-     */
-    private MandatoryCourses(Builder builder) {
-        this.id = builder.id;
-        this.name = builder.name;
-        this.startDateTime = builder.startDateTime;
-        this.endDateTime = builder.endDateTime;
-        
-    }
-
-    
-
     public Long getId() {
         return id;
     }
@@ -79,169 +49,18 @@ public class MandatoryCourses {
         this.name = name;
     }
 
-    public ZonedDateTime getStartDateTime() {
-        return startDateTime;
+    public Set<MandatoryCourses> getMandatoryCoursesSet() {
+        return mandatoryCoursesSet;
     }
 
-    public void setStartDateTime(ZonedDateTime startDateTime) {
-        this.startDateTime = startDateTime;
-    }
-
-    public ZonedDateTime getEndDateTime() {
-        return endDateTime;
-    }
-
-    public void setEndDateTime(ZonedDateTime endDateTime) {
-        this.endDateTime = endDateTime;
+    public void setMandatoryCoursesSet(Set<MandatoryCourses> mandatoryCoursesSet) {
+        this.mandatoryCoursesSet = mandatoryCoursesSet;
     }
 
     @Override
     public String toString() {
-        return "MandatoryCourses [id=" + id + ", name=" + name + ", startDateTime=" + startDateTime
-                + ", endDateTime=" + endDateTime + "]";
+        return "MandatoryCourses [id=" + id + ", name=" + name + ", mandatoryCoursesSet="
+                + mandatoryCoursesSet + "]";
     }
-
-    /**
-     * Builder Class
-     * @author Z.DeGuia
-     *
-     */
-    public static class Builder {
-
-        /**
-         * Id
-         */
-        private Long id;
-
-        /**
-         * Course Name
-         */
-        private String name;
-        
-        /**
-         *  Start Date and Time
-         */
-        private ZonedDateTime startDateTime;
-        
-        /**
-         *  End Date and Time
-         */
-        private ZonedDateTime endDateTime;
-
-        
-        /**
-         * <pre>
-         * Creates a new instance of Builder for courses conducted. 
-         * It validates and sets the argument into the Builder instance variables.
-         * </pre>
-         * 
-         * @param id
-         * @param name
-         */
-        public Builder(Long id, String name, ZonedDateTime startDateTime, ZonedDateTime endDateTime) {
-
-            validateId(id);
-            validateName(name);
-            validateStartDateTime(startDateTime);
-            validateEndDateTime(endDateTime);
-
-            this.id = id;
-            this.name = name;
-            this.startDateTime = startDateTime;
-            this.endDateTime = endDateTime;
-        }
-        
-        public Builder(Long id, String name) {
-
-            validateId(id);
-            validateName(name);
-
-            this.id = id;
-            this.name = name;
-        }
-        
-        public Builder(ZonedDateTime startDateTime, ZonedDateTime endDateTime) {
-            
-            validateStartDateTime(startDateTime);
-            validateEndDateTime(endDateTime);
-
-            this.startDateTime = startDateTime;
-            this.endDateTime = endDateTime;
-        }
-
-        public MandatoryCourses build() {
-            return new MandatoryCourses(this);
-        }
-        
-        /**
-         * <pre>
-         * Validate the id based on the condition below. 
-         * If it is invalid then throw an IllegalArgumentException with the corresponding message.
-         * </pre>
-         * 
-         * @param id
-         */
-        private void validateId(Long id) {
-            
-            if (id == null || id == 0L) {
-                
-                throw new IllegalArgumentException("Id should not be empty");
-                
-            }
-            
-        }
-        
-        /**
-         * <pre>
-         * Validate the name based on the condition below. 
-         * If it is invalid then throw an IllegalArgumentException with the corresponding message.
-         * </pre>
-         * 
-         * @param name
-         */
-        private void validateName(String name) {
-            
-            if (name == null || name.isEmpty()) {
-                
-                throw new IllegalArgumentException("Name should not be empty");
-                
-            }
-            
-        }
-        
-        /**
-        * <pre>
-        * Validate the Start Date and Time based on the condition below. 
-        * If it is invalid then throw an IllegalArgumentException with the corresponding message.
-        * </pre>
-        * 
-        * @param startDateTime
-        */
-       private void validateStartDateTime(ZonedDateTime startDateTime) {
-           
-           if (startDateTime == null || String.valueOf(startDateTime).isEmpty()) {
-               
-               throw new IllegalArgumentException("Start Date and Time should not be empty");
-               
-           }
-       }
-       
-       /**
-        * <pre>
-        * Validate the End Date and Time based on the condition below. 
-        * If it is invalid then throw an IllegalArgumentException with the corresponding message.
-        * </pre>
-        * 
-        * @param endDateTime
-        */
-       private void validateEndDateTime(ZonedDateTime endDateTime) {
-           
-           if (endDateTime == null || String.valueOf(endDateTime).isEmpty()) {
-               
-               throw new IllegalArgumentException("End Date and Time should not be empty");
-               
-           }
-       }
-        
-    }
+    
 }
