abefa00d9ee08b739c065f72fc8079fbe549f665
Zedriz De Guia <z.deguia@fujitsu.com>
2021-05-03 13:02:29 +0800

    updated DaoImpl

--- src/main/java/com/fujitsu/ph/tsup/reports/summary/dao/MandatoryCoursesDaoImpl.java
+++ src/main/java/com/fujitsu/ph/tsup/reports/summary/dao/MandatoryCoursesDaoImpl.java
@@ -1,16 +1,18 @@
-//==================================================================================================																																											
+//==================================================================================================                                                                                                                                                                            
 // Project Name : Training Sign Up
-// System Name  : MandatoryCoursesDaoImpl																																								
-// Class Name   : MandatoryCoursesDaoImpl.java 																																											
-//																																											
-// <<Modification History>> 																																											
-// Version | Date       | Updated By            | Content																																											
-//---------+------------+-----------------------+--------------------------------------------------- 																																											
-// 1.0.0   | 2021/04/21 | WS)J.Barbadillo       | New Creation		
+// System Name  : MandatoryCoursesDaoImpl                                                                                                                                                               
+// Class Name   : MandatoryCoursesDaoImpl.java                                                                                                                                                                          
+//                                                                                                                                                                          
+// <<Modification History>>                                                                                                                                                                             
+// Version | Date       | Updated By            | Content                                                                                                                                                                           
+//---------+------------+-----------------------+---------------------------------------------------                                                                                                                                                                            
+// 1.0.0   | 2021/04/21 | WS)J.Barbadillo       | New Creation      
 //==================================================================================================
 package com.fujitsu.ph.tsup.reports.summary.dao;
 
-
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
 import java.util.HashSet;
 import java.util.List;
@@ -25,7 +27,6 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
-
 import com.fujitsu.ph.tsup.reports.summary.model.*;
 
 /**
@@ -38,112 +39,123 @@ import com.fujitsu.ph.tsup.reports.summary.model.*;
  */
 
 @Repository
-public class MandatoryCoursesDaoImpl implements MandatoryCoursesDao {
-	
-	/**
-	 * Logger Factory
-	 */
-	private static Logger logger = LoggerFactory.getLogger(MandatoryCoursesDaoImpl .class);
-	
-	/**
+public class MandatoryCoursesDaoImpl implements MandatoryCoursesDao{
+    
+    /**
+     * Logger Factory
+     */
+    private static Logger logger = LoggerFactory.getLogger(MandatoryCoursesDaoImpl .class);
+    
+    /**
      * JDBC Template for Named Parameters
      */
     @Autowired
     private NamedParameterJdbcTemplate template;
 
-	
-    @Override
-	public Set<MandatoryCourses> findMandatoryCourses() {
-			
-		String query = "SELECT "
-					+ "C.ID AS ID, "
-					+ "C.NAME AS COURSE_NAME " 																							
-					+ "FROM TSUP.COURSE C "																						
-					+ "WHERE C.MANDATORY = 'YES';";																						
+    // NEED TO ADJUST
+    public Set<MandatoryCourses> findMandatoryCourses(LocalDateTime selectedStartDateTime,
+            LocalDateTime selectedEndDateTime) {
+            
+        String query = "SELECT "
+                    + "     C.ID AS ID, "
+                    + "     C.NAME AS COURSE_NAME "                                                                                             
+                    + "FROM TSUP.COURSE_SCHEDULE AS CSCHED "                                                                                            
+                    + "INNER JOIN TSUP.COURSE_SCHEDULE_DETAIL AS CSD "                                                                                          
+                    + "     ON CSCHED.ID = CSD.COURSE_SCHEDULE_ID "                                                                             
+                    + "INNER JOIN TSUP.COURSE_ATTENDANCE AS CA "                                                                                    
+                    + "     ON CA.ID = CSD.ID "                                                                                         
+                    + "INNER JOIN TSUP.COURSE AS C "                                                                                                
+                    + "     ON CSCHED.COURSE_ID = C.ID "                                                                                            
+                    + "WHERE COALESCE(CSD.RESCHEDULED_START_DATETIME, CSD.SCHEDULED_START_DATETIME) "                                                                                               
+                    + "BETWEEN :scheduledStartDateTime "
+                    + "AND :scheduledEndDateTime "                                                                                  
+                    + "AND C.MANDATORY = 'YES'" ;                                                                                       
+        
+        SqlParameterSource mandatoryCoursesParameters = new MapSqlParameterSource()
+                .addValue("scheduledStartDateTime", selectedStartDateTime)
+                .addValue("scheduledEndDateTime", selectedEndDateTime);
+        
+        List<MandatoryCourses> mandatoryCoursesList = template.query(query, mandatoryCoursesParameters,
+                new MandatoryCoursesRowMapper());
+        
+        Set<MandatoryCourses> mandatoryCourses = new HashSet<>(mandatoryCoursesList);
 
-		List<MandatoryCourses> mandatoryCoursesList = template.query(query,
-				new  MandatoryCoursesRowMapper());
-			Set<MandatoryCourses> mandatoryCourses = new HashSet<>(mandatoryCoursesList );
+        System.out.println("\r\n***** mandatoryCourses *****" + mandatoryCoursesList);
+        
+        logger.debug("Result: {}", mandatoryCoursesList);
 
-		    logger.debug("Result: {}", mandatoryCoursesList );
-			
-			return mandatoryCourses;
-		   
-		
-	}
-	
-	
-    @Override
-	public int findTotalNumberOfJdu() {
-			
-		String query = "SELECT "
-					+ "	  COUNT(*) AS TOTAL_NUMBER_OF_JDU "  																							
-					+ "FROM TSUP.EMPLOYEE AS E "	
-					+ "INNER JOIN DEPARTMENT D "
-					+ "	ON D.ID = E.DEPARTMENT_ID "
-					+ "WHERE  D.DEPARTMENT_NAME = :DEPARTMENT_NAME;";	
-		
-		 SqlParameterSource mandatoryCoursesParameters = new MapSqlParameterSource()
-                 .addValue("DEPARTMENT_NAME", "FDC-G3CC");
-		 return template.queryForObject(query, mandatoryCoursesParameters, Integer.class);
-		 
-	}
-	
-	
-    @Override
-	public int findTotalNumberOfJduWhoFinishedTraining(ZonedDateTime selectedStartDateTime,
-			ZonedDateTime selectedEndDateTime) {
-			
-		String query =  "SELECT "																					
-					+ "		COUNT(CA.ID) AS TOTAL_NUMBER_OF_JDU_WHO_FINISHED_TRAINING "																				
-					+ "FROM TSUP.COURSE_SCHEDULE AS CSCHED "																					
-			        + "INNER JOIN TSUP.COURSE_SCHEDULE_DETAIL AS CSCHEDDET "																					
-				    + "		ON CSCHED.ID = CSCHEDDET.COURSE_SCHEDULE_ID "																				
-			        + "INNER JOIN TSUP.COURSE_ATTENDANCE AS CA "																					
-			        + "		ON CA.ID = CSCHEDDET.ID "																				
-			        + "INNER JOIN tsup.COURSE AS C "																				
-			        + "		ON CSCHED.COURSE_ID = C.ID "
-			        + "INNER JOIN TSUP.CERTIFICATE_UPLOAD AS CUPLOAD "																			
-			        + "		ON CUPLOAD.COURSE_ID = C.ID "
-			        + "WHERE CUPLOAD.CERTIFICATE IS NOT NULL "
-			        + "AND C.MANDATORY = 'YES';";					
-		
-		SqlParameterSource mandatoryCoursesParameters = new MapSqlParameterSource()
-				.addValue("scheduledStartDateTime", selectedStartDateTime.toOffsetDateTime())
+        return mandatoryCourses;
+
+    }
+
+    // IF DEPARTMENT ID IS 2 WHICH IS G3CC
+    // NEED TO ADJUST
+    public int findTotalNumberOfJdu(ZonedDateTime selectedStartDateTime,
+            ZonedDateTime selectedEndDateTime) {
+            
+        String query = "SELECT "
+                    + "   COUNT(*) AS TOTAL_NUMBER_OF_JDU "                                                                                             
+                    + "FROM TSUP.EMPLOYEE AS E "                                                                                            
+                    + "WHERE  E.DEPARTMENT_NAME = :DEPARTMENT_NAME;";   
+        
+         SqlParameterSource mandatoryCoursesParameters = new MapSqlParameterSource()
+                 .addValue(":DEPARTMENT_NAME", "FDC-G3CC");
+         return template.queryForObject(query, mandatoryCoursesParameters, Integer.class);
+         
+    }
+    
+    // NEED TO ADJUST
+    public int findTotalNumberOfJduWhoFinishedTraining(ZonedDateTime selectedStartDateTime,
+            ZonedDateTime selectedEndDateTime) {
+            
+        String query =  "SELECT "                                                                                   
+                    + "     COUNT(CA.ID) AS TOTAL_NUMBER_OF_JDU_WHO_FINISHED_TRAINING "                                                                             
+                    + "FROM TSUP.COURSE_SCHEDULE AS CSCHED "                                                                                    
+                    + "INNER JOIN TSUP.COURSE_SCHEDULE_DETAIL AS CSCHEDDET "                                                                                    
+                    + "     ON CSCHED.ID = CSCHEDDET.COURSE_SCHEDULE_ID "                                                                               
+                    + "INNER JOIN TSUP.COURSE_ATTENDANCE AS CA "                                                                                    
+                    + "     ON CA.ID = CSCHEDDET.ID "                                                                               
+                    + "INNER JOIN tsup.COURSE AS C "                                                                                
+                    + "     ON CSCHED.COURSE_ID = C.ID "
+                    + "INNER JOIN TSUP.CERTIFICATE_UPLOAD AS CUPLOAD "                                                                          
+                    + "     ON CUPLOAD.COURSE_ID = C.ID "
+                    + "WHERE CUPLOAD.CERTIFICATE IS NOT NULL "
+                    + "AND C.MANDATORY = 'YES';";                   
+        
+        SqlParameterSource mandatoryCoursesParameters = new MapSqlParameterSource()
+                .addValue("scheduledStartDateTime", selectedStartDateTime.toOffsetDateTime())
                 .addValue("scheduledEndDateTime", selectedEndDateTime.toOffsetDateTime());
-		
-		
-		 return template.queryForObject(query, mandatoryCoursesParameters, Integer.class);
-	}
-	
-	
-    @Override
-	public int findTotalNumberOfJduWhoFinishedTrainingLastweek(ZonedDateTime selectedStartDateTime,
-			ZonedDateTime selectedEndDateTime) {
-			
-		String query =  "SELECT "																					
-					+ "		COUNT(CA.ID) AS TOTAL_NUMBER_OF_JDU_WHO_FINISHED_TRAINING "																				
-					+ "FROM TSUP.COURSE_SCHEDULE AS CSCHED "																					
-			        + "INNER JOIN TSUP.COURSE_SCHEDULE_DETAIL AS CSCHEDDET "																					
-				    + "		ON CSCHED.ID = CSCHEDDET.COURSE_SCHEDULE_ID "																				
-			        + "INNER JOIN TSUP.COURSE_ATTENDANCE AS CA "																					
-			        + "		ON CA.ID = CSCHEDDET.ID "																				
-			        + "INNER JOIN tsup.COURSE AS C "																				
-			        + "		ON CSCHED.COURSE_ID = C.ID "
-			        + "INNER JOIN TSUP.CERTIFICATE_UPLOAD AS CUPLOAD "																			
-			        + "		ON CUPLOAD.COURSE_ID = C.ID "
-			        + "WHERE CUPLOAD.CERTIFICATE IS NOT NULL "
-			        + "AND C.MANDATORY = 'YES' "
-			        + "AND CA.LOG_OUT_DATETIME <= (NOW()::DATE-EXTRACT(DOW from NOW())::INTEGER)";																				
-			        																						
+        
+        
+         return template.queryForObject(query, mandatoryCoursesParameters, Integer.class);
+    }
+    
+    // NEED TO ADJUST
+    public int findTotalNumberOfJduWhoFinishedTrainingLastWeek(ZonedDateTime selectedStartDateTime,
+            ZonedDateTime selectedEndDateTime) {
+            
+        String query =  "SELECT "                                                                                   
+                    + "     COUNT(CA.ID) AS TOTAL_NUMBER_OF_JDU_WHO_FINISHED_TRAINING_LASTWEEK  "                                                                               
+                    + "FROM TSUP.COURSE_SCHEDULE AS CSCHED "                                                                                    
+                    + "INNER JOIN TSUP.COURSE_SCHEDULE_DETAIL AS CSCHEDDET "                                                                                    
+                    + "     ON CSCHED.ID = CSCHEDDET.COURSE_SCHEDULE_ID "                                                                               
+                    + "INNER JOIN TSUP.COURSE_ATTENDANCE AS CA "                                                                                    
+                    + "     ON CA.ID = CSCHEDDET.ID "                                                                               
+                    + "INNER JOIN tsup.COURSE AS C "                                                                                
+                    + "     ON CSCHED.COURSE_ID = C.ID "                                                                                
+                    + "INNER JOIN TSUP.CERTIFICATE_UPLOAD AS CUPLOAD "                                                                          
+                    + "     ON CUPLOAD.COURSE_ID = C.ID "   
+                    + "WHERE CUPLOAD.UPDATE_DATE IS NOT NULL AND "
+                    + "DATE_PART('week',CA.log_out_datetime) < DATE_PART('week',CURRENT_DATE);";                                                                                
+                                                                                                            
 
-		SqlParameterSource mandatoryCoursesParameters = new MapSqlParameterSource()
-				.addValue("scheduledStartDateTime", selectedStartDateTime.toOffsetDateTime())
+        SqlParameterSource mandatoryCoursesParameters = new MapSqlParameterSource()
+                .addValue("scheduledStartDateTime", selectedStartDateTime.toOffsetDateTime())
                 .addValue("scheduledEndDateTime", selectedEndDateTime.toOffsetDateTime());
 
-		 return template.queryForObject(query, mandatoryCoursesParameters, Integer.class);
-	}
-	
-	
-	
+         return template.queryForObject(query, mandatoryCoursesParameters, Integer.class);
+    }
+    
+    
+    
 }
