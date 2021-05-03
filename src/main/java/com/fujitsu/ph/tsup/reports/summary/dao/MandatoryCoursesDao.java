5b39b705df4583f72368aff687c46ed47acd7ba4
Zedriz De Guia <z.deguia@fujitsu.com>
2021-05-03 13:02:15 +0800

    updated Dao

--- src/main/java/com/fujitsu/ph/tsup/reports/summary/dao/MandatoryCoursesDao.java
+++ src/main/java/com/fujitsu/ph/tsup/reports/summary/dao/MandatoryCoursesDao.java
@@ -1,15 +1,17 @@
-//==================================================================================================																																											
+//==================================================================================================                                                                                                                                                                            
 // Project Name : Training Sign Up
-// System Name  : MandatoryCoursesDao																																								
-// Class Name   : MandatoryCoursesDao.java 																																											
-//																																											
-// <<Modification History>> 																																											
-// Version | Date       | Updated By            | Content																																											
-//---------+------------+-----------------------+--------------------------------------------------- 																																											
-// 1.0.0   | 2021/04/21 | WS)J.Barbadillo       | New Creation																																											
-//==================================================================================================																																																																																						
+// System Name  : MandatoryCoursesDao                                                                                                                                                               
+// Class Name   : MandatoryCoursesDao.java                                                                                                                                                                          
+//                                                                                                                                                                          
+// <<Modification History>>                                                                                                                                                                             
+// Version | Date       | Updated By            | Content                                                                                                                                                                           
+//---------+------------+-----------------------+---------------------------------------------------                                                                                                                                                                            
+// 1.0.0   | 2021/04/21 | WS)J.Barbadillo       | New Creation                                                                                                                                                                          
+//==================================================================================================                                                                                                                                                                                                                                                                                                                                                        
 package com.fujitsu.ph.tsup.reports.summary.dao; 
 
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
 import java.util.Set;
 
@@ -19,7 +21,7 @@ import com.fujitsu.ph.tsup.reports.summary.model.MandatoryCourses;
 
 /**
 * <pre>
-* 	The Dao for the MandatoryCoursesController
+*   The Dao for the MandatoryCoursesController
 * </pre>
 * 
 * @author j.barbadillo
@@ -27,41 +29,47 @@ import com.fujitsu.ph.tsup.reports.summary.model.MandatoryCourses;
 */
 
 public interface MandatoryCoursesDao {
-	
-	/**
-	 * <pre>
-     * 	Find the total number of  JDU based on the given date range
-     * </pre>
-     */
-	Set<MandatoryCourses> findMandatoryCourses();
-	
-	
-	/**
-	 * <pre>
-     * 	Count the total number of JDU
-     * </pre>
-     */
-	int findTotalNumberOfJdu();
-	
-	/**
-	 * <pre>
-     * 	Find the total number of  JDU who finished training  based on the given date range  
+    
+    /**
+     * <pre>
+     *  Find the total number of  JDU based on the given date range
      * </pre>
      * @param selectedStartDateTime
      * @param selectedEndDateTime 
      */
-	int findTotalNumberOfJduWhoFinishedTraining(ZonedDateTime selectedStartDateTime,
-			ZonedDateTime selectedEndDateTime);
-	
-	
-	/**
-	 * <pre>
-     * 	Find the total number of  JDU who finished training lastweek  based on the given date range  
+    Set<MandatoryCourses> findMandatoryCourses(LocalDateTime selectedStartDateTime,
+            LocalDateTime selectedEndDateTime);
+    
+    
+    /**
+     * <pre>
+     *  Count the total number of JDU
      * </pre>
      * @param selectedStartDateTime
      * @param selectedEndDateTime 
      */
-	int findTotalNumberOfJduWhoFinishedTrainingLastweek(ZonedDateTime selectedStartDateTime,
-			ZonedDateTime selectedEndDateTime);
-	
+    int findTotalNumberOfJdu(ZonedDateTime selectedStartDateTime,
+            ZonedDateTime selectedEndDateTime);
+    
+    /**
+     * <pre>
+     *  Find the total number of  JDU who finished training  based on the given date range  
+     * </pre>
+     * @param selectedStartDateTime
+     * @param selectedEndDateTime 
+     */
+    int findTotalNumberOfJduWhoFinishedTraining(ZonedDateTime selectedStartDateTime,
+            ZonedDateTime selectedEndDateTime);
+    
+    
+    /**
+     * <pre>
+     *  Find the total number of  JDU who finished training lastweek  based on the given date range  
+     * </pre>
+     * @param selectedStartDateTime
+     * @param selectedEndDateTime 
+     */
+    int findTotalNumberOfJduWhoFinishedTrainingLastWeek(ZonedDateTime selectedStartDateTime,
+            ZonedDateTime selectedEndDateTime);
+    
 }
