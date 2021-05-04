7ae4cb49717840481100f49d506080a8599f78df
Zedriz De Guia <z.deguia@fujitsu.com>
2021-05-04 08:43:14 +0800

    updated MandatoryCoursesRowMapper

--- src/main/java/com/fujitsu/ph/tsup/reports/summary/dao/MandatoryCoursesRowMapper.java
+++ src/main/java/com/fujitsu/ph/tsup/reports/summary/dao/MandatoryCoursesRowMapper.java
@@ -16,7 +16,6 @@ import java.sql.SQLException;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.fujitsu.ph.tsup.reports.summary.model.MandatoryCourses;
-import com.fujitsu.ph.tsup.scheduling.model.TopLearnersForm;
 
 /**
  * <pre>
@@ -39,33 +38,14 @@ public class MandatoryCoursesRowMapper implements RowMapper<MandatoryCourses> {
      */
     public MandatoryCourses mapRow(ResultSet rs, int rowNum) throws SQLException {
 
-        
         MandatoryCourses mandatoryCourses = new MandatoryCourses();
         Long id = rs.getLong("ID");
         String name = rs.getString("COURSE_NAME");
-//        Long totalNoOfJDUMem = rs.getLong("TOTAL_NUMBER_OF_JDU");
-//        Long totalNoOfJDUMemFin = rs.getLong("TOTAL_NUMBER_OF_JDU_WHO_FINISHED_TRAINING");
-//        Long totalNoOfJDUMemFinLastWk = rs.getLong("TOTAL_NUMBER_OF_JDU_WHO_FINISHED_TRAINING_LASTWEEK");
-
-//        MandatoryCourses mandatoryCourses = new MandatoryCourses.Builder(id, name, totalNoOfJDUMem,
-//                totalNoOfJDUMemFin, totalNoOfJDUMemFinLastWk).build();
-
-        //MandatoryCourses mandatoryCourses = new MandatoryCourses.Builder(id, name).build();
         
         mandatoryCourses.setId(id);
         mandatoryCourses.setName(name);
         
         return mandatoryCourses;
-        
-        
-//        int id = tpl.getInt("PARTICIPANT_ID");
-//        int place = tpl.getRow();
-//        String participantName = tpl.getString("PARTICIPANT_LAST_NAME") + ", " + tpl.getString("PARTICIPANT_FIRST_NAME");
-//        
-//        topLearnersForm.setId(id);
-//        topLearnersForm.setParticipantName(participantName);
-//        topLearnersForm.setPlace(place);
-//        return topLearnersForm;
 
     }
 
