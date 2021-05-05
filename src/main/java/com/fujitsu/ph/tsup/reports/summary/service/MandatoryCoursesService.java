cff1e9be3801bc209a325283a59574f9a4438b85
j.barbadillo <j.barbadillo@G07PHXNWES00056.g07.fujitsu.local>
2021-05-05 11:38:12 +0800

    Refs #134961 MandatoryCoursesService -Updated the comments

--- src/main/java/com/fujitsu/ph/tsup/reports/summary/service/MandatoryCoursesService.java
+++ src/main/java/com/fujitsu/ph/tsup/reports/summary/service/MandatoryCoursesService.java
@@ -1,69 +1,71 @@
-/*
- * Copyright (C) 2021 FUJITSU LIMITED All rights reserved.
- */
+//==================================================================================================                                                                                                                                                                            
+// Project Name : Training Sign Up
+// System Name  : MandatoryCoursesService                                                                                                                                                               
+// Class Name   : MandatoryCoursesService.java                                                                                                                                                                          
+//                                                                                                                                                                          
+// <<Modification History>>                                                                                                                                                                             
+// Version | Date       | Updated By            | Content                                                                                                                                                                           
+//---------+------------+-----------------------+---------------------------------------------------                                                                                                                                                                            
+// 1.0.0   | 2021/04/21 | WS)C.Fuerzas          | New Creation      
+// 1.0.1   | 2021/05/05 | WS)C.Fuerzas          | Updated
+//==================================================================================================
 package com.fujitsu.ph.tsup.reports.summary.service;
 
 import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZonedDateTime;
 import java.util.Set;
 
 import com.fujitsu.ph.tsup.reports.summary.model.MandatoryCourses;
-import com.fujitsu.ph.tsup.reports.summary.model.MandatoryCoursesForm;
 
 /**
+ * <pre>
  * Service for the MandatoryCoursesController
- * 
- * @author c.fuerzas (New Creation by: c.fuerzas)
- * @version Revision: 0.01 Date: 2021-04-21
+ * </pre>
+ * @author c.fuerzas 
+ * @version 1.0.1
  */
 public interface MandatoryCoursesService {
 
-    /**
+	/**
+	 * <pre>
      * Finds all the mandatory courses for all members based on the given date range
-     * 
+     * </pre>
      * @param selectedStartDateTime
      * @param selectedEndDateTime
-     * @return Set of MandatoryCoursesForm
+     * @return Set of MandatoryCourses
      */
     public Set<MandatoryCourses> getMandatoryCourses(LocalDateTime
             selectedStartDateTime, LocalDateTime selectedEndDateTime);
 
     /**
      * Acquires the total number of JDU members.
-     * 
-     * @return int
+     * @return long
      */
     public long getTotalNumberOfJduMembers();
 
     /**
      * Acquires the total number of completion for the specified course.
-     * 
-     * @param MandatoryCourse
-     * @return int
+     * @param mandatoryCourse
+     * @return long
      */
     public long getTotalNumberOfCompletion(String mandatoryCourse);
 
     /**
      * Acquires the total number of completion for the specified course within last
      * week
-     * 
-     * @param courseName
-     * @return int
+     * @param mandatoryCourse
+     * @return long
      */
     public long getTotalNumberOfCompletionLastWeek(String mandatoryCourse);
 
     /**
      * Calculates the percentage completion per course
-     * 
-     * @return int
+     * @return double
      */
     public double getPercentageCompletion();
 
     /**
      * Calculates the percentage completion per course within last week
-     * 
-     * @return int
+     * @return double
      */
     public double getPercentageCompletionLastWeek();
 }
